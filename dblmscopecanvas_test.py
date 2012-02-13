5776b4c3d138cd82d6e822f7f097c5f9f967d741
Éric Piel <piel@delmic.com>
2012-02-13 10:27:56 +0100

    a lot more test cases

--- dblmscopecanvas_test.py
+++ dblmscopecanvas_test.py
@@ -61,17 +61,19 @@ class TestDblMicroscopeCanvas(unittest.TestCase):
         
         # add images
         im1 = wx.EmptyImage(11, 11, clear=True)
-        im1.SetRGB(5, 5, 255, 0, 0) # Red pixel at center, (5,5)
+        px1_cent = (5,5)
+        im1.SetRGB(px1_cent[0], px1_cent[1], 255, 0, 0) # Red pixel at center, (5,5)
         im2 = wx.EmptyImage(201, 201, clear=True)
-        im2.SetRGB(100, 100, 0, 0, 255) # Blue pixel at center (100,100)
+        px2_cent = (100,100)
+        im2.SetRGB(px2_cent[0], px2_cent[1], 0, 0, 255) # Blue pixel at center (100,100)
         self.canvas.SetImage(0, im1, (0,0), ppm * 10)
         self.canvas.SetImage(1, im2, (200,200), ppm)
-        for i in range(im2.GetWidth()):
-            print i
-            for j in range(im2.GetHeight()):
-                px = GetRGB(im2, i, j)
-                if px != (0,0,0):
-                    print px, i, j
+#        for i in range(im2.GetWidth()):
+#            print i
+#            for j in range(im2.GetHeight()):
+#                px = GetRGB(im2, i, j)
+#                if px != (0,0,0):
+#                    print px, i, j
                     
         # merge the images
         ratio = 0.5
@@ -84,20 +86,8 @@ class TestDblMicroscopeCanvas(unittest.TestCase):
         loop()
 
         # copy the buffer into a nice image here
-        resultBmp = wx.EmptyBitmap(*self.canvas.buffer_size)
-        resultDC = wx.MemoryDC()
-        resultDC.SelectObject(resultBmp)
-        resultDC.BlitPointSize((0,0), self.canvas.buffer_size, self.canvas._dcBuffer, (0,0))
-        resultDC.SelectObject(wx.NullBitmap)
-        resultIm = wx.ImageFromBitmap(resultBmp)
-
-#        for i in range(resultIm.GetWidth()):
-#            print i
-#            for j in range(resultIm.GetHeight()):
-#                px = GetRGB(resultIm, i, j)
-#                if px != (0,0,0):
-#                    print px, i, j
-#        
+        resultIm = GetImageFromBuffer(self.canvas)
+        
         px1 = GetRGB(resultIm, self.canvas.buffer_size[0]/2, self.canvas.buffer_size[1]/2)
         self.assertEqual(px1, (127, 0, 0))
         px2 = GetRGB(resultIm, self.canvas.buffer_size[0]/2 + 200, self.canvas.buffer_size[1]/2 + 200)
@@ -106,11 +96,102 @@ class TestDblMicroscopeCanvas(unittest.TestCase):
         # remove first picture
         self.canvas.SetImage(0, None)
         loop()
+        time.sleep(1)
+        loop()
+        
+        resultIm = GetImageFromBuffer(self.canvas)
+        px2 = GetRGB(resultIm, self.canvas.buffer_size[0]/2 + 200, self.canvas.buffer_size[1]/2 + 200)
+        self.assertEqual(px2, (0, 0, 255))
 
+    def test_BasicMove(self):
+        ppm = 0.0001
+        self.canvas.SetMPP(ppm)
+        self.assertEqual(ppm, self.canvas.GetMPP())
+        
+        # add images
+        im1 = wx.EmptyImage(11, 11, clear=True)
+        px1_cent = (5,5)
+        im1.SetRGB(px1_cent[0], px1_cent[1], 255, 0, 0) # Red pixel at center, (5,5)
+        im2 = wx.EmptyImage(201, 201, clear=True)
+        px2_cent = (100,100)
+        im2.SetRGB(px2_cent[0], px2_cent[1], 0, 0, 255) # Blue pixel at center (100,100)
+        self.canvas.SetImage(0, im1, (0,0), ppm * 10)
+        self.canvas.SetImage(1, im2, (200,200), ppm)
+        
+        shift = (100,100)
+        self.canvas.ShiftView(shift)
+        
+        # merge the images
+        ratio = 0.5
+        self.canvas.SetMergeRatio(ratio)
+        self.assertEqual(ratio, self.canvas.GetMergeRatio())
+        
+        loop()
+        # it's supposed to update in less than 1s
+        time.sleep(1)
+        loop()
+
+        # copy the buffer into a nice image here
+        resultIm = GetImageFromBuffer(self.canvas)
+        
+        px1 = GetRGB(resultIm, self.canvas.buffer_size[0]/2 + 100, self.canvas.buffer_size[1]/2 + 100)
+        self.assertEqual(px1, (127, 0, 0))
+        px2 = GetRGB(resultIm, self.canvas.buffer_size[0]/2 + 300, self.canvas.buffer_size[1]/2 + 300)
+        self.assertEqual(px2, (0, 0, 255))
+        
+        
+    def test_ZoomMove(self):
+        ppm = 0.0001
+        self.canvas.SetMPP(ppm)
+        self.assertEqual(ppm, self.canvas.GetMPP())
+        
+        # add images
+        im1 = wx.EmptyImage(11, 11, clear=True)
+        px1_cent = (5,5)
+        im1.SetRGB(px1_cent[0], px1_cent[1], 255, 0, 0) # Red pixel at center, (5,5)
+        self.canvas.SetImage(0, im1, (0,0), ppm)
+        
+        shift = (10,10)
+        self.canvas.ShiftView(shift)
+
+        loop()
+        time.sleep(1)
+        loop()
+        resultIm = GetImageFromBuffer(self.canvas)
+        
+        px1 = GetRGB(resultIm, 
+                     self.canvas.buffer_size[0]/2 + 10,
+                     self.canvas.buffer_size[1]/2 + 10)
+        self.assertEqual(px1, (255, 0, 0))
+
+        # zoom in
+        self.canvas.SetZoom(2)
+        loop()
+        time.sleep(1)
+        loop()
+        resultIm = GetImageFromBuffer(self.canvas)
+        
+        px1 = GetRGB(resultIm, 
+             self.canvas.buffer_size[0]/2 + 40,
+             self.canvas.buffer_size[1]/2 + 40)
+        self.assertEqual(px1, (255, 0, 0))
+
+        
 if __name__ == "__main__":
     unittest.main()
     
 def GetRGB(im, x, y):
     return (im.GetRed(x,y), im.GetGreen(x,y), im.GetBlue(x,y))
 
+def GetImageFromBuffer(canvas):
+    """
+    Copy the current buffer into a wx.Image
+    """
+    resultBmp = wx.EmptyBitmap(*canvas.buffer_size)
+    resultDC = wx.MemoryDC()
+    resultDC.SelectObject(resultBmp)
+    resultDC.BlitPointSize((0,0), canvas.buffer_size,canvas._dcBuffer, (0,0))
+    resultDC.SelectObject(wx.NullBitmap)
+    return wx.ImageFromBitmap(resultBmp)
+    
 # vim:tabstop=4:shiftwidth=4:expandtab:spelllang=en_gb:spell:
