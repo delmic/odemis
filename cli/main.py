dfdab1d069b02c5b972897a8b2205e7689c5c670
Éric Piel <piel@delmic.com>
2012-07-13 18:19:21 +0200

    cli: support listing properties

--- cli/main.py
+++ cli/main.py
@@ -16,8 +16,10 @@ Delmic Acquisition Software is distributed in the hope that it will be useful, b
 You should have received a copy of the GNU General Public License along with Delmic Acquisition Software. If not, see http://www.gnu.org/licenses/.
 '''
 # This is a basic command line interface to the odemis back-end
+import Pyro4
 import __version__
 import argparse
+import inspect
 import logging
 import model
 import os
@@ -109,6 +111,85 @@ def list_components():
     print_microscope_tree(microscope)
     return 0
 
+def print_roattribute(name, value):
+    print "\t" + name + " (RO Attribute)\t value:" + str(value)
+
+#known_fixed_attributes = ["shape", "axes", "ranges"]
+non_roattributes_classes = (Pyro4.core._RemoteMethod, Pyro4.Proxy, model.Component,
+                            model.DataFlow, model.VigilantAttribute)
+non_roattributes_names = ("name", "role", "parent", "children", "affects", 
+                          "actuators", "detectors", "emitters")
+def print_roattributes(component):
+    # roattributes are a bit tricky because they look like completely normal value
+    # so we find them by elimination
+    for name, value in inspect.getmembers(component):
+        # it should not start with a "_"
+        if name.startswith("_"):
+            continue
+        # it should not be a special name
+        if name in non_roattributes_names:
+            continue
+        # it should not be callable
+        if callable(value):
+            continue
+        # it should not be a special class
+        if isinstance(value, non_roattributes_classes):
+            continue
+        print_roattribute(name, value)
+        
+
+def print_data_flow(name, df):
+    print "\t" + name + " (Data-flow)"
+
+def print_data_flows(component):
+    # find all dataflows
+    for name, value in inspect.getmembers(component, lambda x: isinstance(x, model.DataFlow)):
+        print_data_flow(name, value)
+
+def print_vattribute(name, va):
+    if va.unit:
+        unit = "(unit: %s)" % va.unit
+    else:
+        unit = ""
+    print "\t" + name + " (Vigilant Attribute)\t value: %s %s" % (str(va.value), unit)
+
+def print_vattributes(component):
+    # find all vattributes
+    # 
+    for name, value in inspect.getmembers(component, lambda x: isinstance(x, model.VigilantAttribute)):
+        print_vattribute(name, value)
+    
+def print_attributes(component):
+    print "Component '%s':" % component.name
+    print "\trole: %s" % component.role
+    print "\taffects: " + ", ".join(["'"+c.name+"'" for c in component.affects]) 
+    print_roattributes(component)
+    print_vattributes(component)
+    print_data_flows(component)
+
+def list_properties(comp_name):
+    """
+    print the data-flows and vattributes of a component
+    comp_name (string): name of the component
+    """
+    try:
+        components = model.getComponents()
+        component = None
+        for c in components:
+            if c.name == comp_name:
+                component = c
+                break
+    except:
+        logging.error("Failed to contact the back-end")
+        return 127
+   
+    if component is None:
+        logging.error("Failed to find component '%s'", comp_name)
+        return 127
+    
+    print_attributes(component)
+    
+
 def main(args):
     """
     Handles the command line arguments 
@@ -132,6 +213,8 @@ def main(args):
                         help="Check for a running back-end (only returns exit code)")
     dm_grpe.add_argument("--list", "-l", dest="list", action="store_true", default=False,
                         help="List the components of the microscope")
+    dm_grpe.add_argument("--list-prop", "-L", dest="listprop", metavar="COMPONENT",
+                         help="List the properties of a component")
     options = parser.parse_args(args[1:])
     
     # Set up logging before everything else
@@ -147,7 +230,7 @@ def main(args):
     logging.getLogger().addHandler(handler)
     
     # anything to do?
-    if not (options.check or options.kill or options.list):
+    if not (options.check or options.kill or options.list) and options.listprop is None:
         logging.error("No action specified.")
         return 127
     
@@ -169,6 +252,9 @@ def main(args):
 
     if options.list:
         return list_components()
+    
+    if options.listprop is not None:
+        return list_properties(options.listprop)
     
     return 0
 
