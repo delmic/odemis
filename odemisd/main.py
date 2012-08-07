31d30717c777663f101a4528936cae2d3dad1a49
Éric Piel <piel@delmic.com>
2012-08-07 15:49:10 +0200

    odemisd: new --debug parameter to run everything in the original process
    also refactor the code to simplify future extention

--- odemisd/main.py
+++ odemisd/main.py
@@ -23,8 +23,10 @@ import grp
 import logging
 import model
 import os
+import signal
 import stat
 import sys
+import time
 
 # TODO the way metadata is updated has probably to be completely changed
 # cf specification (=> send all the metadata to the data generator)
@@ -45,46 +47,6 @@ def updateMetadata(metadata, parent):
             # no affects == empty set
             pass
 
-def set_base_group():
-    """
-    Change the current process to be running in the base group (odemis)
-    raise:
-        Exception in case it's not possible (lack of permissions...)
-    """
-    try:
-        gid_base = grp.getgrnam(model.BASE_GROUP).gr_gid
-    except KeyError:
-        logging.error(model.BASE_GROUP + " doesn't exists.")
-        raise
-    
-    try:
-        os.setgid(gid_base)
-    except OSError:
-        logging.warning("Not enough permissions to get group " + model.BASE_GROUP + ", trying anyway...")
-        
-    # everything created after must be rw by group
-    os.umask(~(stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP)) 
-
-def mk_base_dir():
-    """
-    Create the base directory for communication between containers if it's not
-    present yet. To create it, you likely need root permissions.
-    raise:
-        Exception in case it's not possible to create it (lack of permissions...)
-    """
-    if not os.path.exists(model.BASE_DIRECTORY):
-        # it will raise an appropriate exception if it fails to create it
-        os.mkdir(model.BASE_DIRECTORY)
-        
-#        # change the group
-#        gid_base = grp.getgrnam(model.BASE_GROUP).gr_gid
-#        os.chown(model.BASE_DIRECTORY, -1, gid_base)
-        # Files inside are all group odemis, and it can be listed by anyone
-        os.chmod(model.BASE_DIRECTORY, stat.S_ISGID | stat.S_IRWXU | stat.S_IRWXG | stat.S_IROTH | stat.S_IXOTH)
-        logging.debug("created directory " + model.BASE_DIRECTORY)
-    elif not os.path.isdir(model.BASE_DIRECTORY):
-        # the unlikely case it's a file
-        logging.warning(model.BASE_DIRECTORY + " is not a directory, trying anyway...")
 
 class BackendContainer(model.Container):
     """
@@ -107,18 +69,175 @@ class BackendContainer(model.Container):
     def setMicroscope(self, component):
         self.rootId = component._pyroId
 
-def terminate_all_components(components):
-    """
-    try to terminate all the components given as much as possible
-    components (set of Components): set of components to stop
-    """
-    for comp in components:
+class BackendRunner(object):
+    CONTAINER_DISABLE="0" # only for debugging: everything is created in the process
+    CONTAINER_ALL_IN_ONE="1" # one backend container for everything
+    CONTAINER_SEPARATED="+" # each component is started in a separate container
+    
+    def __init__(self, model_file, daemon=False, dry_run=False, containement=CONTAINER_SEPARATED):
+        """
+        containement (CONTAINER_*): the type of container policy to use
+        """
+        
+        self.model = model_file
+        self.daemon = daemon
+        self.dry_run = dry_run
+        self.containement = containement
+        
+        self._container = None
+        self._components = set()
+        
+        signal.signal(signal.SIGINT, self.handle_signal)
+    
+    def set_base_group(self):
+        """
+        Change the current process to be running in the base group (odemis)
+        raise:
+            Exception in case it's not possible (lack of permissions...)
+        """
         try:
-            comp.terminate()
-        except:
-            # can happen if it was already terminated 
-            logging.warning("Failed to terminate component '%s'", comp.name)
+            gid_base = grp.getgrnam(model.BASE_GROUP).gr_gid
+        except KeyError:
+            logging.error(model.BASE_GROUP + " doesn't exists.")
+            raise
+        
+        try:
+            os.setgid(gid_base)
+        except OSError:
+            logging.warning("Not enough permissions to get group " + model.BASE_GROUP + ", trying anyway...")
+            
+        # everything created after must be rw by group
+        os.umask(~(stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP)) 
 
+    def mk_base_dir(self):
+        """
+        Create the base directory for communication between containers if it's not
+        present yet. To create it, you likely need root permissions.
+        raise:
+            Exception in case it's not possible to create it (lack of permissions...)
+        """
+        if not os.path.exists(model.BASE_DIRECTORY):
+            # it will raise an appropriate exception if it fails to create it
+            os.mkdir(model.BASE_DIRECTORY)
+            
+    #        # change the group
+    #        gid_base = grp.getgrnam(model.BASE_GROUP).gr_gid
+    #        os.chown(model.BASE_DIRECTORY, -1, gid_base)
+            # Files inside are all group odemis, and it can be listed by anyone
+            os.chmod(model.BASE_DIRECTORY, stat.S_ISGID | stat.S_IRWXU | stat.S_IRWXG | stat.S_IROTH | stat.S_IXOTH)
+            logging.debug("created directory " + model.BASE_DIRECTORY)
+        elif not os.path.isdir(model.BASE_DIRECTORY):
+            # the unlikely case it's a file
+            logging.warning(model.BASE_DIRECTORY + " is not a directory, trying anyway...")
+
+    def handle_signal(self, signum, frame):
+        logging.warning("Received signal %d: quitting", signum)
+        self.stop()
+    
+    def terminate_all_components(self):
+        """
+        try to terminate all the components given as much as possible
+        components (set of Components): set of components to stop
+        """
+        for comp in self._components:
+            try:
+                comp.terminate()
+            except:
+                # can happen if it was already terminated 
+                logging.warning("Failed to terminate component '%s'", comp.name)
+    
+    def stop(self):
+        self.terminate_all_components()
+        if self._container:
+            self._container.terminate()
+            self._container.close()
+    
+    def run(self):
+        # parse the instantiation file 
+        try:
+            logging.debug("model instantiation file is: %s", self.model.name)
+            inst_model = modelgen.get_instantiation_model(self.model)
+            logging.info("model has been read successfully")
+        except modelgen.ParseError:
+            logging.exception("Error while parsing file %s", self.model.name)
+            return 127
+    
+        # change to odemis group and create the base directory
+        try:
+            self.set_base_group()
+        except:
+            logging.exception("Failed to get group " + model.BASE_GROUP)
+            return 127
+    
+        try:
+            self.mk_base_dir() 
+        except:
+            logging.exception("Failed to create back-end directory " + model.BASE_DIRECTORY)
+            return 127
+    
+        # create the root container
+        try:
+            # create daemon for containing the backend container
+            if self.daemon:
+                pid = os.fork()
+                if pid:
+                    logging.debug("Daemon started with pid %d", pid)
+                    return 0
+            if self.containement != BackendRunner.CONTAINER_DISABLE:
+                self._container = BackendContainer()
+        except:
+            logging.exception("Failed to create back-end container")
+            return 127
+        
+        try:
+            if self.containement == BackendRunner.CONTAINER_SEPARATED: 
+                create_sub_containers = True
+            else:
+                create_sub_containers = False
+            mic, comps, sub_containers = modelgen.instantiate_model(
+                                            inst_model, self._container, 
+                                            create_sub_containers,
+                                            dry_run=self.dry_run)
+            # update the model
+            if self._container:
+                self._container.setMicroscope(mic)
+                self._container.sub_containers |= sub_containers
+            logging.info("model has been successfully instantiated")
+            logging.debug("model microscope is %s", mic.name) 
+            logging.debug("model components are %s", ", ".join([c.name for c in comps])) 
+        except:
+            logging.exception("When instantiating file %s", self.model.name)
+            self.stop()
+            return 127
+        
+        if self.dry_run:
+            logging.info("model has been successfully validated, exiting")
+            self.stop()
+            return 0    # everything went fine
+        
+        if self.containement == BackendRunner.CONTAINER_DISABLE:
+            # in case it was not clear it's only for debug!
+            logging.warning("Going to wait for an hour and die")
+            time.sleep(3600)
+            self.stop()
+            return 0
+        
+        try:
+            logging.info("Microscope is now available in container '%s'", model.BACKEND_NAME)
+            self._container.run()
+        except:
+            # This is coming here in case of signal received when the daemon is running
+            logging.exception("When running back-end container")
+            self.stop()
+            return 127
+        
+        try:
+            self.stop()
+        except:
+            logging.exception("Failed to end the back-end container cleanly")
+            return 127
+        
+        return 0
 
 BACKEND_RUNNING = "RUNNING"
 BACKEND_DEAD = "DEAD"
@@ -140,90 +259,11 @@ status_to_xtcode = {BACKEND_RUNNING: 0,
                     BACKEND_STOPPED: 2
                     }
 
-# TODO catch kill signal
-def run_backend(model_file, daemon=False, dry_run=False):
-    
-    # parse the instantiation file 
-    try:
-        logging.debug("model instantiation file is: %s", model_file.name)
-        inst_model = modelgen.get_instantiation_model(model_file)
-        logging.info("model has been read successfully")
-    except modelgen.ParseError:
-        logging.exception("Error while parsing file %s", model_file.name)
-        return 127
 
-    # change to odemis group and create the base directory
-    try:
-        set_base_group()
-    except:
-        logging.exception("Failed to get group " + model.BASE_GROUP)
-        return 127
 
-    try:
-        mk_base_dir() 
-    except:
-        logging.exception("Failed to create back-end directory " + model.BASE_DIRECTORY)
-        return 127
-
-    # create the root container
-    try:
-        # create daemon for containing the backend container
-        if daemon:
-            pid = os.fork()
-            if pid:
-                logging.debug("Daemon started with pid %d", pid)
-                return 0
-        container = BackendContainer()
-    except:
-        logging.exception("Failed to create back-end container")
-        return 127
-    
-    try:
-        mic, comps, sub_containers = modelgen.instantiate_model(
-                                        inst_model, container, 
-                                        create_sub_containers=True,
-                                        dry_run=dry_run)
-        # update the model
-        container.setMicroscope(mic)
-        container.sub_containers |= sub_containers
-        logging.info("model has been successfully instantiated")
-        logging.debug("model microscope is %s", mic.name) 
-        logging.debug("model components are %s", ", ".join([c.name for c in comps])) 
-    except:
-        logging.exception("When instantiating file %s", model_file.name)
-        container.terminate()
-        return 127
-    
-    if dry_run:
-        logging.info("model has been successfully validated, exiting")
-        terminate_all_components(comps)
-        container.terminate()
-        return 0    # everything went fine
-    
-    try:
-        logging.info("Microscope is now available in container '%s'", model.BACKEND_NAME)
-        container.run()
-    except:
-        # This is coming here in case of signal received when the daemon is running
-        logging.exception("When running backend container")
-        terminate_all_components(comps)
-        container.terminate()
-        return 127
-    
-    try:
-        terminate_all_components(comps)
-        container.close()
-    except:
-        logging.exception("Failed to end the backend container cleanly")
-        return 127
-    
-#    dagui.main(mic)
-#    logging.warning("nothing else to do")
-    return 0
 # This is the cli interface of odemisd, which allows to start the back-end
 # It parses the command line and accordingly reads the microscope instantiation
 # file, generates a model out of it, and then provides it to the front-end 
-
 def main(args):
     """
     Contains the console handling code for the daemon
@@ -240,18 +280,20 @@ def main(args):
     dm_grp = parser.add_argument_group('Daemon management')
     dm_grpe = dm_grp.add_mutually_exclusive_group()
     dm_grpe.add_argument("--kill", "-k", dest="kill", action="store_true", default=False,
-                        help="Kill the running back-end")
+                         help="Kill the running back-end")
     dm_grpe.add_argument("--check", dest="check", action="store_true", default=False,
                         help="Check for a running back-end (only returns exit code)")
     dm_grpe.add_argument("--daemonize", "-D", action="store_true", dest="daemon",
                          default=False, help="Daemonize the back-end after startup")
     opt_grp = parser.add_argument_group('Options')
     opt_grp.add_argument('--validate', dest="validate", action="store_true", default=False,
-                        help="Validate the microscope description file and exit")
+                         help="Validate the microscope description file and exit")
+    dm_grpe.add_argument("--debug", action="store_true", dest="debug",
+                         default=False, help="Activate debug mode, where everything runs in one process")
     opt_grp.add_argument("--log-level", dest="loglev", metavar="LEVEL", type=int,
-                        default=0, help="Set verbosity level (0-2, default = 0)")
+                         default=0, help="Set verbosity level (0-2, default = 0)")
     opt_grp.add_argument("--log-target", dest="logtarget", metavar="{auto,stderr,filename}",
-                default="auto", help="Specify the log target (auto, stderr, filename)")
+                         default="auto", help="Specify the log target (auto, stderr, filename)")
     parser.add_argument("model", metavar="file.odm.yaml", nargs='?', type=open, 
                         help="Microscope model instantiation file (*.odm.yaml)")
 
@@ -308,9 +350,15 @@ def main(args):
     if options.model is None:
         logging.error("No microscope model instantiation file provided")
         return 127
-        
+    
+    if options.debug:
+        cont_pol = BackendRunner.CONTAINER_DISABLE
+    else:
+        cont_pol = BackendRunner.CONTAINER_SEPARATED
+    
     # let's become the back-end for real
-    return run_backend(options.model, options.daemon, options.validate)
+    runner = BackendRunner(options.model, options.daemon, options.validate, cont_pol)
+    return runner.run()
 
 if __name__ == '__main__':
     ret = main(sys.argv)
