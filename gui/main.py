c680a765d922898fa88bd390f8839db9b2c342f6
R. de Laat <laat@delmic.com>
2012-08-20 14:24:51 +0200

    Maximize GUI on launch

--- gui/main.py
+++ gui/main.py
@@ -19,6 +19,8 @@ import odemis.gui.main_xrc
 from odemis.gui.xmlh import odemis_get_resources
 from odemis.gui.log import log, create_gui_logger
 from odemis.gui.instrmodel import OpticalBackendConnected
+from odemis.gui.controler.settingspanel import SettingsPanel
+
 
 class OdemisGUIApp(wx.App):
     """ This is Odemis' main GUI application class
@@ -31,10 +33,6 @@ class OdemisGUIApp(wx.App):
 
         # Declare attributes BEFORE calling the super class constructor
         # because it will call 'OnInit' which uses them.
-
-        # Reference to the main application frame which provides references
-        # to screen widgets of interest.
-        self.frm = None
 
         # Startup Dialog frame
         self.dlg_startup = None
@@ -67,7 +65,7 @@ class OdemisGUIApp(wx.App):
                 sys.exit(1)
 
         # Load the main frame
-        self.frm = odemis.gui.main_xrc.xrcfr_main(None)
+        self.main_frame = odemis.gui.main_xrc.xrcfr_main(None)
 
         self.init_logger()
         self.init_gui()
@@ -78,8 +76,8 @@ class OdemisGUIApp(wx.App):
 
     def init_logger(self):
         """ Initialize logging functionality """
-        create_gui_logger(self.frm.txt_log)
-        log.info("Starting Odemis GUI version x.xx")
+        create_gui_logger(self.main_frame.txt_log)
+        log.info("Starting Odemis GUI version 0.80")
 
 
     def init_gui(self):
@@ -90,79 +88,95 @@ class OdemisGUIApp(wx.App):
             # Add frame icon
             ib = wx.IconBundle()
             ib.AddIconFromFile(os.path.join(self._module_path(), "img/odemis.ico"), wx.BITMAP_TYPE_ANY)
-            self.frm.SetIcons(ib)
+            self.main_frame.SetIcons(ib)
 
-            _, _, w, h = wx.ClientDisplayRect()
+            #log.debug("Setting frame size to %sx%s", w, h)
 
-            h -= 28
-
-            log.debug("Setting frame size to %sx%s", w, h)
-
-            self.frm.SetSize((w, h))
-            self.frm.SetPosition((0, 0))
+            #self.main_frame.SetSize((w, h))
+            #self.main_frame.SetPosition((0, 0))
 
 
-            self.tabs = [(self.frm.tab_btn_live, self.frm.pnl_tab_live),
-                         (self.frm.tab_btn_gallery, self.frm.pnl_tab_gallery),
+            self.tabs = [(self.main_frame.tab_btn_live,
+                            self.main_frame.pnl_tab_live),
+                         (self.main_frame.tab_btn_gallery,
+                            self.main_frame.pnl_tab_gallery),
                         ]
 
             for btn, _ in self.tabs:
                 btn.Bind(wx.EVT_LEFT_DOWN, self.OnTabClick)
 
             # Do a final layout of the fold panel bar
-            #wx.CallAfter(self.frm.fpb_settings.FitBar)
+            #wx.CallAfter(self.main_frame.fpb_settings.FitBar)
 
             ##################################################
             # TEST CODE
             ##################################################
             def dodo(evt):
-                from odemis.gui.comp.stream import FixedStreamPanelEntry
-                fp = FixedStreamPanelEntry(self.frm.pnl_stream,
-                                           label="First Fixed Stream")
-                self.frm.pnl_stream.add_stream(fp)
+                from odemis.gui.comp.stream import FixedStreamPanelEntry, \
+                    CustomStreamPanelEntry
+                # fp = FixedStreamPanelEntry(self.main_frame.pnl_stream,
+                #                            label="First Fixed Stream")
+                fp = CustomStreamPanelEntry(self.main_frame.pnl_stream,
+                                            label="Custom Stream")
+                self.main_frame.pnl_stream.add_stream(fp)
 
-            self.frm.btn_aquire.Bind(wx.EVT_BUTTON, dodo)
+            self.main_frame.btn_aquire.Bind(wx.EVT_BUTTON, dodo)
 
 
-            #from wx.lib.inspection import InspectionTool
-            #InspectionTool().Show()
+
 
 
             # Menu events
 
-            wx.EVT_MENU(self.frm,
-                        self.frm.menu_item_debug.GetId(),
+            wx.EVT_MENU(self.main_frame,
+                        self.main_frame.menu_item_debug.GetId(),
                         self.on_debug)
 
-            # wx.EVT_MENU(self.frm, self.frm.menu_item_exit.GetId(), self.on_close_window)
-            # wx.EVT_MENU(self.frm, self.frm.menu_item_debug.GetId(), self.on_debug)
-            # wx.EVT_MENU(self.frm, self.frm.menu_item_error.GetId(), self.on_send_report)
-            # wx.EVT_MENU(self.frm, self.frm.menu_item_activate.GetId(), self.on_activate)
-            # wx.EVT_MENU(self.frm, self.frm.menu_item_update.GetId(), elit.updater.Updater.check_for_update)
+            wx.EVT_MENU(self.main_frame,
+                        self.main_frame.menu_item_inspect.GetId(),
+                        self.on_inspect)
+
+            # wx.EVT_MENU(self.main_frame, self.main_frame.menu_item_debug.GetId(), self.on_debug)
+            # wx.EVT_MENU(self.main_frame, self.main_frame.menu_item_error.GetId(), self.on_send_report)
+            # wx.EVT_MENU(self.main_frame, self.main_frame.menu_item_activate.GetId(), self.on_activate)
+            # wx.EVT_MENU(self.main_frame, self.main_frame.menu_item_update.GetId(), elit.updater.Updater.check_for_update)
 
             # Keep track of focus
-            self.scope_panels = [self.frm.pnl_view_tl,
-                                 self.frm.pnl_view_tr,
-                                 self.frm.pnl_view_bl,
-                                 self.frm.pnl_view_br]
+            self.scope_panels = [self.main_frame.pnl_view_tl,
+                                 self.main_frame.pnl_view_tr,
+                                 self.main_frame.pnl_view_bl,
+                                 self.main_frame.pnl_view_br]
 
             for scope_panel in self.scope_panels:
                 scope_panel.Bind(wx.EVT_CHILD_FOCUS, self.OnScopePanelFocus)
 
 
-            self.frm.Bind(wx.EVT_CLOSE, self.on_close_window)
+            self.main_frame.Bind(wx.EVT_CLOSE, self.on_close_window)
 
-            self.frm.Show()
-            self.frm.Raise()
-            self.frm.Refresh()
+            self.main_frame.Maximize()
+            self.main_frame.Show()
+            #self.main_frame.Raise()
+            #self.main_frame.Refresh()
 
             if log.level == logging.DEBUG:
                 self.goto_debug_mode()
 
 
+            self.settings_controler = SettingsPanel(self.main_frame)
+
+            # Query Odemis daemon (Should move this to separate thread)
+
+            microscope = odemis.model.getMicroscope()
+
+            for comp in microscope.detectors:
+                if comp.role == 'ccd':
+                    self.settings_controler.add_ccd(comp)
+
+            #print_microscope_tree(microscope)
+
         except Exception:
             self.excepthook(*sys.exc_info())
-            raise
+            #raise
 
 
     def init_config(self):
@@ -181,7 +195,7 @@ class OdemisGUIApp(wx.App):
         if button.GetToggle():
             return
 
-        self.frm.Freeze()
+        self.main_frame.Freeze()
 
         for btn, tab in self.tabs:
             btn.SetToggle(False)
@@ -190,9 +204,9 @@ class OdemisGUIApp(wx.App):
             else:
                 tab.Hide()
 
-        self.frm.Layout()
+        self.main_frame.Layout()
 
-        self.frm.Thaw()
+        self.main_frame.Thaw()
 
         evt.Skip()
 
@@ -210,17 +224,21 @@ class OdemisGUIApp(wx.App):
     def goto_debug_mode(self):
         """ This method sets the application into debug mode, setting the
         log level and opening the log panel. """
-        self.frm.menu_item_debug.Check()
+        self.main_frame.menu_item_debug.Check()
         self.on_debug()
 
     def on_timer(self, event): #pylint: disable=W0613
         """ Timer stuff """
         pass
 
+    def on_inspect(self, evt):
+        from wx.lib.inspection import InspectionTool
+        InspectionTool().Show()
+
     def on_debug(self, evt=None): #pylint: disable=W0613
         """ Show or hides the log text field according to the debug menu item. """
-        self.frm.pnl_log.Show(self.frm.menu_item_debug.IsChecked())
-        self.frm.Layout()
+        self.main_frame.pnl_log.Show(self.main_frame.menu_item_debug.IsChecked())
+        self.main_frame.Layout()
 
     def on_close_window(self, evt=None): #pylint: disable=W0613
         """ This method cleans up and closes the Odemis GUI. """
@@ -230,7 +248,7 @@ class OdemisGUIApp(wx.App):
         # Put cleanup actions here (like disconnect from odemisd)
 
         #self.dlg_startup.Destroy()
-        self.frm.Destroy()
+        self.main_frame.Destroy()
         sys.exit(0)
 
     def excepthook(self, type, value, trace): #pylint: disable=W0622
@@ -244,11 +262,11 @@ class OdemisGUIApp(wx.App):
             # TODO: create custom dialogs for Odemis
 
             # msg = "Unexpected error!"
-            # answer = elit.dialog.error_report_dialog(self.frm, msg, 'Onverwachte fout!')
+            # answer = elit.dialog.error_report_dialog(self.main_frame, msg, 'Onverwachte fout!')
             # if  answer == wx.ID_YES:
             #     try:
             #         print "Sending error report"
-            #         self.frm.Hide()
+            #         self.main_frame.Hide()
             #         elit.util.report_error()
             #         print "Error report sent"
             #     except: #pylint: disable=W0702
