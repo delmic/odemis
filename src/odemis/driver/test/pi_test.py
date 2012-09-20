fb8013a52ece66614ffc6f964a0603915fe2dbdd
Éric Piel <piel@delmic.com>
2012-09-20 17:44:35 +0200

    driver pi: fix import need after file layout change

--- src/odemis/driver/test/pi_test.py
+++ src/odemis/driver/test/pi_test.py
@@ -17,7 +17,7 @@ Odemis is distributed in the hope that it will be useful, but WITHOUT ANY WARRAN
 You should have received a copy of the GNU General Public License along with Odemis. If not, see http://www.gnu.org/licenses/.
 '''
 from concurrent import futures
-from driver import pi
+from odemis.driver import pi
 import logging
 import math
 import os
