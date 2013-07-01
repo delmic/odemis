d0aa8cbda891de5602ffaa85dad5b33db0863aea
Éric Piel <piel@delmic.com>
2013-07-01 10:07:22 +0200

    util: add test to conversion
    also improve readability of wave2rgb

--- src/odemis/gui/util/conversion.py
+++ src/odemis/gui/util/conversion.py
@@ -31,43 +31,39 @@ def wave2rgb(wavelength):
     wavelength (0<float): wavelength in m
     return (3-tupe int in 0..255): RGB value
     """
-
     w = wavelength * 1e9
     # outside of the visible spectrum, use fixed colour
-    if w < 350: # ultraviolet
-        w = 350
-    elif w > 780: # infrared
-        w = 780
+    w = min(max(w, 350), 780)
 
     # colour
-    if w >= 350 and w < 440:
+    if 350 <= w < 440:
         r = -(w - 440) / (440 - 350)
         g = 0
         b = 1
-    elif w >= 440 and w < 490:
+    elif 440 <= w < 490:
         r = 0
         g = (w - 440) / (490 - 440)
         b = 1
-    elif w >= 490 and w < 510:
+    elif 490 <= w < 510:
         r = 0
         g = 1
         b = -(w - 510) / (510 - 490)
-    elif w >= 510 and w < 580:
+    elif 510 <= w < 580:
         r = (w - 510) / (580 - 510)
         g = 1
         b = 0
-    elif w >= 580 and w < 645:
+    elif 580 <= w < 645:
         r = 1
         g = -(w - 645) / (645 - 580)
         b = 0
-    elif w >= 645 and w <= 780:
+    elif 645 <= w <= 780:
         r = 1
         g = 0
         b = 0
     else:
         logging.warning("Unable to compute RGB for wavelength %d", w)
 
-    return int(255 * r), int(255 * g), int(255 * b)
+    return int(round(255 * r)), int(round(255 * g)), int(round(255 * b))
 
 def hex_to_rgb(hex_str):
     """
