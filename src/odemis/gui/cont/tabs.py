42e7c3a780b05ed17f498b532df8778399ecab44
Éric Piel <piel@delmic.com>
2013-09-18 09:58:10 +0200

    gui lens align: fix conversion AB to XY
    after we changed to the convention that Y is going up, this has to change too.

--- src/odemis/gui/cont/tabs.py
+++ src/odemis/gui/cont/tabs.py
@@ -706,10 +706,10 @@ class LensAlignTab(Tab):
 
         main_frame.vp_align_sem.ShowLegend(False)
 
-        # See axes convention: A/B are 135° from X/Y
+        # See axes convention: A/B are 135° from Y/X
         self._stage_ab = InclinedStage("converter-ab", "stage",
                                        children={"aligner": main_data.aligner},
-                                       axes=["a", "b"],
+                                       axes=["b", "a"],
                                        angle=135)
         # vp_align_sem is connected to the stage
         vpv = collections.OrderedDict([
