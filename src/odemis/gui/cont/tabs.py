b06ed92daf6202554054713b36bfa32f6340f480
Éric Piel <piel@delmic.com>
2013-09-24 12:03:42 +0200

    gui lens align: fix A/B move computation
    according to theoritical axis convention.
    Still need a real trial to check it's really correct.

--- src/odemis/gui/cont/tabs.py
+++ src/odemis/gui/cont/tabs.py
@@ -986,8 +986,8 @@ class LensAlignTab(Tab):
                   fov_size[1] * ((t + b) / 2 - 0.5))
 
         # same formula as InclinedStage._convertPosToChild()
-        ang = math.radians(135)
-        bc, ac = [xc * math.cos(ang) - yc * math.sin(ang),
+        ang = math.radians(-135)
+        ac, bc = [xc * math.cos(ang) - yc * math.sin(ang),
                   xc * math.sin(ang) + yc * math.cos(ang)]
 
         # Force values to 0 if very close to it (happens often as can be on just
