80c4ba1ca9919944d69e6622aa47d4b1b9d558a4
ktsitsikas <ktsitsikas@gmail.com>
2013-11-29 14:18:47 +0100

    SpotCoordinates test module

---
+++ src/odemis/acq/align/test/coordinates_test.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 28 Nov 2013
+
+@author: Kimon Tsitsikas
+
+Copyright © 2012-2013 Kimon Tsitsikas, Delmic
+
+This file is part of Odemis.
+
+Odemis is free software: you can redistribute it and/or modify it under the terms 
+of the GNU General Public License version 2 as published by the Free Software 
+Foundation.
+
+Odemis is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with 
+Odemis. If not, see http://www.gnu.org/licenses/.
+'''
+import logging
+import numpy
+import unittest
+
+from numpy import genfromtxt
+from odemis import model
+from odemis.dataio import hdf5
+from odemis.acq.align import coordinates
+
+logging.getLogger().setLevel(logging.DEBUG)
+
+class TestSpotCoordinates(unittest.TestCase):
+    """
+    Test SpotCoordinates functions
+    """
+    def test_find_center(self):
+        """
+        Test FindCenterCoordinates
+        """
+        data = hdf5.read_data("single_part.h5")
+        C, T, Z, Y, X = data[0].shape
+        data[0].shape = Y, X
+        subimages = []
+        subimages.append(model.DataArray(data[0]))
+
+        spot_coordinates = coordinates.FindCenterCoordinates(subimages)
