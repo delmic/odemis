7db88f11d2bbd550dcd50059975ffa91829d8b45
ktsitsikas <ktsitsikas@gmail.com>
2013-12-03 16:23:41 +0100

    Tests added

--- src/odemis/acq/align/test/coordinates_test.py
+++ src/odemis/acq/align/test/coordinates_test.py
@@ -21,10 +21,13 @@ Odemis. If not, see http://www.gnu.org/licenses/.
 '''
 import logging
 import unittest
+import numpy
+import csv
 
 from odemis import model
 from odemis.dataio import hdf5
 from odemis.acq.align import coordinates
+from scipy import misc
 
 logging.getLogger().setLevel(logging.DEBUG)
 
@@ -32,14 +35,107 @@ class TestSpotCoordinates(unittest.TestCase):
     """
     Test SpotCoordinates functions
     """
+    @unittest.skip("skip")
     def test_find_center(self):
         """
         Test FindCenterCoordinates
         """
-        data = hdf5.read_data("single_part.h5")
-        C, T, Z, Y, X = data[0].shape
-        data[0].shape = Y, X
+        data1 = numpy.genfromtxt('image1.csv', delimiter=',')
+        data2 = numpy.genfromtxt('image2.csv', delimiter=',')
+        data3 = numpy.genfromtxt('image3.csv', delimiter=',')
+        data4 = numpy.genfromtxt('image4.csv', delimiter=',')
+        data5 = numpy.genfromtxt('image5.csv', delimiter=',')
+        data6 = numpy.genfromtxt('image6.csv', delimiter=',')
+        data7 = numpy.genfromtxt('image7.csv', delimiter=',')
+        data8 = numpy.genfromtxt('image8.csv', delimiter=',')
+        data9 = numpy.genfromtxt('image9.csv', delimiter=',')
+        data10 = numpy.genfromtxt('image10.csv', delimiter=',')
+
         subimages = []
-        subimages.append(model.DataArray(data[0]))
+        subimages.append(model.DataArray(data1))
+        subimages.append(model.DataArray(data2))
+        subimages.append(model.DataArray(data3))
+        subimages.append(model.DataArray(data4))
+        subimages.append(model.DataArray(data5))
+        subimages.append(model.DataArray(data6))
+        subimages.append(model.DataArray(data7))
+        subimages.append(model.DataArray(data8))
+        subimages.append(model.DataArray(data9))
+        subimages.append(model.DataArray(data10))
 
         spot_coordinates = coordinates.FindCenterCoordinates(subimages)
+        expected_coordinates = [(4.9998, 4.9768), (5.4181, 4.2244), (15.5542, 15.4534),
+                                (8.1512, 8.2081), (5.1537, 4.9287), (5.2221, 5.0449),
+                                (4.1433, 6.7063), (6.4313, 7.2690), (4.9355, 5.1400), (5.0209, 4.9929)]
+        numpy.testing.assert_almost_equal(spot_coordinates, expected_coordinates, 3)
+
+    @unittest.skip("skip")
+    def test_devide_neighborhoods_spot(self):
+        """
+        Test DivideInNeighborhoods for one spot image
+        """
+        spot_image = numpy.zeros(shape=(256, 256))
+        spot_image[112:120, 114:122].fill(255)
+
+        subimages, subimage_coordinates = coordinates.DivideInNeighborhoods(spot_image, (1, 1))
+        self.assertEqual(subimages.__len__(), 1)
+
+    @unittest.skip("skip")
+    def test_devide_neighborhoods_grid(self):
+        """
+        Test DivideInNeighborhoods for 3x3 grid
+        """
+        grid_image = numpy.zeros(shape=(256, 256))
+        x_start, y_start = 70, 64
+        for x in xrange(3):
+            y_start_in = y_start
+            for y in xrange(3):
+                grid_image[x_start:x_start + 8, y_start_in:y_start_in + 8].fill(255)
+                y_start_in = y_start_in + 40
+            x_start = x_start + 40
+
+        subimages, subimage_coordinates = coordinates.DivideInNeighborhoods(grid_image, (3, 3))
+        self.assertEqual(subimages.__len__(), 9)
+
+    @unittest.skip("skip")
+    def test_devide_neighborhoods_real_sample(self):
+        """
+        Test DivideInNeighborhoods for one spot real image
+        """
+        spot_image = hdf5.read_data("single_part.h5")
+        C, T, Z, Y, X = spot_image[0].shape
+        spot_image[0].shape = Y, X
+
+        subimages, subimage_coordinates = coordinates.DivideInNeighborhoods(spot_image[0], (1, 1))
+        self.assertEqual(subimages.__len__(), 1)
+
+    @unittest.skip("skip")
+    def test_devide_and_find_center_spot(self):
+        """
+        Test DivideInNeighborhoods combined with FindCenterCoordinates
+        """
+        spot_image = hdf5.read_data("single_part.h5")
+        C, T, Z, Y, X = spot_image[0].shape
+        spot_image[0].shape = Y, X
+
+        subimages, subimage_coordinates = coordinates.DivideInNeighborhoods(spot_image[0], (1, 1))
+        # hdf5.export("subimage.h5", subimages[0], thumbnail=None)
+        spot_coordinates = coordinates.FindCenterCoordinates(subimages)
+        # print spot_coordinates
+
+    def test_devide_and_find_center_grid(self):
+        """
+        Test DivideInNeighborhoods combined with FindCenterCoordinates
+        """
+        grid_data = numpy.genfromtxt('grid4.csv', delimiter=',')
+
+        subimages, subimage_coordinates = coordinates.DivideInNeighborhoods(grid_data, (10, 10))
+        print subimage_coordinates.__len__()
+
+        hdf5.export("subimage.h5", model.DataArray(subimages[1]), thumbnail=None)
+        spot_coordinates = coordinates.FindCenterCoordinates(subimages)
+        optical_coordinates = coordinates.ReconstructImage(subimage_coordinates, spot_coordinates)
+        # a = numpy.array([tuple(i) for i in optical_coordinates], dtype=(float, 2))
+        # numpy.savetxt("optical_coordinates.csv", a, delimiter=",")
+        print optical_coordinates
+
