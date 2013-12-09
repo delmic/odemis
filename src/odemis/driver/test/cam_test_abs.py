f8c496921ae25a9295e52debbde40e5f8f73ba8f
Éric Piel <piel@delmic.com>
2013-12-09 17:38:35 +0100

    driver: refactor camera test cases to share code
    andorcam2 still passes... at least with the fake camera

--- src/odemis/driver/test/cam_test_abs.py
+++ src/odemis/driver/test/cam_test_abs.py
@@ -23,11 +23,26 @@ Odemis. If not, see http://www.gnu.org/licenses/.
 # This is not a real test case, but just a stub to be used for each camera driver.
 
 from abc import ABCMeta, abstractproperty
-from odemis import model
 import gc
+import logging
+import numpy
+from odemis import model
+from odemis.driver import semcomedi
 import time
 import unittest
+
+
 #gc.set_debug(gc.DEBUG_LEAK | gc.DEBUG_STATS)
+# arguments used for the creation of the SEM simulator
+# Note that you need to run this line after a boot, for the simulator to work:
+# sudo comedi_config /dev/comedi0 comedi_test 1000000,1000000
+CONFIG_SED = {"name": "sed", "role": "sed", "channel":5, "limits": [-3, 3]}
+CONFIG_SCANNER = {"name": "scanner", "role": "ebeam", "limits": [[0, 5], [0, 5]],
+                  "channels": [0, 1], "settle_time": 10e-6, "hfw_nomag": 10e-3}
+CONFIG_SEM = {"name": "sem", "role": "sem", "device": "/dev/comedi0",
+              "children": {"detector0": CONFIG_SED, "scanner": CONFIG_SCANNER}
+              }
+
 
 class VirtualStaticTestCam(object):
     """
@@ -74,16 +89,14 @@ class VirtualTestCam(object):
     def camera_kwargs(self):
         pass
     
-    camera = None
-    
-#    # doesn't work as it's not a TestCase
-#    @classmethod
-#    def setUpClass(cls):
-#        cls.camera = cls.camera_type(**cls.camera_kwargs)
-#    
-#    @classmethod
-#    def tearDownClass(cls):
-#        cls.camera.terminate()
+    # These need to be called explicitly from the child as it's not a TestCase
+    @classmethod
+    def setUpClass(cls):
+        cls.camera = cls.camera_type(**cls.camera_kwargs)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.camera.terminate()
  
     def setUp(self):
         # reset size and binning
@@ -375,4 +388,118 @@ class VirtualTestCam(object):
             self.fail("Negative exposure time should fail")
         except:
             pass # good!
+
+
+class VirtualTestSynchronized(object):
+    """
+    Test the synchronizedOn(Event) interface, using the fake SEM
+    """
+    __metaclass__ = ABCMeta
+
+    # needs:
+    # camera_type : class of the camera
+    # camera_kwargs : dict of arguments to create a camera
+    @abstractproperty
+    def camera_type(self):
+        pass
+
+    @abstractproperty
+    def camera_kwargs(self):
+        pass
+
+    @classmethod
+    def setUpClass(cls):
+        cls.ccd = cls.camera_type(**cls.camera_kwargs)
+        cls.sem = semcomedi.SEMComedi(**CONFIG_SEM)
+
+        for child in cls.sem.children:
+            if child.name == CONFIG_SED["name"]:
+                cls.sed = child
+            elif child.name == CONFIG_SCANNER["name"]:
+                cls.scanner = child
+
+    @classmethod
+    def tearUpClass(cls):
+        cls.ccd.terminate()
+        cls.sem.terminate()
+
+    def setUp(self):
+        pass
+
+    def tearUp(self):
+        # just in case it failed
+        self.ccd.data.subscribe(self.receive_ccd_image)
+        self.sed.data.subscribe(self.receive_sem_data)
+
+    def test_basic(self):
+        """
+        check the synchronization of the SEM with the CCD:
+        The SEM scans a region and for each point, the CCD acquires one image.
+        """
+        start = time.time()
+        # use large binning, to reduce the resolution
+        self.ccd.binning.value = (4, self.ccd.binning.range[1][1])
+
+        exp = 50e-3 #s
+        # in practice, it takes up to 500ms to take an image of 50 ms exposure
+        self.sem_size = (10, 10)
+        self.ccd_size = self.ccd.resolution.value
+        numbert = numpy.prod(self.sem_size)
+
+        self.ccd.exposureTime.value = exp
+        # magical formula to get a long enough dwell time.
+        # works with PVCam and Andorcam, but is probably different with other drivers :-(
+        readout = numpy.prod(self.ccd_size) / self.ccd.readoutRate.value
+        # it seems with the iVac, 20ms is enough to account for the overhead and extra image acquisition
+        self.scanner.dwellTime.value = (exp + readout) * 1.1 + 0.05
+        self.scanner.resolution.value = self.sem_size
+        # pixel write/read setup is pretty expensive ~10ms
+        expected_duration = numbert * (self.scanner.dwellTime.value + 0.01)
+
+        self.sem_left = 1 # unsubscribe just after one
+        self.ccd_left = numbert # unsubscribe after receiving
+
+        self.ccd.data.synchronizedOn(self.scanner.newPosition)
+        self.ccd.data.subscribe(self.receive_ccd_image)
+
+        self.sed.data.subscribe(self.receive_sem_data)
+        for i in range(10):
+            # * 3 because it can be quite long to setup each pixel.
+            time.sleep(expected_duration * 2 / 10)
+            if self.sem_left == 0:
+                break # just to make it quicker if it's quicker
+
+        logging.info("Took %g s", self.end_time - start)
+        time.sleep(exp + readout)
+        self.assertEqual(self.sem_left, 0)
+        self.assertEqual(self.ccd_left, 0)
+        self.ccd.data.synchronizedOn(None)
+
+        # check we can still get data normally
+        d = self.ccd.data.get()
+
+        time.sleep(0.1)
+
+    # TODO: test .softwareTrigger event
+
+
+    def receive_sem_data(self, dataflow, image):
+        """
+        callback for SEM df
+        """
+        self.assertEqual(image.shape, self.sem_size[::-1])
+        self.assertIn(model.MD_DWELL_TIME, image.metadata)
+        self.sem_left -= 1
+        if self.sem_left <= 0:
+            dataflow.unsubscribe(self.receive_sem_data)
+
+    def receive_ccd_image(self, dataflow, image):
+        """
+        callback for CCD
+        """
+        self.assertEqual(image.shape, self.ccd_size[::-1])
+        self.ccd_left -= 1
+        if self.ccd_left <= 0:
+            dataflow.unsubscribe(self.receive_ccd_image)
+            self.end_time = time.time()
 
