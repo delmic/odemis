bc767ec812b52b7b2ea85562f9d4016c8f836726
Éric Piel <piel@delmic.com>
2013-12-13 11:50:00 +0100

    gui acq: save file in a separate thread after acquisition.
    This allows to not block the whole GUI while the file is being saved.

--- src/odemis/gui/cont/acquisition.py
+++ src/odemis/gui/cont/acquisition.py
@@ -26,7 +26,10 @@ This module contains classes to control the actions related to the acquisition
 of microscope images.
 """
 
+from concurrent import futures
 from concurrent.futures._base import CancelledError
+import logging
+import math
 from odemis import model, dataio
 from odemis.gui import acqmng, conf
 from odemis.gui.acqmng import preset_as_is
@@ -36,9 +39,6 @@ from odemis.gui.util import img, get_picture_folder, call_after, units, \
     limit_invocation
 from odemis.gui.win.acquisition import AcquisitionDialog, \
     ShowAcquisitionFileDialog
-from wx.lib.pubsub import pub
-import logging
-import math
 import os
 import re
 import subprocess
@@ -46,6 +46,7 @@ import sys
 import threading
 import time
 import wx
+from wx.lib.pubsub import pub
 
 
 class AcquisitionController(object):
@@ -389,6 +390,8 @@ class SparcAcquiController(AcquisitionController):
         self.gauge_acq = self._main_frame.gauge_sparc_acq
         self.lbl_acqestimate = self._main_frame.lbl_sparc_acq_estimate
 
+        self._executor = futures.ThreadPoolExecutor(max_workers=2)
+
         # TODO: only if the current tab is acquisition?
         # Link snapshot menu to snapshot action
         # Note: On the Sparc, there is only one view, the SEM (complete ROI). So
@@ -513,6 +516,26 @@ class SparcAcquiController(AcquisitionController):
         # Make sure that the acquisition button is enabled again.
         self._main_frame.btn_sparc_acquire.Enable()
 
+    def _reset_acquisition_gui(self, text=None, keep_filename=False):
+        """
+        Set back every GUI elements to be ready for the next acquisition
+        text (None or str): a (error) message to display instead of the
+          estimated acquisition time
+        keep_filename (bool): if True, will not update the filename
+        """
+        self.btn_cancel.Hide()
+        self.btn_acquire.Enable()
+        self._main_frame.Layout()
+        self._resume_settings()
+
+        if not keep_filename:
+            # change filename, to ensure not overwriting anything
+            self.filename.value = self._get_default_filename()
+
+        if text is not None:
+            self.lbl_acqestimate.SetLabel(text)
+        else:
+            self.update_acquisition_time()          
 
     def _show_acquisition(self, data, acqfile):
         """
@@ -523,7 +546,6 @@ class SparcAcquiController(AcquisitionController):
         # get the analysis tab
         mtc = get_main_tab_controller()
         analysis_tab = mtc['analysis']
-
         analysis_tab.display_new_data(acqfile.name, data)
 
         # show the new tab
@@ -568,63 +590,48 @@ class SparcAcquiController(AcquisitionController):
         self.acq_future.cancel()
         # all the rest will be handled by on_acquisition_done()
 
+    def _export_to_file(self, acq_future):
+        """
+        return (list of DataArray, filename): data exported and filename
+        """
+        st = self._tab_data_model.acquisitionView.stream_tree
+        thumb = acqmng.computeThumbnail(st, acq_future)
+        data = acq_future.result()
+        filename = self.filename.value
+        exporter = dataio.get_exporter(self.conf.last_format)
+        exporter.export(filename, data, thumb)
+        logging.info("Acquisition saved as file '%s'.", filename)
+        return data, filename
+
     @call_after
     def on_acquisition_done(self, future):
         """
         Callback called when the acquisition is finished (either successfully or
         cancelled)
         """
-        new_lbl = None # If a string, will use it instead of the acquisition time
+        self.btn_cancel.Disable()
         try:
-            try:
-                data = future.result(1) # timeout is just for safety
-                # make sure the progress bar is at 100%
-                self.gauge_acq.Value = self.gauge_acq.Range
-            except CancelledError:
-                # hide progress bar (+ put pack estimated time)
-                self.gauge_acq.Hide()
-                self.btn_cancel.Hide()
-                # don't change filename => we can reuse it
-                return
-            except Exception:
-                # We cannot do much: just warn the user and pretend it was cancelled
-                logging.exception("Acquisition failed")
-                new_lbl = "Acquisition failed."
-                # leave the gauge, to give a hint on what went wrong.
-                self.btn_cancel.Disable()
-                return
-
-            # hide progress bar
+            future.result()
+        except CancelledError:
+            # hide progress bar (+ put pack estimated time)
             self.gauge_acq.Hide()
-            self.btn_cancel.Hide()
+            # don't change filename => we can reuse it
+            self._reset_acquisition_gui(keep_filename=True)
+            return
+        except Exception:
+            # leave the gauge, to give a hint on what went wrong.
+            logging.exception("Acquisition failed")
+            self._reset_acquisition_gui("Acquisition failed.")
+            return
 
-            # TODO: on big acquisitions, it can take 20s => put in a thread + future
-            # save result to file
-            try:
-                st = self._tab_data_model.acquisitionView.stream_tree
-                thumb = acqmng.computeThumbnail(st, future)
-                filename = self.filename.value
-                exporter = dataio.get_exporter(self.conf.last_format)
-                exporter.export(filename, data, thumb)
-                logging.info("Acquisition saved as file '%s'.", filename)
-            except Exception:
-                logging.exception("Saving acquisition failed")
-                new_lbl = "Saving acquisition file failed"
-                return
+        # make sure the progress bar is at 100%
+        self.gauge_acq.Value = self.gauge_acq.Range
 
-            # change filename, to ensure not overwriting anything
-            self.filename.value = self._get_default_filename()
-
-            # display in the analysis tab
-            self._show_acquisition(data, open(filename))
-        finally:
-            self.btn_acquire.Enable()
-            self._main_frame.Layout()
-            self._resume_settings()
-            if new_lbl is not None:
-                self.lbl_acqestimate.SetLabel(new_lbl)
-            else:
-                self.update_acquisition_time()
+        # save result to file
+        self.lbl_acqestimate.SetLabel("Saving file...")
+        # TODO: on big acquisitions, it can take 20s => put in a thread + future
+        sf = self._executor.submit(self._export_to_file, future)
+        sf.add_done_callback(self.on_file_export_done)
 
     @call_after
     def on_acquisition_upd(self, future, past, left):
@@ -661,4 +668,22 @@ class SparcAcquiController(AcquisitionController):
             # don't be too precise
             self.lbl_acqestimate.SetLabel("a few seconds left.")
 
+    @call_after
+    def on_file_export_done(self, future):
+        """
+        Callback called when the acquisition is finished (either successfully or
+        cancelled)
+        """
+        # hide progress bar
+        self.gauge_acq.Hide()
 
+        try:
+            data, filename = future.result()
+        except Exception:
+            logging.exception("Saving acquisition failed")
+            self._reset_acquisition_gui("Saving acquisition file failed")
+            return
+
+        # display in the analysis tab
+        self._show_acquisition(data, open(filename))
+        self._reset_acquisition_gui()
