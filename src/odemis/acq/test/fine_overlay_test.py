52038b65d871e109ee0cf76a963d1489c6692a9f
ktsitsikas <ktsitsikas@gmail.com>
2013-12-20 13:13:49 +0100

    Report creation

--- src/odemis/acq/test/fine_overlay_test.py
+++ src/odemis/acq/test/fine_overlay_test.py
@@ -25,7 +25,7 @@ import unittest
 from odemis import model
 from odemis.acq import fine_overlay
 
-logging.getLogger().setLevel(logging.DEBUG)
+# logging.getLogger().setLevel(logging.DEBUG)
 
 class TestFineOverlay(unittest.TestCase):
     """
@@ -51,4 +51,31 @@ class TestFineOverlay(unittest.TestCase):
             raise KeyError("Not all components found")
 
         (calc_translation_x, calc_translation_y), calc_scaling, calc_rotation = fine_overlay.DoFineOverlay((9, 9), 1e-06, 1e-07, escan, ccd, detector)
-        print (calc_translation_x, calc_translation_y), calc_scaling, calc_rotation
+        numpy.testing.assert_almost_equal((calc_translation_x, calc_translation_y, calc_scaling, calc_rotation), (-280.91827079065121, -195.55748765461769, 13.9363892133, -1.47833441067), 1)
+
+    def test_do_fine_overlay_failure(self):
+        """
+        Test DoFineOverlay failure due to low maximum allowed difference
+        """
+        escan = None
+        detector = None
+        ccd = None
+        # find components by their role
+        for c in model.getComponents():
+            if c.role == "e-beam":
+                escan = c
+            elif c.role == "se-detector":
+                detector = c
+            elif c.role == "ccd":
+                ccd = c
+        if not all([escan, detector, ccd]):
+            logging.error("Failed to find all the components")
+            raise KeyError("Not all components found")
+
+        (calc_translation_x, calc_translation_y), calc_scaling, calc_rotation = fine_overlay.DoFineOverlay((9, 9), 1e-06, 1e-08, escan, ccd, detector)
+        numpy.testing.assert_equal(((calc_translation_x, calc_translation_y), calc_scaling, calc_rotation), ((None, None), None, None))
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestFineOverlay)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
