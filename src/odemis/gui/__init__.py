d373de7b7b171defd2713c7beb7e5ce53cdac428
R. de Laat <laat@delmic.com>
2014-01-28 08:22:06 +0100

    gui: Small colour update

--- src/odemis/gui/__init__.py
+++ src/odemis/gui/__init__.py
@@ -10,7 +10,7 @@ STREAM_BACKGROUND_COLOUR = "#4D4D4D"    # Stream panel background
 FOREGROUND_COLOUR = "#DDDDDD"           # Default foreground colour
 FOREGROUND_COLOUR_DIS = "#666666"       # Disabled foreground colour
 FOREGROUND_COLOUR_EDIT = "#2FA7D4"      # Edit colour
-FOREGROUND_COLOUR_2ND = "#A053D8"       # Secundary edit colour
+FOREGROUND_COLOUR_2ND = "#53D8AD"       # Secundary edit colour
 FOREGROUND_COLOUR_HIGHLIGHT = "#FFA300" # Highlight colour
 
 # Border colours for the viewports
