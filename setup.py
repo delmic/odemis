b0bdd022babec4fef092fd94984eac7eba07bb25
Éric Piel <piel@delmic.com>
2014-04-14 16:17:17 +0200

    config: clean up config files
    remove some non useful config files
    move the config files specific to some sites to the examples/ directory.
    This should make it clear they should not be used as-is, but first copied.
    The main problem of using them as-is is that the odemis debian package will update them
    silently on the next version.

--- setup.py
+++ setup.py
@@ -66,6 +66,7 @@ if sys.platform.startswith('linux'):
                   # TODO udev rules might actually be better off in /lib/udev/rules.d/
                   ('/lib/udev/rules.d', glob.glob('install/linux/lib/udev/rules.d/*.rules')),
                   ('share/odemis/', glob.glob('install/linux/usr/share/odemis/*.odm.yaml')),
+                  ('share/odemis/examples', glob.glob('install/linux/usr/share/odemis/examples/*.odm.yaml')),
                   # TODO: need to run desktop-file-install in addition to update-desktop-database?
                   ('share/applications/', glob.glob('install/linux/usr/share/applications/*.desktop')),
                   ('share/icons/hicolor/32x32/apps/', ['install/linux/usr/share/icons/hicolor/32x32/apps/odemis.png']),
