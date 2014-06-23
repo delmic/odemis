343aab7191ba3d9888016c5bae43c5f703795fed
Éric Piel <piel@delmic.com>
2014-06-23 14:47:35 +0200

    util img_fast: small optimisation if tint and many white or black pixels
    multiplication can be skipped in these cases. It saves 50% time for each extreme pixel.

--- src/odemis/util/img_fast.pyx
+++ src/odemis/util/img_fast.pyx
@@ -26,6 +26,7 @@ cimport numpy
 # TODO: use fused types to support multiple types (but needs Cython >= 0.20)
 ctypedef numpy.uint16_t uint16_t
 
+# nogil allows multi-threading but prevents use of any Python objects or call
 @cython.cdivision(True)
 cdef void cDataArray2RGB(uint16_t* data, int datalen, uint16_t irange0, uint16_t irange1,
                     int* tint, numpy.uint8_t* ret) nogil:
@@ -33,11 +34,11 @@ cdef void cDataArray2RGB(uint16_t* data, int datalen, uint16_t irange0, uint16_t
     cdef double br = (b * <double>tint[0]) / 255.
     cdef double bg = (b * <double>tint[1]) / 255.
     cdef double bb = (b * <double>tint[2]) / 255.
-    
+
     cdef numpy.uint8_t di
     cdef double df
     cdef int retpos=0
-    
+
     if tint[0] == tint[1] == tint[2] == 255:
         # optimised version, without tinting (about 2x faster)
         for i in range(datalen):
@@ -58,17 +59,27 @@ cdef void cDataArray2RGB(uint16_t* data, int datalen, uint16_t irange0, uint16_t
         for i in range(datalen):
             # clip
             if data[i] <= irange0:
-                df = 0
+                ret[retpos] = 0
+                retpos += 1
+                ret[retpos] = 0
+                retpos += 1
+                ret[retpos] = 0
+                retpos += 1
             elif data[i] >= irange1:
-                df = irange1 - irange0
+                ret[retpos] = tint[0]
+                retpos += 1
+                ret[retpos] = tint[1]
+                retpos += 1
+                ret[retpos] = tint[2]
+                retpos += 1
             else:
                 df = (data[i] - irange0) 
-            ret[retpos] = <numpy.uint8_t> (df * br)
-            retpos += 1
-            ret[retpos] = <numpy.uint8_t> (df * bg)
-            retpos += 1
-            ret[retpos] = <numpy.uint8_t> (df * bb)
-            retpos += 1
+                ret[retpos] = <numpy.uint8_t> (df * br)
+                retpos += 1
+                ret[retpos] = <numpy.uint8_t> (df * bg)
+                retpos += 1
+                ret[retpos] = <numpy.uint8_t> (df * bb)
+                retpos += 1
 
 # This function is probably not needed, but I have no idea how to instantiate 
 # a numpy array which can be passed as a pointer
@@ -78,16 +89,13 @@ def wrapDataArray2RGB(numpy.ndarray[uint16_t, ndim=2] data not None,
                   irange,
                   tint,
                   numpy.ndarray[numpy.uint8_t, ndim=3] ret not None):
-    w = data.shape[0]
-    h = data.shape[1]
     cdef int ctint[3]
     ctint[0] = tint[0]
     ctint[1] = tint[1]
     ctint[2] = tint[2]
-    cDataArray2RGB(&data[0,0], w * h, irange[0], irange[1], ctint, &ret[0,0,0])
+    cDataArray2RGB(&data[0,0], data.size, irange[0], irange[1], ctint, &ret[0,0,0])
 
-def DataArray2RGB(data,
-                  irange, tint=(255, 255, 255)):
+def DataArray2RGB(data, irange, tint=(255, 255, 255)):
     if not data.flags.c_contiguous:
         raise ValueError("Optimised version only works with C-contiguous arrays")
     # Note: we could also make an optimised version for F-contiguous arrays, 
