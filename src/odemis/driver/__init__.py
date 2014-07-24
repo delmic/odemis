bba73c591cec720ca4683912ce403e9c810f243d
Éric Piel <piel@delmic.com>
2014-07-24 15:06:35 +0200

    driver: add new drivers to __all__
    Allows to scan the new type of hardware.

--- src/odemis/driver/__init__.py
+++ src/odemis/driver/__init__.py
@@ -20,5 +20,6 @@ You should have received a copy of the GNU General Public License along with
 Odemis. If not, see http://www.gnu.org/licenses/.
 '''
 # in case it's need to find all the devices supported (e.g., for a scan)
-__all__ = ["andorcam3", "andorcam2", "pi", "pigcs", "lle", "simulated", 
-           "semcomedi", "spectrapro", "pvcam", "omicronxx", "tlfw"]
+__all__ = ["andorcam3", "andorcam2", "andorshrk", "pi", "pigcs", "lle", "simulated",
+           "semcomedi", "spectrapro", "pvcam", "omicronxx", "tlfw", "tlaptmf",
+           "tmcm", "phenom", "tescan_sem", "simcam", "simsem"]
