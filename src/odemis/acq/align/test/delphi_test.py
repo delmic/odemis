e6cb075176ad64b060fa5f55cce354c4f7744418
Kimon Tsitsikas <tsitsikas@delmic.com>
2014-09-12 11:09:13 +0200

    Do not move stage in lens alignment

--- src/odemis/acq/align/test/delphi_test.py
+++ src/odemis/acq/align/test/delphi_test.py
@@ -104,6 +104,19 @@ class TestCalibration(unittest.TestCase):
         expected_coordinates = (390.5, 258.5)
         numpy.testing.assert_almost_equal(hole_coordinates, expected_coordinates)
 
+
+    # @unittest.skip("skip")
+    def test_find_lens_center(self):
+        """
+        Test FindCircleCenter for lenses
+        """
+        data = hdf5.read_data("navcam-calib2.h5")
+        Z, Y, X = data[0].shape
+
+        lens_coordinates = delphi.FindCircleCenter(data[0][0], delphi.LENS_RADIUS, 6)
+        expected_coordinates = (450.5, 445.5)
+        numpy.testing.assert_almost_equal(lens_coordinates, expected_coordinates)
+
     # @unittest.skip("skip")
     def test_find_lens_center(self):
         """
