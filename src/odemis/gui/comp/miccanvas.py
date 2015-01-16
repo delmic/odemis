a18aeffed71022ee496007e0308a60cac6a32ae4
Éric Piel <piel@delmic.com>
2015-01-16 15:24:31 +0100

    gui canvas: make the focus even less sensitive
    After updating the DELPHI focus to the real distance, it appears the focus is still too sensitive.
    
    So divide sensitivity by 2 again.
    
    Now we might have not enough sensitivity for the SEM focus... really need an adaptive sensitivity.

--- src/odemis/gui/comp/miccanvas.py
+++ src/odemis/gui/comp/miccanvas.py
@@ -627,13 +627,13 @@ class DblMicroscopeCanvas(canvas.DraggableCanvas):
         """
 
         if axis == 1 and self.microscope_view.focus is not None:
-            # conversion: 1 unit => 0.01 μm (so a whole screen, ~44000u, is a
+            # conversion: 1 unit => 5 nm (so a whole screen, ~44000u, is a
             # less than a mm)
-            # TODO: this should be adjusted by the lens magnification:
+            # TODO: this should be adjusted by the lens NA or magnification:
             # the higher the magnification, the smaller is the change
             # (=> proportional ?)
             # negative == go up == closer from the sample
-            val = 0.01e-6 * shift  # m
+            val = 5e-9 * shift  # m
             assert(abs(val) < 0.01) # a move of 1 cm is a clear sign of bug
             # logging.error("%s, %s", axis, shift)
             self.microscope_view.moveFocusRel(val)
