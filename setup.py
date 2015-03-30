1a5c48fb18e9f868e288d906bb7e85912566ce33
Éric Piel <piel@delmic.com>
2015-03-30 09:49:34 +0200

    getting ready for v2.1.3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ import sys
 
 
 # To be updated to the current version
-VERSION = "2.1.2"
+VERSION = "2.1.3"
 # We cannot use the git version because it's not (always) available when building
 # the debian package
 
