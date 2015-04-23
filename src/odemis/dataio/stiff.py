aa55062ae48032e4dd4e9e0c2e218b953f582914
Kimon Tsitsikas <tsitsikas@delmic.com>
2015-04-23 12:05:22 +0200

    Tiff multiple-files exporter added

---
+++ src/odemis/dataio/stiff.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 21 Apr 2015
+
+@author: Kimon Tsitsikas
+
+Copyright © 2015 Kimon Tsitsikas, Delmic
+
+This file is part of Odemis.
+
+Odemis is free software: you can redistribute it and/or modify it under the terms
+of the GNU General Public License version 2 as published by the Free Software
+Foundation.
+
+Odemis is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+Odemis. If not, see http://www.gnu.org/licenses/.
+'''
+from __future__ import division
+
+from odemis.dataio import tiff
+
+# User-friendly name
+FORMAT = "TIFF"
+# list of file-name extensions possible, the first one is the default when saving a file
+EXTENSIONS = [u".s.ome.tiff"]
+
+# An almost identical OME-XML metadata block is inserted into the first IFD of
+# each constituent OME-TIFF file. This is for redundancy purposes: if even one of
+# the constituent files survives a data crash, the metadata survives. Each of
+# the files in the set has identical metadata apart from the UUID, the unique
+# identifier in each file.
+
+def export(filename, data, thumbnail=None, compressed=True):
+    '''
+    Write a collection of multiple OME-TIFF files with the given images and 
+    metadata
+    filename (unicode): filename of the file to create (including path)
+    data (list of model.DataArray, or model.DataArray): the data to export.
+       Metadata is taken directly from the DA object. If it's a list, a multiple
+       files distribution is created.
+    thumbnail (None or numpy.array): Image used as thumbnail for the first file. 
+      Can be of any (reasonable) size. Must be either 2D array (greyscale) or 3D 
+      with last dimension of length 3 (RGB). If the exporter doesn't support it, 
+      it will be dropped silently.
+    compressed (boolean): whether the file is compressed or not.
+    '''
+    tiff.export(filename, data, thumbnail, compressed, multiple_files=True)
