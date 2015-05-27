delmic/odemis

commit 569a6241907514451a8f9445da877aa87671860f

GUI: Repaired dead object warnings for deleted stream panels.

src/odemis/gui/cont/streams.py
@@ -105,6 +105,8 @@ class StreamController(object):
             label_edit = True
 
         self.stream_panel = StreamPanel(stream_bar, stream, label_edit)
+        self.stream_panel.Bind(wx.EVT_WINDOW_DESTROY, self._on_stream_panel_destroy)
+
         self.tab_data_model = tab_data_model
 
         # Peak excitation/emission wavelength of the selected dye, to be used for peak text and
@@ -148,6 +150,25 @@ class StreamController(object):
         self.stream_panel.Bind(EVT_STREAM_VISIBLE, self._on_stream_visible)
 
         stream_bar.add_stream_panel(self.stream_panel, show)
+
+    def _on_stream_panel_destroy(self, _):
+        """ Remove all references to setting entries and the possible VAs they might contain
+
+        TODO: Make stream panel creation and destruction cleaner by having the StreamBarController
+        being the main class respobsible for it.
+
+        """
+
+        # Destroy references to this controller in even handlers
+        # (More references are present, see getrefcount 
+        self.stream_panel.Unbind(wx.EVT_WINDOW_DESTROY)
+        self.stream_panel.header_change_callback = None
+        self.stream_panel.Unbind(EVT_STREAM_VISIBLE)
+
+        self.entries = None
+
+        # import sys, gc
+        # print sys.getrefcount(self), "\n".join([str(s) for s in gc.get_referrers(self)])
 
     def _on_stream_visible(self, evt):
         """ Show or hide a stream in the focussed view if the visibility button is clicked """
@@ -589,6 +610,7 @@ class StreamController(object):
             :param hist: ndArray of integers, the contents is a list a values in [0.0..1.0]
 
             TODO: don't update when folded: it's useless => unsubscribe
+            FIXME: Make sure this works as intended, see sandbox
 
             """
 
@@ -735,9 +757,9 @@ class StreamBarController(object):
             for channel_data in channels_data:
                 # TODO: be more clever to detect the type of stream
                 if (
-                                    model.MD_WL_LIST in channel_data.metadata or
-                                    model.MD_WL_POLYNOMIAL in channel_data.metadata or
-                            (len(channel_data.shape) >= 5 and channel_data.shape[-5] > 1)
+                    model.MD_WL_LIST in channel_data.metadata or
+                    model.MD_WL_POLYNOMIAL in channel_data.metadata or
+                    (len(channel_data.shape) >= 5 and channel_data.shape[-5] > 1)
                 ):
                     name = channel_data.metadata.get(model.MD_DESCRIPTION, "Spectrum")
                     klass = acqstream.StaticSpectrumStream
@@ -746,19 +768,17 @@ class StreamBarController(object):
                     ar_data.append(channel_data)
                     continue
                 elif (
-                            (model.MD_IN_WL in channel_data.metadata and
-                                     model.MD_OUT_WL in channel_data.metadata) or
-                                model.MD_USER_TINT in channel_data.metadata
+                        (model.MD_IN_WL in channel_data.metadata and
+                         model.MD_OUT_WL in channel_data.metadata) or
+                        model.MD_USER_TINT in channel_data.metadata
                 ):
                     # No explicit way to distinguish between Brightfield and Fluo,
                     # so guess it's Brightfield iif:
                     # * No tint
                     # * (and) Large band for excitation wl (> 100 nm)
                     in_wl = d.metadata[model.MD_IN_WL]
-                    if (
-                                    model.MD_USER_TINT in channel_data.metadata or
-                                        in_wl[1] - in_wl[0] < 100e-9
-                    ):
+                elif model.MD_IN_WL in channel_data.metadata:  # no MD_OUT_WL
+                    if model.MD_USER_TINT in channel_data.metadata or in_wl[1] - in_wl[0] < 100e-9:
                         # Fluo
                         name = channel_data.metadata.get(model.MD_DESCRIPTION, "Filtered colour")
                         klass = acqstream.StaticFluoStream
@@ -766,7 +786,6 @@ class StreamBarController(object):
                         # Brightfield
                         name = channel_data.metadata.get(model.MD_DESCRIPTION, "Brightfield")
                         klass = acqstream.StaticBrightfieldStream
-                elif model.MD_IN_WL in channel_data.metadata:  # no MD_OUT_WL
                     name = channel_data.metadata.get(model.MD_DESCRIPTION, "Brightfield")
                     klass = acqstream.StaticBrightfieldStream
                 else:
@@ -1094,9 +1113,6 @@ class StreamBarController(object):
         if not view:
             return
 
-        # import sys
-        # print sys.getrefcount(self)
-
         # hide/show the stream panels which are compatible with the view
         allowed_classes = view.stream_classes
         for e in self._stream_bar.stream_panels:
@@ -1111,11 +1127,11 @@ class StreamBarController(object):
         for e in self._stream_bar.stream_panels:
             e.set_visible(e.stream in visible_streams)
 
-        logging.debug("Sending stream.ctrl message")
-        pub.sendMessage('stream.ctrl',
-                        streams_present=True,
-                        streams_visible=self._has_visible_streams(),
-                        tab=self._tab_data_model)
+        # logging.debug("Sending stream.ctrl message")
+        # pub.sendMessage('stream.ctrl',
+        #                 streams_present=True,
+        #                 streams_visible=self._has_visible_streams(),
+        #                 tab=self._tab_data_model)
 
     def _onStreamUpdate(self, stream, updated):
         """
@@ -1319,11 +1335,11 @@ class StreamBarController(object):
         except ValueError:
             logging.warn("Stream not found, so not removed")
 
-        logging.debug("Sending stream.ctrl.removed message")
-        pub.sendMessage('stream.ctrl.removed',
-                        streams_present=self._has_streams(),
-                        streams_visible=self._has_visible_streams(),
-                        tab=self._tab_data_model)
+        # logging.debug("Sending stream.ctrl.removed message")
+        # pub.sendMessage('stream.ctrl.removed',
+        #                 streams_present=self._has_streams(),
+        #                 streams_visible=self._has_visible_streams(),
+        #                 tab=self._tab_data_model)
 
     def clear(self):
         """
@@ -1351,11 +1367,11 @@ class StreamBarController(object):
         if self._has_streams() or self._has_visible_streams():
             logging.warning("Failed to remove all streams")
 
-        logging.debug("Sending stream.ctrl.removed message")
-        pub.sendMessage('stream.ctrl.removed',
-                        streams_present=False,
-                        streams_visible=False,
-                        tab=self._tab_data_model)
+        # logging.debug("Sending stream.ctrl.removed message")
+        # pub.sendMessage('stream.ctrl.removed',
+        #                 streams_present=False,
+        #                 streams_visible=False,
+        #                 tab=self._tab_data_model)
 
     def _has_streams(self):
         return len(self._stream_bar.stream_panels) > 0

src/odemis/gui/test/sandbox.py
@@ -195,10 +195,30 @@ class MainWindow(wx.Frame):
         # cpd = CalibrationProgressDialog(self, 1, 1, 1, 1, 1, 1)
         # cpd.ShowModal()
 
-app = wx.App(False)
-win = MainWindow(None)
-app.MainLoop()
+# app = wx.App(False)
+# win = MainWindow(None)
+# app.MainLoop()
 
 # END THREAD RENDER TEST
 
 #=============================================================================
+
+fs = []
+
+for i in range(4):
+    def grr():
+        def t():
+            res = "%s %s" % (id(t), t.x)
+            t.x += 1
+            return res
+        t.x = i
+        return t
+    fs.append(grr())
+
+    # def t():
+    #     t.x = i + 0
+    #     return "%s %s %s" % (id(t), t.x, id(t.x))
+    # fs.append(t)
+
+print [f() for f in fs]
+print [f() for f in fs]

src/odemis/gui/util/__init__.py
@@ -107,7 +107,7 @@ def dead_object_wrapper(f):
             if app:
                 return f(*args, **kwargs)
         except (wx.PyDeadObjectError, RuntimeError):
-            logging.warn("Dead object ignored in %s", f.__name__)
+            logging.error("Dead object ignored in %s", f.__name__)
     return dead_object_wrapzor
 
 # ============== ENDWrappers

src/odemis/gui/win/acquisition.py
@@ -94,8 +94,8 @@ class AcquisitionDialog(xrcfr_acq):
         orig_view = orig_tab_data.focussedView.value
         view = self._tab_data_model.focussedView.value
 
-        self.stream_controller = StreamBarController(self._tab_data_model,
-                                                     self.pnl_secom_streams)
+        self.streambar_controller = StreamBarController(self._tab_data_model,
+                                                        self.pnl_secom_streams)
         # The streams currently displayed are the one visible
         self.add_all_streams()
 
@@ -180,7 +180,7 @@ class AcquisitionDialog(xrcfr_acq):
         # go through all the streams available in the interface model
         for s in self._tab_data_model.streams.value:
             view.addStream(s)  # Add first to the view, so "visible" button is correct
-            self.stream_controller.add_acquisition_stream_cont(s)
+            self.streambar_controller.add_acquisition_stream_cont(s)
 
     def remove_all_streams(self):
         """ Remove the streams we added to the view on creation """

commit a408f7813477a86c49e7a576171898449b2cc90d

gui cont: fix logic for stream opening
was broken by commit 569a6241907514, "GUI: Repaired dead object warnings for deleted stream panels.".

The if statement is messy, and got completely misrearranged.

src/odemis/gui/cont/streams.py
@@ -776,8 +776,7 @@ class StreamBarController(object):
                     # so guess it's Brightfield iif:
                     # * No tint
                     # * (and) Large band for excitation wl (> 100 nm)
-                    in_wl = d.metadata[model.MD_IN_WL]
-                elif model.MD_IN_WL in channel_data.metadata:  # no MD_OUT_WL
+                    in_wl = d.metadata.get(model.MD_IN_WL, (0, 0))
                     if model.MD_USER_TINT in channel_data.metadata or in_wl[1] - in_wl[0] < 100e-9:
                         # Fluo
                         name = channel_data.metadata.get(model.MD_DESCRIPTION, "Filtered colour")
@@ -786,6 +785,7 @@ class StreamBarController(object):
                         # Brightfield
                         name = channel_data.metadata.get(model.MD_DESCRIPTION, "Brightfield")
                         klass = acqstream.StaticBrightfieldStream
+                elif model.MD_IN_WL in channel_data.metadata:  # no MD_OUT_WL
                     name = channel_data.metadata.get(model.MD_DESCRIPTION, "Brightfield")
                     klass = acqstream.StaticBrightfieldStream
                 else: