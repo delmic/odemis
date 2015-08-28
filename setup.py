e8d748b439ac906b95f73524fe5325bbbb40f134
Éric Piel <piel@delmic.com>
2015-08-28 16:16:34 +0200

    config: prepare for release 2.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ import sys
 
 
 # To be updated to the current version
-VERSION = "2.1"
+VERSION = "2.2~rc1"
 # We cannot use the git version because it's not (always) available when building
 # the debian package
 
