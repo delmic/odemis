a290bc336204d03de823739044c5e62a3a85d067
Éric Piel <piel@delmic.com>
2015-09-04 11:50:05 +0200

    driver test spectrometer: clean up

--- src/odemis/driver/test/spectrometer_test.py
+++ src/odemis/driver/test/spectrometer_test.py
@@ -8,24 +8,22 @@ Copyright © 2013 Éric Piel, Delmic
 
 This file is part of Odemis.
 
-Odemis is free software: you can redistribute it and/or modify it under the terms 
-of the GNU General Public License version 2 as published by the Free Software 
+Odemis is free software: you can redistribute it and/or modify it under the terms
+of the GNU General Public License version 2 as published by the Free Software
 Foundation.
 
-Odemis is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
-without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+Odemis is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 PURPOSE. See the GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License along with 
+You should have received a copy of the GNU General Public License along with
 Odemis. If not, see http://www.gnu.org/licenses/.
 '''
 from __future__ import division
-from numpy.polynomial import polynomial
 from odemis import model
 from odemis.driver import spectrometer, spectrapro, pvcam, andorcam2
 from unittest.case import skip
 import logging
-import math
 import os
 import time
 import unittest
@@ -73,32 +71,32 @@ class TestSimulated(unittest.TestCase):
                                            "spectrograph": cls.spectrograph})
         #save position
         cls._orig_pos = cls.spectrograph.position.value
-    
+
     @classmethod
     def tearDownClass(cls):
         # restore position
         f = cls.spectrograph.moveAbs(cls._orig_pos)
         f.result() # wait for the move to finish
-        
+
         cls.spectrometer.terminate()
         cls.detector.terminate()
         cls.spectrograph.terminate()
-    
+
     def setUp(self):
         # put a meaningful wavelength
         f = self.spectrograph.moveAbs({"wavelength": 500e-9})
-        
+
         # save basic VA
         self._orig_binning = self.spectrometer.binning.value
         self._orig_res = self.spectrometer.resolution.value
-        
+
         f.result() # wait for the position to be set
-    
+
     def tearDown(self):
         # put back VAs
         self.spectrometer.binning.value = self._orig_binning
         self.spectrometer.resolution.value = self._orig_res
-    
+
     def test_simple(self):
         """
         Just ensures that the device has all the VA it should
@@ -109,26 +107,26 @@ class TestSimulated(unittest.TestCase):
         self.assertGreaterEqual(self.spectrometer.shape[0], self.spectrometer.shape[1])
         self.assertGreater(self.spectrometer.exposureTime.value, 0)
         self.assertIn("wavelength", self.spectrograph.axes)
-    
+
     def test_acquisition(self):
         exp = 0.1 #s
         self.spectrometer.exposureTime.value = exp
-        
+
         begin = time.time()
         data = self.spectrometer.data.get()
         duration = time.time() - begin
         self.assertGreaterEqual(duration, exp)
         self.assertEqual(data.shape[0], 1)
         self.assertEqual(data.shape[-1::-1], self.spectrometer.resolution.value)
-        
+
         begin = time.time()
         data = self.spectrometer.data.get()
         duration = time.time() - begin
         self.assertGreaterEqual(duration, exp)
         self.assertEqual(data.shape[0], 1)
         self.assertEqual(data.shape[-1::-1], self.spectrometer.resolution.value)
-        
-#@skip("only simulated")
+
+
 class TestCompositedSpectrometer(unittest.TestCase):
     """
     Test the CompositedSpectrometer class
@@ -144,32 +142,32 @@ class TestCompositedSpectrometer(unittest.TestCase):
                                            "spectrograph": cls.spectrograph})
         #save position
         cls._orig_pos = cls.spectrograph.position.value
-    
+
     @classmethod
     def tearDownClass(cls):
         # restore position
         f = cls.spectrograph.moveAbs(cls._orig_pos)
         f.result() # wait for the move to finish
-        
+
         cls.spectrometer.terminate()
         cls.detector.terminate()
         cls.spectrograph.terminate()
-    
+
     def setUp(self):
         # put a meaningful wavelength
         f = self.spectrograph.moveAbs({"wavelength": 500e-9})
-        
+
         # save basic VA
         self._orig_binning = self.spectrometer.binning.value
         self._orig_res = self.spectrometer.resolution.value
-        
+
         f.result() # wait for the position to be set
-    
+
     def tearDown(self):
         # put back VAs
         self.spectrometer.binning.value = self._orig_binning
         self.spectrometer.resolution.value = self._orig_res
-    
+
     def test_simple(self):
         """
         Just ensures that the device has all the VA it should
@@ -179,25 +177,25 @@ class TestCompositedSpectrometer(unittest.TestCase):
         self.assertEqual(len(self.spectrometer.shape), 3)
         self.assertGreaterEqual(self.spectrometer.shape[0], self.spectrometer.shape[1])
         self.assertGreater(self.spectrometer.exposureTime.value, 0)
-        
+
     def test_acquisition(self):
         exp = 0.1 #s
         self.spectrometer.exposureTime.value = exp
-        
+
         begin = time.time()
         data = self.spectrometer.data.get()
         duration = time.time() - begin
         self.assertGreaterEqual(duration, exp)
         self.assertEqual(data.shape[0], 1)
         self.assertEqual(data.shape[-1::-1], self.spectrometer.resolution.value)
-        
+
         begin = time.time()
         data = self.spectrometer.data.get()
         duration = time.time() - begin
         self.assertGreaterEqual(duration, exp)
         self.assertEqual(data.shape[0], 1)
         self.assertEqual(data.shape[-1::-1], self.spectrometer.resolution.value)
-        
+
 #    @skip("simple")
     def test_vbinning(self):
         """
@@ -205,7 +203,7 @@ class TestCompositedSpectrometer(unittest.TestCase):
         """
         if (self.spectrometer.binning.range[1][1] == 1):
             self.skipTest("Spectrometer doesn't support vertical binning")
-        
+
         # normally vertical binning is by default the maximum, so it's not going
         # to change much
         binning = [self.spectrometer.binning.value[0],    # as-is
@@ -213,22 +211,22 @@ class TestCompositedSpectrometer(unittest.TestCase):
         self.spectrometer.binning.value = binning
         self.spectrometer.resolution.value = self.spectrometer.resolution.range[1]
         self.assertEqual(self.spectrometer.binning.value, tuple(binning))
-        
+
         data = self.spectrometer.data.get()
         self.assertEqual(data.shape[-1::-1], self.spectrometer.resolution.value)
         md = data.metadata
         self.assertEqual(md[model.MD_BINNING], tuple(binning))
-        
+
         # reduce the binning (v resolution stays 1)
         binning[1] //= 2
         self.spectrometer.binning.value = binning
         self.assertEqual(self.spectrometer.resolution.value[1], 1)
-        
+
         data = self.spectrometer.data.get()
         self.assertEqual(data.shape[-1::-1], self.spectrometer.resolution.value)
         md = data.metadata
         self.assertEqual(md[model.MD_BINNING], tuple(binning))
-    
+
 #    @skip("simple")
     def test_hbinning(self):
         """
@@ -259,28 +257,28 @@ class TestCompositedSpectrometer(unittest.TestCase):
         self.assertEqual(data.shape[-1::-1], self.spectrometer.resolution.value)
         md = data.metadata
         self.assertEqual(md[model.MD_BINNING], tuple(binning))
-        
-    
+
+
     def test_resolution(self):
         """
-        Check the (unusual) behaviour of the resolution 
+        Check the (unusual) behaviour of the resolution
         """
         if (self.spectrometer.resolution.range[0] == self.spectrometer.resolution.range[1]):
             self.skipTest("Spectrometer doesn't support changing the resolution, boring")
-        
+
         # horizontally, resolution behaves pretty normally
         res = self.spectrometer.resolution.range[1] # max
         self.spectrometer.resolution.value = res
         res = self.spectrometer.resolution.value # the actual value
         data = self.spectrometer.data.get()
         self.assertEqual(data.shape[-1::-1], self.spectrometer.resolution.value)
-        
+
         res = self.spectrometer.resolution.range[0] # min
         self.spectrometer.resolution.value = res
         res = self.spectrometer.resolution.value # the actual value
         data = self.spectrometer.data.get()
         self.assertEqual(data.shape[-1::-1], self.spectrometer.resolution.value)
-        
+
         # vertically, it's fixed to one
         new_res = (self.spectrometer.resolution.value[0], 2)
         try:
@@ -289,7 +287,7 @@ class TestCompositedSpectrometer(unittest.TestCase):
             pass
         else:
             self.fail("vertical resolution should not be allowed above 1, got %r" % new_res)
-    
+
     def test_spec_calib(self):
         """
         Check that the calibration of the wavelength make _some_ sense
@@ -299,8 +297,8 @@ class TestCompositedSpectrometer(unittest.TestCase):
         # the wavelength bandwidth across the CCD should be pretty much constant
         # independent of the resolution (not exactly, as the wavelength is for
         # the center of the pixel, so the bigger are the pixels, the closer are
-        # the centers) 
-        
+        # the centers)
+
         # horizontal maximum res/min binning
         binning = (self.spectrometer.binning.range[0][0], # min
                    self.spectrometer.binning.range[1][1]) # max
@@ -308,13 +306,13 @@ class TestCompositedSpectrometer(unittest.TestCase):
         res = self.spectrometer.resolution.range[1] # max
         self.spectrometer.resolution.value = res
         res = self.spectrometer.resolution.value # actual value
-        
+
         # read calibration
         data = self.spectrometer.data.get()
         wl = data.metadata[model.MD_WL_LIST]
         self.assertEqual(len(wl), res[0])
         # pixel 0 to pixel N +1 => whole CCD
-        
+
         # do they make any sense?
         cwl = wl[res[0] // 2]
         # should be a monotonic function
@@ -323,19 +321,19 @@ class TestCompositedSpectrometer(unittest.TestCase):
         # centre wavelength should about (~20 nm) the same as the wavelength position
         exp_cwl = self.spectrograph.position.value["wavelength"]
         self.assertAlmostEqual(cwl, exp_cwl, delta=20e-9)
-        
+
         # 8 times smaller resolution
         binning = (min(binning[0] * 8, self.spectrometer.binning.range[1][0]),
                    binning[1])
         self.spectrometer.binning.value = binning
         res = self.spectrometer.resolution.value # new resolution
-        
+
         # read calibration
         data = self.spectrometer.data.get()
         wl = data.metadata[model.MD_WL_LIST]
         self.assertEqual(len(wl), res[0])
         # pixel 0 to pixel N +1 => whole CCD
-        
+
         # do they make any sense?
         cwl = wl[res[0] // 2]
         # should be a monotonic function
@@ -357,43 +355,43 @@ class TestCompositedSpectrometer(unittest.TestCase):
                 break
         else:
             raise IOError("Failed to find grating with density %d l/mm" % gdensity)
-        
+
     def test_known_calib(self):
         """
         Check that the calibration of the wavelength give similar results for
         a known system as computed theoretically (by PI).
         """
         # This assumes that we have a PIXIS 400 (1340 x 400)
-        if (self.spectrometer.shape[0] != 1340 or 
+        if (self.spectrometer.shape[0] != 1340 or
             self.spectrometer.pixelSize.value[0] != 20e-6):
             self.skipTest("Hardware needs to have to be a PIXIS 400 for the test")
-        # TODO: check we have a SpectraPro i2300 or FakeSpectraPro 
-        
+        # TODO: check we have a SpectraPro i2300 or FakeSpectraPro
+
         res = self.spectrometer.resolution.value
-        
+
         # 300 l/mm / 600 nm
         # => CCD coverage = 278 nm
         self._select_grating(300)
         f = self.spectrograph.moveAbs({"wavelength": 600e-9})
         f.result() # wait for the position to be set
-        
+
         data = self.spectrometer.data.get()
         wl = data.metadata[model.MD_WL_LIST]
         wl_bw = wl[-1] - wl[0]
         logging.debug("Got CCD coverage = %f nm", wl_bw * 1e9)
         self.assertAlmostEqual(wl_bw, 278e-9, 2)
-        
+
         # 1200 l/mm / 900 nm
         # => CCD coverage = 48 nm
         self._select_grating(1200)
         f = self.spectrograph.moveAbs({"wavelength": 900e-9})
         f.result() # wait for the position to be set
-        
+
         data = self.spectrometer.data.get()
         wl = data.metadata[model.MD_WL_LIST]
         wl_bw = wl[-1] - wl[0]
         logging.debug("Got CCD coverage = %f nm", wl_bw * 1e9)
         self.assertAlmostEqual(wl_bw, 48e-9, 2)
-        
+
 if __name__ == '__main__':
     unittest.main()
