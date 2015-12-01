40373f0d167e6f2a1e45d51873f77feb5c372641
Éric Piel <piel@delmic.com>
2015-12-01 14:04:02 +0100

    gui util: don't change the default socket timeout
    That affects too many things, including breaking Pyro callbacks.
    Instead, use urllib2, which allows to specify the timeout.
    
    Also add a basic test case

---
+++ src/odemis/gui/util/test/updater_test.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+"""
+Created on 1 Dec 2015
+
+@author: Éric Piel
+
+Copyright © 2015 Éric Piel, Delmic
+
+This file is part of Odemis.
+
+Odemis is free software: you can redistribute it and/or modify it under the terms
+of the GNU General Public License version 2 as published by the Free Software
+Foundation.
+
+Odemis is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+Odemis. If not, see http://www.gnu.org/licenses/.
+
+"""
+
+# test the functions of the gui.util.updater module
+from __future__ import division
+from odemis.gui.util import updater
+import logging
+import unittest
+
+logging.getLogger().setLevel(logging.DEBUG)
+
+
+class TestWindowsUpdater(unittest.TestCase):
+    def test_version(self):
+        # That should work on any OS
+        u = updater.WindowsUpdater()
+        lv = u.get_local_version()
+        # Should be #.#.###
+        self.assertEqual(len(lv.split('.')), 3)
+
+        rv, rsize = u.get_remote_version()
+        # Note: if not internet access, it would be acceptable to get None
+        self.assertEqual(len(rv.split('.')), 3)
+        self.assertGreater(rsize, 1000)
+
+    # TODO: test more methods
+
+if __name__ == "__main__":
+    unittest.main()
