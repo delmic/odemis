2c817e357aa71f46f6954f697e45e32e2b1a8849
Éric Piel <piel@delmic.com>
2016-01-25 14:06:11 +0100

    driver pigcs: clean up

--- src/odemis/driver/pigcs.py
+++ src/odemis/driver/pigcs.py
@@ -3122,7 +3122,6 @@ class SerialBusAccesser(object):
                 anssplited, ans = anssplited[:-1], anssplited[-1]
 
                 for l in anssplited:
-                    # logging.debug("Processing %s", l)
                     if not continuing:
                         lines = []  # one string per answer line
                         # remove the prefix
