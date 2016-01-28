be1396f88beaac9ef8e02ceb42e3cd5b005e355d
Rinze <rinze@elit.nl>
2016-01-28 13:55:20 +0100

    Windows: Added modifications for building the Delphi Viewer

--- install/windows/build.py
+++ install/windows/build.py
@@ -27,40 +27,41 @@ def add_size_to_version():
         version = '.'.join(odemis._get_version().split('-')[:2])
         f.write(str(os.path.getsize("dist\OdemisViewer-%s.exe" % version)) + '\n')
 
-print "Building OdemisViewer", '.'.join(odemis._get_version().split('-')[:2])
+print "Build Odemis viewers", '.'.join(odemis._get_version().split('-')[:2])
 
 os.chdir(os.path.dirname(__file__) or '.')
 
 while True:
     i = raw_input("""
-    [1] - Compile CPython modules
-    [2] - Build Odemis Viewer executable
-    [3] - Build Odemis Viewer installer
-    [4] - Build Delphi Viewer executable
-    [5] - Build Delphi Viewer installer
-    [6] - Run all
+    [1] - Executable build
+    [2] - Installer build
 
     > """)
 
     if i == '1':
         run_command(cpy_command)
-    elif i == '2':
         run_command(pyi_command, "odemis")
-    elif i == '3':
-        run_command(nsis_command, "odemis")
-        add_size_to_version()
-    elif i == '4':
         run_command(pyi_command, "delphi")
-    elif i == '5':
-        run_command(nsis_command, "delphi")
+    elif i == '2':
+        info = [
+            "/DPRODUCT_NAME=OdemisViewer",
+            "/DPRODUCT_HNAME=Odemis Viewer",
+            "/DIMAGE=install_odemis.bmp",
+            "/DWEBSITE=http://www.delmic.com",
+        ]
+        nsis_cmd = nsis_command[:-1] + info + [nsis_command[-1]]
+        run_command(nsis_cmd, "odemis")
+
+        info = [
+            "/DPRODUCT_NAME=DelphiViewer",
+            "/DPRODUCT_HNAME=Delphi Viewer",
+            "/DIMAGE=install_delphi.bmp",
+            "/DWEBSITE=http://www.delphimicroscope.com",
+        ]
+        nsis_cmd = nsis_command[:-1] + info + [nsis_command[-1]]
+        run_command(nsis_cmd, "delphi")
+
         add_size_to_version()
-    elif i == '6':
-        ret_code = run_command(cpy_command)
-        if ret_code == 0:
-            ret_code = run_command(pyi_command)
-            if ret_code == 0:
-                ret_code = run_command(nsis_command)
-                add_size_to_version()
     else:
         break
     print "\n\nBuild Done."
