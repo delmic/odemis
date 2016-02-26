4680590fcf0be94bcff5f17348baea5824979276
Éric Piel <piel@delmic.com>
2016-02-26 14:02:59 +0100

    config: preparing for v2.3.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ import sys
 
 
 # To be updated to the current version
-VERSION = "2.3~rc1"
+VERSION = "2.3.1"
 # We cannot use the git version because it's not (always) available when building
 # the debian package
 
