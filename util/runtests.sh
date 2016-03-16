4c9cd54fcf6697dab74495d43b356004639cc0b8
Éric Piel <piel@delmic.com>
2016-03-16 18:09:11 +0100

    util runtest: also run integration tests
    Run the integration test after the unit tests.
    Automatically kill the unit test if it takes too long.
    Save the log file with a different name every day.

--- util/runtests.sh
+++ util/runtests.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
+# Should be run in the root directory of Odemis source
 
-# Run all the tests that can be found:
-# Every file which is in the pattern /test/*_test.py
-
-TESTLOG=./test.log
-ODEMISPATH="$(readlink -m "./src/odemis")"
-
-# This environment variable makes the GUI test cases automatically close the test frames
-export NOMANUAL=1
-
-# This environment variable (should) make the driver test not try to use real hardware (only simulator)
-export TEST_NOHW=1
-
+# Some basic static analysis
 echo "Total number of lines of code (not including test cases):"
 find src/ -name "*.py" -a -not -name "*_test.py" -print0 | wc -l --files0-from=- | tail -1
 
@@ -30,12 +20,29 @@ echo "These files do not have the license header:"
 grep -LIr "GNU General Public License" --include=*.py src/
 echo "---"
 
+DATE=$(date +%Y%m%d)
+
+# Run all the unit tests that can be found:
+# Every file which is in the pattern /test/*_test.py
+MAXTIME=1800  # 30 min maximum per test case
+
+ODEMISPATH="$(readlink -m "./src/odemis")"
 PYTHONPATH=./src/:../Pyro4/src/
 if [ -f /etc/odemis.conf ]; then
     # use the odemis config if it's available
     . /etc/odemis.conf
 fi
 export PYTHONPATH
+
+# This environment variable makes the GUI test cases automatically close the test frames
+export NOMANUAL=1
+
+# This environment variable (should) make the driver test not try to use real hardware (only simulator)
+export TEST_NOHW=1
+
+TESTLOG=./unittest-full-$DATE.log
+# make sure it is full path
+TESTLOG="$(readlink -m "$TESTLOG")"
 
 if [ ! -d /var/run/odemisd ] ; then
     echo  "Need /var/run/odemisd"
@@ -44,9 +51,6 @@ fi
 
 # stop the backend
 sudo odemis-stop
-
-# make sure it is full path
-TESTLOG="$(readlink -m "$TESTLOG")"
 
 # find the test scripts (should not contain spaces)
 testfiles="$(find "$ODEMISPATH" -wholename "*/test/*test.py")"
@@ -58,7 +62,9 @@ if [ "$skippedfiles" != "" ]; then
     echo "$skippedfiles"
 fi
 
-echo "Running tests on $(date)" > "$TESTLOG"
+echo "Running unit tests"
+echo "Running unit tests on $(date)" > "$TESTLOG"
+
 # run each test script and save the output
 failures=0
 for f in $testfiles; do
@@ -69,14 +75,18 @@ for f in $testfiles; do
     echo "Running $f:" >> "$TESTLOG"
     # run it in its own directory (sometimes they need specific files from there)
     pushd "$(dirname $f)" > /dev/null
-        python $f --verbose >> "$TESTLOG" 2>&1
-        #echo coucou >> "$TESTLOG" 2>&1
+        # Automatically kill after MAXTIME, then try harder after 30 s
+        timeout -k 30 $MAXTIME python $f --verbose #>> "$TESTLOG" 2>&1
         status=$?
         echo $f returned $status >> "$TESTLOG" 2>&1
     popd > /dev/null
     grep -E "(OK|FAILED)" "$TESTLOG" | tail -1
+    echo Failures so far: $failures
     if [ "$status" -gt 0 ]; then
+        # TODO: failures can increase even if the test reported OK, if it was killed
+        # => synchronise it with FAILED
         failures=$(( $failures + 1 ))
+        echo $failures
     fi
 done
 
@@ -88,4 +98,13 @@ else
 fi
 
 # try to clean up a bit
-pkill -f odemis.odemisd.main
+sudo odemis-stop
+
+
+# Run the integration tests
+ODMPATH=../mic-odm-yaml/
+INTEGLOGDIR="./integtest-$DATE/"
+mkdir -p "$INTEGLOGDIR"
+
+echo "Running integration tests"
+./util/run_intg_tests.py --log-path "$INTEGLOGDIR" "$ODMPATH"/*/
