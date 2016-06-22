1e706fb7601a7913851c528dfc42ba27824efc91
Éric Piel <piel@delmic.com>
2016-06-22 13:24:19 +0200

    prepare for v2.4.4 release

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ import sys
 
 
 # To be updated to the current version
-VERSION = "2.4.3"
+VERSION = "2.4.4"
 # We cannot use the git version because it's not (always) available when building
 # the debian package
 
