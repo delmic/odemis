924e878f26dd5c65dce909cbf9c835ad941a8a8b
Éric Piel <piel@delmic.com>
2016-08-15 15:56:22 +0200

    util spot: supress some numpy warnings
    Avoid numpy warning about NaN data, and about indices being float.
    They were displayed with newer versions of numpy (v1.11)
    
    Also add some more test cases, fix the documentation, and ensure the center is computed correctly.

--- src/odemis/util/test/spot_test.py
+++ src/odemis/util/test/spot_test.py
@@ -21,8 +21,11 @@ import numpy
 from odemis import model
 from odemis.dataio import tiff, hdf5
 from odemis.util import spot
+import os
 import unittest
 
+
+TEST_IMAGE_PATH = os.path.dirname(__file__)
 
 class TestMomentOfInertia(unittest.TestCase):
     """
@@ -30,8 +33,8 @@ class TestMomentOfInertia(unittest.TestCase):
     """
     def setUp(self):
         # These are example data (computer generated)
-        data = tiff.read_data("moi_input.tif")[0]
-        background = tiff.read_data("moi_background.tif")[0]
+        data = tiff.read_data(os.path.join(TEST_IMAGE_PATH, "moi_input.tif"))[0]
+        background = tiff.read_data(os.path.join(TEST_IMAGE_PATH, "moi_background.tif"))[0]
         self.data = data
         self.background = background
 
@@ -87,7 +90,6 @@ class TestSpotIntensity(unittest.TestCase):
         self.assertAlmostEqual(si, 0.8621370728816907)
 
 
-TEST_IMAGE_PATH = "./"
 class TestFindCenterCoordinates(unittest.TestCase):
     """
     Test FindCenterCoordinates()
@@ -108,12 +110,54 @@ class TestFindCenterCoordinates(unittest.TestCase):
                                 (0.020941736978718473, -0.0071056828496776324)]
 
         for i in range(10):
-            data = hdf5.read_data(TEST_IMAGE_PATH + "image" + str(i + 1) + ".h5")[0]
+            data = hdf5.read_data(os.path.join(TEST_IMAGE_PATH, "image" + str(i + 1) + ".h5"))[0]
             C, T, Z, Y, X = data.shape
             data.shape = Y, X
             spot_coordinates = spot.FindCenterCoordinates(data)
             numpy.testing.assert_almost_equal(spot_coordinates, expected_coordinates[i], 3)
 
+    def test_find_center_big(self):
+        """
+        Test FindCenterCoordinates on large data
+        """
+        # Note: it's not very clear why, but the center is not exactly the same
+        # as with the original data.
+        expected_coordinates = [(-0.0003367114224783442, -0.022941682748052378),
+                                (0.4213370004373016, -0.25313267150174318),
+                                (0.054153514028894206, -0.046475569488448026),
+                                (0.15117193581594143, 0.20813363301021551),
+                                (0.1963834856403108, -0.18329597166583256),
+                                (0.23159684275306583, 1.3670166271550004),
+                                (-1.3363782613242998, 0.20192181693837058),
+                                (-0.15084662336702331, 0.65850157367975504),
+                                (-0.058984235874285897, 0.13071737132569164),
+                                (0.021009283646695891, -0.007037802630523865)]
+
+        for i in range(10):
+            data = hdf5.read_data(os.path.join(TEST_IMAGE_PATH, "image" + str(i + 1) + ".h5"))[0]
+            data.shape = data.shape[-2:]
+            Y, X = data.shape
+            databig = numpy.zeros((200 + Y, 200 + X), data.dtype)
+            databig += numpy.min(data)
+            # We put it right at the center, so shouldn't change expected coordinates
+            databig[100:100 + Y:, 100: 100 + X] = data
+            spot_coordinates = spot.FindCenterCoordinates(databig)
+            numpy.testing.assert_almost_equal(spot_coordinates, expected_coordinates[i], 3)
+
+    def test_find_center_syn(self):
+        """
+        Test FindCenterCoordinates on synthetic data
+        """
+        offsets = [(0,0),
+                   (-1, -1),
+                   (3, 2),
+                   ]
+        for ofs in offsets:
+            data = numpy.zeros((201, 201), numpy.uint16)
+            # Just one point, so it should be easy to find
+            data[100 + ofs[1], 100 + ofs[0]] = 500
+            spot_coordinates = spot.FindCenterCoordinates(data)
+            numpy.testing.assert_almost_equal(spot_coordinates, ofs, 3)
 
 if __name__ == "__main__":
     unittest.main()
