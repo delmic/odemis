76b30c3036b454a2e423d03237e59f68b51ad2b1
Éric Piel <piel@delmic.com>
2017-01-30 14:38:30 +0100

    prepare for v2.6.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ import sys
 
 
 # To be updated to the current version
-VERSION = "2.6.0"
+VERSION = "2.6.1"
 # We cannot use the git version because it's not (always) available when building
 # the debian package
 
