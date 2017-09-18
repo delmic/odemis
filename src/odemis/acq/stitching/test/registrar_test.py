f2f85b999bd325ef4b8d1bfc53edcb4e7bba0969
Philip Winkler <winkler@delmic.com>
2017-09-18 15:20:47 +0200

    acq: moved decompose function into registrar_test

--- src/odemis/acq/stitching/test/registrar_test.py
+++ src/odemis/acq/stitching/test/registrar_test.py
@@ -19,15 +19,17 @@ PURPOSE. See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along with 
 Odemis. If not, see http://www.gnu.org/licenses/.
 '''
+
+from __future__ import division
+import random
 import logging
 from odemis import model
 import numpy as np
 import time
 import unittest
 
-from decompose import decomposeImage
-from odemis.acq.stitching import *
 
+from odemis.acq.stitching import *
 
 from PIL import Image
 
@@ -230,6 +232,62 @@ class TestShiftRegistrar(unittest.TestCase):
         calculatedPositions = registrar.getPositions()[0]
         self.assertAlmostEqual(calculatedPositions[1][0],522,places=1)
         self.assertAlmostEqual(calculatedPositions[1][1],204,places=1)          
+        
+def decomposeImage(img, overlap=0.1, numTiles = 5, method="horizontalLines"):
+    """ 
+    Decomposes image into tiles for testing. The tiles overlap and their center positions are subject to random noise.
+    Returns list of tiles and list of the actual positions. 
+    img: square image obtained by PIL module
+    numTiles: number of desired tiles in each direction
+    method: acquisition method, "horizontalLines" scans image by row and starts at the left for each row,
+    "verticalLines" scans image by columns starting at the top for each row, and "horizontalZigzag" scans 
+    a row, then scans the next row in reverse, etc. mimicking the behaviour of DELMIC microscopes. 
+    """
+    
+    tileSize = int(img.size[0]/numTiles)
+    
+    pos = []
+    tiles = []
+    for i in range(numTiles):
+        for j in range(numTiles):
+            # Positions top left            
+            if method == "verticalLines":
+                posX = int(i*(1-overlap)*tileSize)
+                posY = int(j*(1-overlap)*tileSize)
+            elif method == "horizontalLines":
+                posX = int(j*(1-overlap)*tileSize)
+                posY = int(i*(1-overlap)*tileSize)
+            elif method == "horizontalZigzag":  
+                if i%2 == 0:
+                    posX = int(j*(1-overlap)*tileSize)
+                else:
+                    posX = int((numTiles-j-1)*(1-overlap)*tileSize) # reverse direction for every second row
+                posY = int(i*(1-overlap)*tileSize)
+                
+            md = {
+                model.MD_PIXEL_SIZE: [tileSize,tileSize],  # m/px
+                model.MD_POS: (posY+tileSize//2,posX+tileSize//2),  # m
+            }
+             
+            # Add noise
+            maxNoise = int(0.2*overlap*tileSize)
+            random.seed(1)
+            noise = [random.randrange(-maxNoise,maxNoise) for _ in range(2)]
+            if i>0 or j>0:
+                posX = max(0,int(posX + noise[0]))
+                posY = max(0,int(posY + noise[1]))
+            
+            # Crop images
+            cropped = img.crop((posX,posY,posX+tileSize,posY+tileSize))
+            tile = np.array(cropped.convert('L'))
+            
+            # Create list of tiles and positions
+            tile = model.DataArray(tile, md)
+            
+            tiles.append(tile)
+            pos.append([posX+tileSize//2,posY+tileSize//2])
+            
+    return [tiles, pos] 
     
 if __name__ == '__main__':
     unittest.main()
