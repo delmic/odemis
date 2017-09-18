a3fa6c9b55c36329b97f30303b5ace3bad8f9e3f
Philip Winkler <winkler@delmic.com>
2017-09-18 13:20:34 +0200

    acq: implementation and testing of stitching module

--- src/odemis/acq/stitching/__init__.py
+++ src/odemis/acq/stitching/__init__.py
@@ -2,7 +2,7 @@
 '''
 Created on 19 Jul 2017
 
-@author: Éric Piel
+@author: Éric Piel, Philip Winkler
 
 Copyright © 2017 Éric Piel, Delmic
 
@@ -16,6 +16,50 @@ You should have received a copy of the GNU General Public License along with Ode
 '''
 from __future__ import division
 
-
 from ._registrar import *
 from ._weaver import *
+
+import copy
+
+
+def register(tiles, method="REGISTER_SHIFT"):
+    """ 
+    tiles (list of DataArray of shape YX or tuples of DataArrays): The tiles to compute the registration. 
+    If it's tuples, the first tile of each tuple is the “main tile”, and the following ones are 
+    dependent tiles.
+    method (REGISTER_*): REGISTER_SHIFT → ShiftRegistrar, REGISTER_IDENTITY → IdentityRegistrar
+    returns:
+        tiles (list of DataArray of shape YX or tuples of DataArrays): The tiles as passed, but with updated 
+        MD_POS metadata
+    """
+    # TODO: implement functionality for dependent tiles. 
+    if method == "REGISTER_SHIFT":
+        registrar = ShiftRegistrar()
+        
+    elif method == "REGISTER_IDENTITY":
+        registrar = IdentityRegistrar()
+    
+    else:
+        raise ValueError("Invalid registrar") 
+
+    updatedTiles=[]
+    for i in range(len(tiles)):
+        if isinstance(tiles[i],tuple):
+            tile = tiles[i][0]
+            dep_tiles = tiles[i][1:]
+        else:
+            tile = tiles[i]
+            dep_tiles = None
+        registrar.addTile(tile,dep_tiles)
+
+        md=copy.deepcopy(tile.metadata)
+
+        md[model.MD_POS] = registrar.getPositions()[i]
+        tileUpd = model.DataArray(tile,md)
+        updatedTiles.append(tileUpd)
+        
+    return updatedTiles
+
+def weave(tiles, method="WEAVER_MEAN"):
+    pass
+
