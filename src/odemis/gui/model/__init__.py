delmic/odemis

commit 2bc4e07c183e2c704a26b66f4c67b018070dcd3b

gui model: make StreamView.getStreams() always return Streams
It would return directly stream_tree.getStreams(), but that means that now it
could return DataProjections. The rest of the methods of the View are hiding
the DataProjection, and this method should do too.

That simplifies some code (and maybe solve already some bugs).

If DataProjections are needed, then the caller should now explicitely call
View.stream_tree.getStreams().

src/odemis/gui/model/__init__.py
@@ -1212,17 +1212,15 @@ class StreamView(View):
 
     def getStreams(self):
         """
-        :return: [Stream or DataProjection] list of streams that are displayed in the view
+        :return: [Stream] list of streams that are displayed in the view
 
         Do not modify directly, use addStream(), and removeStream().
-        Note: use .stream_tree for getting the raw StreamTree
+        Note: use .stream_tree for getting the raw StreamTree (with the DataProjection)
         """
-        # TODO: should it just always return the streams? As addStream() and
-        # removeStream() automatically convert Stream -> projection.
-        # [s.stream if isinstance(s, DataProjection) else s for s in ss]
-        # It'd still be possible to get the projection by directly asking the
-        # stream_tree.
-        return self.stream_tree.getStreams()
+        ss = self.stream_tree.getStreams()
+        # ss is a list of either Streams or DataProjections, so need to convert
+        # back to only streams.
+        return [s.stream if isinstance(s, DataProjection) else s for s in ss]
 
     def addStream(self, stream):
         """

plugins/manual_overlay.py
@@ -140,7 +140,7 @@ class ManualOverlayPlugin(Plugin):
         views.extend(self.main_app.main_data.tab.value.tab_data_model.views.value)
 
         for v in views:
-            for sp in v.getStreams():  # stream or projection
+            for sp in v.stream_tree.getStreams():  # stream or projection
                 if isinstance(sp, DataProjection):
                     s = sp.stream
                 else:

src/odemis/gui/comp/miccanvas.py
@@ -370,7 +370,8 @@ class DblMicroscopeCanvas(canvas.DraggableCanvas):
         #   widget is unspecifying anyway.
         # The merge ratio actually corresponds to the opacity of the last image drawn
 
-        streams = self.microscope_view.getStreams()
+        # Use the stream tree, to get the DataProjection if there is one
+        streams = self.microscope_view.stream_tree.getStreams()
         images_opt = []
         images_spc = []
         images_std = []
@@ -983,7 +984,7 @@ class DblMicroscopeCanvas(canvas.DraggableCanvas):
         # Find bounding box of all the content
         bbox = [None, None, None, None]  # ltrb in m
         if self.microscope_view is not None:
-            streams = self.microscope_view.getStreams()
+            streams = self.microscope_view.stream_tree.getStreams()
             for s in streams:
                 try:
                     s_bbox = s.getBoundingBox()
@@ -1211,7 +1212,7 @@ class SparcARCanvas(DblMicroscopeCanvas):
         Same as the overridden method, but ensures the goal image keeps the alpha
         and is displayed second. Also force the mpp to be the one of the sensor.
         """
-        streams = self.microscope_view.getStreams()
+        streams = self.microscope_view.stream_tree.getStreams()
         ims = []
 
         # order and display the images
@@ -1548,7 +1549,7 @@ class AngularResolvedCanvas(canvas.DraggableCanvas):
         """
 
         # Normally the view.streamtree should have only one image anyway
-        streams = self.microscope_view.getStreams()
+        streams = self.microscope_view.stream_tree.getStreams()
 
         # add the images in order
         ims = []

src/odemis/gui/comp/viewport.py
@@ -407,17 +407,8 @@ class MicroscopeViewport(ViewPort):
                     self.bottom_legend.set_stream_type(wx.LEFT, EMStream)
                     self.bottom_legend.set_stream_type(wx.RIGHT, SpectrumStream)
                 else:
-                    sc = self._microscope_view.stream_tree[0]
-                    # if sc is an instance of DataProjection, get the inner stream
-                    if isinstance(sc, DataProjection):
-                        sc = sc.stream
-                    self.bottom_legend.set_stream_type(wx.LEFT, sc.__class__)
-
-                    sc = self._microscope_view.stream_tree[1]
-                    # if sc is an instance of DataProjection, get the inner stream
-                    if isinstance(sc, DataProjection):
-                        sc = sc.stream
-                    self.bottom_legend.set_stream_type(wx.RIGHT, sc.__class__)
+                    self.bottom_legend.set_stream_type(wx.LEFT, streams[0].__class__)
+                    self.bottom_legend.set_stream_type(wx.RIGHT, streams[1].__class__)
 
                 self.ShowMergeSlider(True)
         else:

src/odemis/gui/cont/export.py
@@ -211,7 +211,7 @@ class ExportController(object):
         """
         fview = self._data_model.focussedView.value
         vp = self.get_viewport_by_view(fview)
-        streams = fview.getStreams()
+        streams = fview.stream_tree.getStreams()  # Stream tree to get the DataProjection
         if export_type == 'AR':
             exported_data = ar_to_export_data(streams, raw)
         elif export_type == 'spectrum':

src/odemis/gui/cont/streams.py
@@ -2185,7 +2185,7 @@ class SparcStreamsController(StreamBarController):
 
         fv = self._tab_data_model.focussedView.value
         if (isinstance(stream, fv.stream_classes) and  # view is compatible
-                not stream in fv.getStreams()):
+            stream not in fv.stream_tree):
             # Add to the view
             fv.addStream(stream)
             # Update the graphical display

commit d79f95123692fc09b6e0ba3e94834c99fbe10447

gui model: fix pyramidal images not updating when zooming in
Was broken by 2bc4e07c183e2c704a26b66f4c67b018070dcd3b, that caused
.getStreams() to return the original streams.
The view actually needs the projection, so we need to access .stream_tree.getStreams().

src/odemis/gui/model/__init__.py
@@ -969,7 +969,7 @@ class StreamView(View):
             self.view_pos.value[0] + half_fov[0],
             self.view_pos.value[1] - half_fov[1],
         )
-        streams = self.getStreams()
+        streams = self.stream_tree.getStreams()
         for stream in streams:
             if hasattr(stream, 'rect'): # the stream is probably pyramidal
                 stream.rect.value = stream.rect.clip(view_rect)