b62412645cfad6086d8c7649d90cbdb7b56c2152
Philip Winkler <winkler@delmic.com>
2017-12-06 10:56:11 +0100

    gui: use merge ratio for history overlay
    Using both a built-up overview image and a history overlay at the same time makes the image too cluttered. Therefore we don't want to use the history overlay on the built-up overview image, but only on the global image taken by the camera. The transparency of the overlay changes with the merge ratio.

--- src/odemis/gui/comp/overlay/view.py
+++ src/odemis/gui/comp/overlay/view.py
@@ -39,6 +39,7 @@ from odemis.gui.util.conversion import change_brightness
 import odemis.model as model
 import odemis.util.conversion as conversion
 import odemis.util.units as units
+from odemis.model._vattributes import VigilantAttribute
 
 
 class TextViewOverlay(base.ViewOverlay):
@@ -1343,6 +1344,8 @@ class HistoryOverlay(base.ViewOverlay):
         self.history = history_list_va  # ListVA  of (center, size) tuples
         self.history.subscribe(self._on_history_update)
 
+        self.merge_ratio = None
+
     def __str__(self):
         return "History (%d): \n" % len(self) + "\n".join([str(h) for h in self.history.value[-5:]])
 
@@ -1377,6 +1380,8 @@ class HistoryOverlay(base.ViewOverlay):
 
         for i, (p_center, p_size) in enumerate(self.history.value):
             alpha = (i + 1) * (0.8 / len(self.history.value)) + 0.2 if self.fade else 1.0
+            if self.merge_ratio is not None:
+                alpha = alpha * (1 - self.merge_ratio)
             v_center = self.cnvs.phys_to_view(p_center, offset)
 
             if scale:
