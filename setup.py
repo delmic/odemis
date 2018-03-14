c0a2706ea97519b5cee93231d449933bc95b875e
Éric Piel <piel@delmic.com>
2018-03-14 13:30:36 +0100

    prepare for v2.7.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ import sys
 
 
 # To be updated to the current version
-VERSION = "2.7.2"
+VERSION = "2.7.3"
 # We cannot use the git version because it's not (always) available when building
 # the debian package
 
