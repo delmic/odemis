aa548c7ef842c6ac75bdcfdfae10d6e662c34ed6
Éric Piel <piel@delmic.com>
2018-06-17 20:55:04 +0200

    odemisd mdupdater: also update the monochromator affected by spectrograph-dedicated
    On the (rare) systems where a monochromator is behind a spectrograph-dedicated (instead of spectrograph),
    the MD_OUT_WL wouldn't be updated.
    => check for any role which starts with "spectrograph". Hopefully we'll never have a role like "spectrograph-not".

--- src/odemis/odemisd/mdupdater.py
+++ src/odemis/odemisd/mdupdater.py
@@ -105,7 +105,7 @@ class MetadataUpdater(model.Component):
                 elif a.role == "light":
                     # update the emitted light wavelength
                     self.observeLight(a, d)
-                elif a.role == "spectrograph":
+                elif a.role.startswith("spectrograph"):
                     # update the output wavelength range
                     self.observeSpectrograph(a, d)
                 elif a.role in ("cl-filter", "filter"):
