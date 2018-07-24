delmic/odemis

commit d8ddddd1bee017945190aa4ba119c306a71fb67b

odemisd mdupdater: also update the monochromator affected by spectrograph-dedicated
On the (rare) systems where a monochromator is behind a spectrograph-dedicated (instead of spectrograph),
the MD_OUT_WL wouldn't be updated.
=> check for any role which starts with "spectrograph". Hopefully we'll never have a role like "spectrograph-not".

src/odemis/odemisd/mdupdater.py
@@ -105,7 +105,7 @@ class MetadataUpdater(model.Component):
                 elif a.role == "light":
                     # update the emitted light wavelength
                     self.observeLight(a, d)
-                elif a.role == "spectrograph":
+                elif a.role.startswith("spectrograph"):
                     # update the output wavelength range
                     self.observeSpectrograph(a, d)
                 elif a.role in ("cl-filter", "filter"):

commit 474988d9ef40df954e3085b42180f4069e224408

odemisd mdupdater: fix NoneType error when a component has None as role
Commit d8ddddd1be (also update the monochromator affected by spectrograph-dedicated)
broke metadata update when a component which has affects has a None role.
It's not very typical, but happens on the Delphi.

src/odemis/odemisd/mdupdater.py
@@ -104,7 +104,7 @@ class MetadataUpdater(model.Component):
                 elif a.role == "light":
                     # update the emitted light wavelength
                     observed = self.observeLight(a, d)
-                elif a.role.startswith("spectrograph"):  # spectrograph-XXX too
+                elif a.role and a.role.startswith("spectrograph"):  # spectrograph-XXX too
                     # update the output wavelength range
                     observed = self.observeSpectrograph(a, d)
                 elif a.role in ("cl-filter", "filter"):