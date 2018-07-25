369820a891dd725bf95dd80eb6337fa6c4cf624f
Éric Piel <piel@delmic.com>
2018-07-25 10:18:08 +0200

    prepare v2.8.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ import sys
 
 
 # To be updated to the current version
-VERSION = "2.8.1"
+VERSION = "2.8.2"
 # We cannot use the git version because it's not (always) available when building
 # the debian package
 
