delmic/odemis

commit 3824a158ddcba638a808cab7ad3f4df64174d1f4

gui comp buttons: fix compatibility with old numpy
We still try to support Ubuntu 12.04

src/odemis/gui/comp/buttons.py
@@ -72,7 +72,11 @@ def darken_image(image, mltp=0.5):
       bright white.
     """
     # Update data in-place
-    data = numpy.asarray(image.GetDataBuffer())
+    # data = numpy.asarray(image.GetDataBuffer())
+    # TODO: on numpy 1.6 (Ubuntu 12.04), asarray() doesn't pick the buffer
+    # correctly. Once this is not supported anymore, use the cleaner asarray().
+    # Note that both functions allow to directly access and modify the data.
+    data = numpy.frombuffer(image.GetDataBuffer(), dtype=numpy.uint8)
     numpy.multiply(data, mltp, out=data, casting="unsafe")
 
 

commit aee3bfe6511e6bc01ceb91904373e86898c2822d

gui comp buttons: fix compatibility with wxPython4
Commit 3824a158 (fix compatibility with old numpy) broke the use with wxPython4.

=> Try to get "the best of both world", by selecting the right call based
on wxPython version.
It would have been nicer to check based on the numpy version... but there is
not simple way to compare the version.

src/odemis/gui/comp/buttons.py
@@ -72,11 +72,16 @@ def darken_image(image, mltp=0.5):
       bright white.
     """
     # Update data in-place
-    # data = numpy.asarray(image.GetDataBuffer())
-    # TODO: on numpy 1.6 (Ubuntu 12.04), asarray() doesn't pick the buffer
-    # correctly. Once this is not supported anymore, use the cleaner asarray().
-    # Note that both functions allow to directly access and modify the data.
-    data = numpy.frombuffer(image.GetDataBuffer(), dtype=numpy.uint8)
+    if wx.MAJOR_VERSION <= 3:
+        # On numpy 1.6 (Ubuntu 12.04), asarray() doesn't pick the buffer
+        # correctly. However, with wxPython 4, GetDataBuffer() returns a
+        # memoryview instead of a buffer, which is not accepted by frombuffer().
+        # Luckily, on systems with wxPython 4, we can expect also a newer numpy.
+        # TODO: Once this is not supported anymore, use the cleaner asarray().
+        # Note that both functions allow to directly access and modify the data.
+        data = numpy.frombuffer(image.GetDataBuffer(), dtype=numpy.uint8)
+    else:
+        data = numpy.asarray(image.GetDataBuffer())
     numpy.multiply(data, mltp, out=data, casting="unsafe")
 
 