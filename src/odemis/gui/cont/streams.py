delmic/odemis

commit d690dea0a9579fca846d80f2b6761bdd9d989dc5

gui util: Clean-up decorators
There are decorators and wrappers. They do the same thing, so let's make sure they
have the same code (modulo the fact that decorators are for wx.Object methods).

Also handle better when a widget is destroyed and has wxlimit_invocation().
With wxlimit_invocation, the wrapper cannot check if the object is dead before calling the function.
So explicitly check for the object being dead in the decorated functions.

Also unsubscribe from the most obvious events when a Viewport is destroyed.
That ensures that it doesn't keep processing events long after it's not in use.

src/odemis/gui/cont/streams.py
@@ -40,7 +40,7 @@ from odemis.gui.conf import data
 from odemis.gui.conf.data import get_local_vas
 from odemis.gui.conf.util import create_setting_entry, create_axis_entry, SettingEntry
 from odemis.gui.model import dye, TOOL_SPOT, TOOL_NONE
-from odemis.gui.util import call_in_wx_main, wxlimit_invocation, dead_object_wrapper
+from odemis.gui.util import call_in_wx_main, wxlimit_invocation
 from odemis.util import fluo
 from odemis.util.conversion import wave2rgb
 from odemis.util.fluo import to_readable_band, get_one_center
@@ -677,12 +677,15 @@ class StreamController(object):
                           ctrl_2_va=_get_bandwidth)
         self.entries[se.name] = se
 
-        # TODO: should the stream have a way to know when the raw data has changed? => just a
-        # spectrum VA, like histogram VA
+        # TODO: should the stream have a way to know when the raw data has changed?
+        # => just a spectrum VA, like histogram VA
         self.stream.image.subscribe(self._on_new_spec_data, init=True)
 
     @wxlimit_invocation(0.2)
     def _on_new_spec_data(self, _):
+        if not self or not self._sld_spec:
+            return  # already deleted
+
         logging.debug("New spec data")
         # Display the global spectrum in the visual range slider
         gspec = self.stream.getMeanSpectrum()
@@ -705,8 +708,7 @@ class StreamController(object):
             coef = 1
 
         gspec = (gspec - mins) * coef
-        # TODO: use decorator for this (call_in_wx_main_wrapper), once this code is stable
-        wx.CallAfter(dead_object_wrapper(self._sld_spec.SetContent), gspec.tolist())
+        self._sld_spec.SetContent(gspec.tolist())
 
     def _add_dye_ctrl(self):
         """

src/odemis/gui/comp/canvas.py
@@ -502,6 +502,8 @@ class BufferedCanvas(wx.Panel):
         """ Update the drawing when the on draw timer fires """
         # thread_name = threading.current_thread().name
         # logging.debug("Drawing timer in thread %s", thread_name)
+        if not self:
+            return
         self.update_drawing()
 
     # ########### END Event Handlers ############
@@ -889,7 +891,7 @@ class BitmapCanvas(BufferedCanvas):
         """
 
         # Don't draw anything if the canvas is disabled, leave the current buffer intact.
-        if not self.IsEnabled() or 0 in self.GetClientSize():
+        if not self or not self.IsEnabled() or 0 in self.GetClientSize():  # or not self
             return
 
         ctx = wxcairo.ContextFromDC(self._dc_buffer)
@@ -1993,7 +1995,7 @@ class PlotCanvas(BufferedCanvas):
         #     logging.warn("No buffer created yet, ignoring draw request")
         #     return
 
-        if not self.IsEnabled():
+        if not self or not self.IsEnabled():
             return
 
         ctx = wxcairo.ContextFromDC(self._dc_buffer)

src/odemis/gui/comp/legend.py
@@ -300,7 +300,8 @@ class AxisLegend(wx.Panel):
         """
         Refresh, which can be called safely from other threads
         """
-        wx.Panel.Refresh(self)
+        if self:
+            wx.Panel.Refresh(self)
 
     def on_paint(self, _):
         if self._value_range is None:
@@ -414,7 +415,8 @@ class RadioLegend(wx.Panel):
         """
         Refresh, which can be called safely from other threads
         """
-        wx.Panel.Refresh(self)
+        if self:
+            wx.Panel.Refresh(self)
 
     def OnPolarizationButton(self, event):
         """

src/odemis/gui/comp/miccanvas.py
@@ -127,6 +127,23 @@ class DblMicroscopeCanvas(canvas.DraggableCanvas):
         self._dc_region = None  # The ROI VA of the drift correction
         self.driftcor_overlay = None
 
+        self.Bind(wx.EVT_WINDOW_DESTROY, self._on_destroy)
+
+    def _on_destroy(self, evt):
+        # FIXME: it seems like this object stays in memory even after being destroyed.
+        # => need to make sure that the object is garbage collected
+        # (= no more references) once it's not used.
+        if self.view:
+            # Drop references
+            self.view = None
+
+            # As the tab data model may stay longer, we need to make sure it
+            # doesn't update the canvas anymore
+            tab_data = self._tab_data_model
+            tab_data.tool.unsubscribe(self._on_tool)
+            tab_data.main.debug.unsubscribe(self._on_debug)
+            self._tab_data_model = None
+
     # Ability manipulation
 
     def disable_zoom(self):
@@ -252,9 +269,6 @@ class DblMicroscopeCanvas(canvas.DraggableCanvas):
         """ Activate or deactivate interpolation"""
         self.request_drawing_update()
 
-    # FIXME: seems like it might still be called while the Canvas has been destroyed
-    # => need to make sure that the object is garbage collected (= no more references) once it's
-    # not used. (Or explicitly unsubscribe??)
     @ignore_dead
     def _on_debug(self, activated):
         """ Called when GUI debug mode changes => display FPS overlay """
@@ -534,7 +548,7 @@ class DblMicroscopeCanvas(canvas.DraggableCanvas):
 
     @wxlimit_invocation(2)  # max 1/2 Hz
     def update_thumbnail(self):
-        if self.IsEnabled():
+        if self and self.IsEnabled():
             img = self._get_img_from_buffer()
             if img is not None:
                 self.view.thumbnail.value = img
@@ -955,7 +969,7 @@ class OverviewCanvas(DblMicroscopeCanvas):
     @wxlimit_invocation(2)  # max 1/2 Hz
     def update_thumbnail(self):
 
-        if not self.IsEnabled() or self.ClientSize.x * self.ClientSize.y <= 0:
+        if not self or not self.IsEnabled() or 0 in self.ClientSize:
             return  # nothing to update
 
         # We need to scale the thumbnail ourselves, instead of letting the
@@ -1115,7 +1129,7 @@ class BarPlotCanvas(canvas.PlotCanvas):
 
     @wxlimit_invocation(2)  # max 1/2 Hz
     def update_thumbnail(self):
-        if self.IsEnabled():
+        if self and self.IsEnabled():
             if self._data is None:
                 self.view.thumbnail.value = None
             else:
@@ -1243,7 +1257,7 @@ class TwoDPlotCanvas(BitmapCanvas):
 
     @wxlimit_invocation(2)  # max 1/2 Hz
     def update_thumbnail(self):
-        if self.IsEnabled():
+        if self and self.IsEnabled():
             if all(i is None for i in self.images):
                 self.view.thumbnail.value = None
             else:
@@ -1420,7 +1434,7 @@ class AngularResolvedCanvas(canvas.DraggableCanvas):
 
     @wxlimit_invocation(2)  # max 1/2 Hz
     def update_thumbnail(self):
-        if self.IsEnabled():
+        if self and self.IsEnabled():
             img = self._get_img_from_buffer()
             if img is not None:
                 self.view.thumbnail.value = img

src/odemis/gui/comp/viewport.py
@@ -130,6 +130,12 @@ class ViewPort(wx.Panel):
 
         self.Bind(wx.EVT_CHILD_FOCUS, self.OnChildFocus)
         self.Bind(wx.EVT_SIZE, self.OnSize)
+        self.Bind(wx.EVT_WINDOW_DESTROY, self._on_destroy)
+
+    def _on_destroy(self, evt):
+        # Drop references
+        self._view = None
+        self._tab_data_model = None
 
     def __str__(self):
         return "{0} {2} {1}".format(

src/odemis/gui/util/__init__.py
@@ -36,31 +36,75 @@ import time
 import weakref
 import wx
 
+# Decorators & Wrappers
+# They are almost the same but the decorators assume that they are decorating
+# a method of a wx.Object (ie, it has a "self" argument)
 
-# ============== Decorators
+
 @decorator
 def call_in_wx_main(f, self, *args, **kwargs):
     """ This method decorator makes sure the method is called from the main
     (GUI) thread.
+    The function may be run asynchronously, so the function return value cannot
+    be returned. So it's typically an error if a decorated function returns
+    something useful.
     """
     # TODO: with Python 3, update that line to:
     # if threading.current_thread() == threading.main_thread()
     if isinstance(threading.current_thread(), threading._MainThread):
-        return f(self, *args, **kwargs)
-    return wx.CallAfter(f, self, *args, **kwargs)
+        f(self, *args, **kwargs)
+        return
+
+    wx.CallAfter(f, self, *args, **kwargs)
+
+
+def call_in_wx_main_wrapper(f):
+
+    @wraps(f)
+    def call_after_wrapzor(*args, **kwargs):
+        if isinstance(threading.current_thread(), threading._MainThread):
+            f(*args, **kwargs)
+            return
+
+        app = wx.GetApp()
+        if app:
+            wx.CallAfter(f, *args, **kwargs)
+
+    return call_after_wrapzor
 
 
 @decorator
 def ignore_dead(f, self, *args, **kwargs):
     """
+    Suppresses errors caused code trying to access
+    wxPython widgets that have already been destroyed
+
     If used on a function also decorated with call_in_wx_main, it should be
     the closest to the real function. IOW, always put this decorator at the
     bottom of the decorators.
     """
+    if not self:
+        logging.info("Skipping call to %s() as object is already dead", f.__name__)
+        return
+
     try:
         return f(self, *args, **kwargs)
     except RuntimeError:
-        logging.warn("Dead object ignored in %s", f.__name__)
+        logging.warn("Dead object %s ignored in %s", self, f.__name__)
+
+
+def dead_object_wrapper(f):
+
+    @wraps(f)
+    def dead_object_wrapzor(*args, **kwargs):
+        try:
+            app = wx.GetApp()
+            if app:
+                return f(*args, **kwargs)
+        except RuntimeError:
+            logging.warning("Dead object ignored in %s", f.__name__)
+
+    return dead_object_wrapzor
 
 
 def wxlimit_invocation(delay_s):
@@ -90,50 +134,6 @@ def wxlimit_invocation(delay_s):
         wf = call_in_wx_main_wrapper(wf)
         return liwrapper(wf)
     return wxwrapper
-
-# ============== END Decorators
-
-
-# ============== Wrappers
-
-def call_in_wx_main_wrapper(f):
-    @wraps(f)
-    def call_after_wrapzor(*args, **kwargs):
-        app = wx.GetApp()
-        if app:
-            wx.CallAfter(f, *args, **kwargs)
-    return call_after_wrapzor
-
-
-def dead_object_wrapper(f):
-    """ This simple wrapper suppresses errors caused code trying to access
-    wxPython widgets that have already been destroyed
-    """
-    @wraps(f)
-    def dead_object_wrapzor(*args, **kwargs):
-        try:
-            app = wx.GetApp()
-            if app:
-                return f(*args, **kwargs)
-        except RuntimeError:
-            logging.warning("Dead object ignored in %s", f.__name__)
-        # TODO: Investigate if this needs fixing
-        # It used to check for objects which are dead, and now it check for object which are an Exception.
-        # Probably the initial code is not correct, as it looks like it should use any instead of all.
-        # It also doesn't check in kwargs.
-        # I don't know how to check for dead objects now,
-        # and I don't know if a TypeError would happen in such case anyway.
-        # except TypeError:
-        #     # If a wx.Window is destroyed and then passed as a parameter, a TypeError might reulst
-        #     # instead of a PyDeadObjectError.
-        #     if all([isinstance(a, RuntimeError) for a in args]):
-        #         logging.warning("Dead object parameter ignored in %s", f.__name__)
-        #     else:
-        #         raise
-
-    return dead_object_wrapzor
-
-# ============== ENDWrappers
 
 
 # Path functions

src/odemis/gui/win/acquisition.py
@@ -299,6 +299,9 @@ class AcquisitionDialog(xrcfr_acq):
 
     @wxlimit_invocation(0.1)
     def update_setting_display(self):
+        if not self:
+            return
+
         # if gauge was left over from an error => now hide it
         if self.acquiring:
             self.gauge_acq.Show()

commit a6e7081f77cf157602adbffe67bfbb50a27dce6d

gui cont streams: fix repetition not being displayed
Another fallout from commit d690dea0a957 (Clean-up decorators).
Now the update to the value of the combobox would happen sooner.
Even sooner than the change of the choices of the combobox,
which so far had always happened before due to total luck.

So this change broke our luck. Now we have to be careful, and explicitly
put the value after clearing the combobox, because no one is doing it for us.

Also make sure that extra choice update happens in the main GUI thread,
as it touches the widgets. In practice, so far, it always happened in the main
GUI thread anyway.

src/odemis/gui/cont/streams.py
@@ -1036,6 +1036,9 @@ class StreamController(object):
             if vaname == "repetition":
                 self._rep_ctrl = ent.value_ctrl
                 # Update the combo box choices based on the current repetition value
+                # (an alternative would be to override our own va_2_ctrl to the
+                # SettingEntry, but currently create_setting_entry() doesn't
+                # allow to change it)
                 self.stream.repetition.subscribe(self._onStreamRep, init=True)
 
             if vaname in ("repetition", "pixelSize"):
@@ -1176,7 +1179,7 @@ class StreamController(object):
         evt.Skip()
 
     # Repetition combobox content updater
-
+    @call_in_wx_main
     def _onStreamRep(self, rep):
         """
         Called when the repetition VAs of a RepetitionStream is modified.
@@ -1185,10 +1188,10 @@ class StreamController(object):
         ratio = rep[1] / rep[0]
 
         # Create the entries:
-        choices = [(1, 1)]  # 1 x 1 should always be there
+        choices = [(1, 1), rep]  # 1 x 1 should always be there
 
         # Add a couple values below/above the current repetition
-        for m in (1 / 4, 1 / 2, 1, 2, 4, 10):
+        for m in (1 / 4, 1 / 2, 2, 4, 10):
             x = int(round(rep[0] * m))
             y = int(round(x * ratio))
             choices.append((x, y))
@@ -1210,6 +1213,9 @@ class StreamController(object):
         self._rep_ctrl.Clear()
         for choice in choices:
             self._rep_ctrl.Append(u"%s x %s px" % choice, choice)
+
+        # Make sure the current value is selected
+        self._rep_ctrl.SetSelection(choices.index(rep))
 
 
 class StreamBarController(object):