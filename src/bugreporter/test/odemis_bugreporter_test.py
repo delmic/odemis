43a68a88a6ad149221df5ec6b4cfbd1d17e9acf2
Éric Piel <piel@delmic.com>
2019-02-04 11:53:54 +0100

    bug reporter: test unicode characters in ticket
    Also make the key_path a constant (and fix the path).
    Also create an explicit test case for compress_file()
    Also rename TEST_SUPPORT_TICKET -> TEST_NO_SUPPORT_TICKET, as setting it _disables_ the testing

--- src/bugreporter/test/odemis_bugreporter_test.py
+++ src/bugreporter/test/odemis_bugreporter_test.py
@@ -30,10 +30,12 @@ from urllib2 import HTTPError
 import odemis
 
 REPORTER_TEST_ID = 12
-# Set TEST_SUPPORT_TICKET=1 to skip test cases which create a ticket, so we don't fill up
+# Set TEST_NO_SUPPORT_TICKET=1 to skip test cases which create a ticket, so we don't fill up
 # the osticket database with test messages.
-TEST_SUPPORT_TICKET = (os.environ.get("TEST_SUPPORT_TICKET", 0) != 0)
+TEST_NO_SUPPORT_TICKET = (os.environ.get("TEST_NO_SUPPORT_TICKET", 0) != 0)
 TEST_SUPPORT_TEAM_EMAIL = "winkler@delmic.com"
+
+TEST_KEY_PATH = os.path.dirname(odemis.__file__) + '/../../install/linux/usr/share/odemis/osticket.key'
 
 
 class TestOdemisBugreporter(unittest.TestCase):
@@ -54,19 +56,17 @@ class TestOdemisBugreporter(unittest.TestCase):
             self.assertRaises(LookupError, self.bugreporter.search_api_key)
         else:
             try:
-                api_key = self.bugreporter.search_api_key
+                api_key = self.bugreporter.search_api_key()
                 self.assertEqual(type(api_key), str, "API key needs to be a string.")
                 self.assertFalse('\n' in api_key, "API key should not contain newline character")
             except Exception as e:
-                self.fail("search_api_key failed with exception %s " % e)
+                self.fail("search_api_key failed with exception %s" % e)
 
     def test_source_api_key(self):
         """
         Tests format of source api key.
         """
-        key_path = (os.path.dirname(odemis.__file__) +
-                    'install/linux/usr/share/odemis/osticket.key')
-        with open(key_path, 'r') as key_file:
+        with open(TEST_KEY_PATH, 'r') as key_file:
             api_key = key_file.read().strip('\n')
         self.assertEqual(type(api_key), str, "API key needs to be a string.")
         self.assertFalse('\n' in api_key, "API key should not contain newline character")
@@ -75,20 +75,18 @@ class TestOdemisBugreporter(unittest.TestCase):
         """
         Tests ticket creation on osticket server.
         """
-        if TEST_SUPPORT_TICKET == 1:
+        if TEST_NO_SUPPORT_TICKET == 1:
             logging.info('Skipping "test_create_ticket"')
             return
 
-        key_path = (os.path.expanduser(u"~") +
-                    '/development/odemis/install/linux/usr/share/odemis/osticket.key')
-        with open(key_path, 'r') as key_file:
+        with open(TEST_KEY_PATH, 'r') as key_file:
             api_key = key_file.read().strip('\n')
 
         description = {
-            'name': 'Testingteam member',
-            'email': 'winkler@delmic.com',
-            'subject': 'Bugreporter test',
-            'message': "This is a test.",
+            'name': u'TÉstingteam member',
+            'email': u'winkler@delmic.com',
+            'subject': u'Bugreporter test',
+            'message': u"This is a test, including some non-ascii characters like µ or 你好",
             'topicId': REPORTER_TEST_ID,
             'attachments': [],
             }
@@ -99,9 +97,16 @@ class TestOdemisBugreporter(unittest.TestCase):
         except Exception as e:
             self.fail('Uploading without a file failed with exception %s' % e)
 
-        # Checking upload with large file in test_large_file, also tests compression
-
-        # Check with incorrect API key
+    def test_wrong_api_key(self):
+        """Create a ticket with an incorrect API key"""
+        description = {
+            'name': u'Testingteam member',
+            'email': u'winkler@delmic.com',
+            'subject': u'Bugreporter test',
+            'message': u"This is a test",
+            'topicId': REPORTER_TEST_ID,
+            'attachments': [],
+            }
         incorrect_api_key = 'xxxxxxx'
         self.assertRaises(HTTPError, self.bugreporter.create_ticket,
                            incorrect_api_key, description)
@@ -110,7 +115,6 @@ class TestOdemisBugreporter(unittest.TestCase):
         """
         Tests whether large files are compressed and uploaded properly.
         """
-
         # Add large file to list of files that will be compressed
         if not os.path.isdir(os.path.expanduser(u"~") + '/odemis-overlay-report'):
             os.mkdir(os.path.expanduser(u"~") + '/odemis-overlay-report')
@@ -123,14 +127,12 @@ class TestOdemisBugreporter(unittest.TestCase):
             self.bugreporter.compress_files()
         except Exception as e:
             self.fail("Compression of large file failed with exception %s" % e)
+        self.assertTrue(zipfile.is_zipfile(self.bugreporter.zip_fn))
 
         home_dir = os.path.expanduser(u"~")
         files = ['/var/log/odemis.log', os.path.join(home_dir, 'odemis-gui.log'),
                  os.path.join(home_dir, 'odemis-gui.log.1'), '/etc/odemis.conf', '/var/log/syslog',
                  os.path.join(home_dir, 'odemis-mic-selector.log'), '/tmp/odemis-bug-screenshot.png']
-        self.bugreporter.compress_files()
-
-        self.assertTrue(zipfile.is_zipfile(self.bugreporter.zip_fn))
 
         zip_file = zipfile.ZipFile(self.bugreporter.zip_fn)
         for f in files:
@@ -139,10 +141,8 @@ class TestOdemisBugreporter(unittest.TestCase):
                 self.assertTrue(f[1:] in zip_file.namelist(), "File %s not found in archive." % f)
 
         # Create ticket
-        if TEST_SUPPORT_TICKET != 1:
-            key_path = (os.path.expanduser(u"~") +
-                        '/development/odemis/install/linux/usr/share/odemis/osticket.key')
-            with open(key_path, 'r') as key_file:
+        if TEST_NO_SUPPORT_TICKET != 1:
+            with open(TEST_KEY_PATH, 'r') as key_file:
                 api_key = key_file.read().strip('\n')
     
             description = {
@@ -152,7 +152,7 @@ class TestOdemisBugreporter(unittest.TestCase):
                 'message': "This is a test.",
                 'topicId': REPORTER_TEST_ID,
                 'attachments': [],
-                }
+            }
     
             try:
                 self.bugreporter.create_ticket(api_key, description, [self.bugreporter.zip_fn])
@@ -164,6 +164,11 @@ class TestOdemisBugreporter(unittest.TestCase):
         os.remove(self.bugreporter.zip_fn)
         os.remove(fn)
 
+    def test_compress_file(self):
+        """Test log file compression"""
+        self.bugreporter.compress_files()
+        self.assertTrue(zipfile.is_zipfile(self.bugreporter.zip_fn))
+        os.remove(self.bugreporter.zip_fn)
 
 if __name__ == '__main__':
     unittest.main()
