delmic/odemis

commit 6bac84a23dc99f50b01e0525fe7677d0757e1c41

gui util img: round back very tiny values to 0
Due to floating point errors, when computing the tick for a legend,
if the 0 is at the middle, it's not always exactly 0, but value 1e-16 times
smaller than the range.
For other numbers, this error is hidden when displaying the number,
but with 0, it's displayed as just a very very tiny number which needs
"e-N" notation.
That's quite ugly => detect it and change it back to "pure" 0, so that
it shows up nicely.

src/odemis/gui/util/img.py
@@ -886,6 +886,7 @@ def calculate_ticks(value_range, client_size, orientation, tick_spacing):
         return tick_list, vtp_ratio
 
     vtp_ratio = abs(pixel_space / value_space)
+    epsilon = vtp_ratio / 10  # "tiny" value: a 10th of a pixel
 
     num_ticks = pixel_space // tick_spacing
     # Calculate the best step size in powers of 10, so it will cover at
@@ -926,9 +927,12 @@ def calculate_ticks(value_range, client_size, orientation, tick_spacing):
     for tick_value in tick_values:
         pixel = value_to_pixel(tick_value, pixel_space, vtp_ratio, value_range,
                                orientation)
+        # Round "almost 0" (due to floating point errors) to 0
+        if abs(tick_value) < epsilon:
+            tick_value = 0
         pix_val = (pixel, tick_value)
         if pix_val not in ticks:
-            if (tick_value not in [numpy.min(tick_values), numpy.max(tick_values)] and
+            if (tick_value not in (numpy.min(tick_values), numpy.max(tick_values)) and
                     abs(pixel - prev_pixel) < min_margin):
                 # keep a min distance between ticks
                 continue

commit c73ec0ae3973a75f7d99f0dd8b1b4acbef272ee3

gui util img: do not round every tick value to 0, only the tiny ones
Commit 6bac84a23dc (gui util img: round back very tiny values to 0) introduced a bug:
instead of only changint to 0 the tiny values, it would set to 0 any value which is small.

That's because I misunderstood the vtp_ratio variable. It's in px/unit -> the smaller the range,
the bigger the value. I expected it to be in unit/px.
=> Need to compute use the _inverse_ of vtp_ratio.

src/odemis/gui/util/img.py
@@ -885,8 +885,8 @@ def calculate_ticks(value_range, client_size, orientation, tick_spacing):
         tick_list = [(pixel, min_val)]
         return tick_list, vtp_ratio
 
-    vtp_ratio = abs(pixel_space / value_space)
-    epsilon = vtp_ratio / 10  # "tiny" value: a 10th of a pixel
+    vtp_ratio = abs(pixel_space / value_space)  # px/unit
+    epsilon = (1 / vtp_ratio) / 10  # "tiny" value: a 10th of a pixel
 
     num_ticks = pixel_space // tick_spacing
     # Calculate the best step size in powers of 10, so it will cover at