delmic/odemis

commit 323cd00d6aa510037b23303ac421d0b91d793443

driver actuator MultiplexActuator: fix very first position update not reported
The initial position was not using the _applyInversion(). So .position VA contained exactly
the ._position object. That meant that on the next update, the VA would already contain the new
value and so think that it's not worthy notifying.
On the following calls, as _applyInversion() was used, the object would be different, and this
wouldn't have the issue.

=> _applyInversion() at init too.

This fixes issues where the metadata would sometimes be incorrect if the hardware has just been moved once.
For instance, this could happen for the filter position of the SPARC with MD_OUT_WL.

src/odemis/driver/actuator.py
@@ -110,7 +110,7 @@ class MultiplexActuator(model.Actuator):
                 dependencies_axes[dep] = {axis}
 
         # position & speed: special VAs combining multiple VAs
-        self.position = model.VigilantAttribute(self._position, readonly=True)
+        self.position = model.VigilantAttribute(self._applyInversion(self._position), readonly=True)
         for c, ax in dependencies_axes.items():
 
             def update_position_per_dep(value, ax=ax, c=c):
@@ -186,7 +186,7 @@ class MultiplexActuator(model.Actuator):
         update the position VA
         """
         # it's read-only, so we change it via _value
-        pos = self._applyInversion(self._position)
+        pos = self._applyInversion(self._position)  # makes a copy
         logging.debug("reporting position %s", pos)
         self.position._set_value(pos, force_write=True)
 
@@ -194,9 +194,8 @@ class MultiplexActuator(model.Actuator):
         """
         update the speed VA
         """
-        # we must not call the setter, so write directly the raw value
-        self.speed._value = self._speed
-        self.speed.notify(self._speed)
+        # .speed is copied to detect changes to it on next update
+        self.speed.notify(self._speed.copy(), force_write=True)
 
     def _updateReferenced(self):
         """

commit 249eaadbbe5d6d80efb11dbf50d6e6f7d791bf53

driver actuator MultiplexActuator: fix speed not being updated
Typo introduced in commit 323cd00d6 (driver actuator MultiplexActuator: fix very first position update not reported).

src/odemis/driver/actuator.py
@@ -195,7 +195,7 @@ class MultiplexActuator(model.Actuator):
         update the speed VA
         """
         # .speed is copied to detect changes to it on next update
-        self.speed.notify(self._speed.copy(), force_write=True)
+        self.speed._set_value(self._speed.copy(), force_write=True)
 
     def _updateReferenced(self):
         """