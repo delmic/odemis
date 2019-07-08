c279fddc771df8ab2145031023357a853b482b08
Thera Pals <pals@delmic.com>
2019-07-08 15:35:20 +0200

    cleaned up simulator and tests
    - raise HwError when the hardware is not present
    - changed simulator to use a fake remote node and simulate not the entire focus tracker class
    - get version number from the hardware
    - merge simulator and hw test in to one and do not use bakcend
    - changed pid attributes to private attributes
    - added .eds to package_data in setup.py so that the focustracker.eds also gets packaged

--- src/odemis/driver/focustracker.py
+++ src/odemis/driver/focustracker.py
@@ -19,13 +19,18 @@ PURPOSE. See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along with
 Odemis. If not, see http://www.gnu.org/licenses/.
 """
-import logging
-import os
+from __future__ import division
 
+import numbers
+import pkg_resources
+
+import can
 import canopen  # TODO package canopen and add to project requirements.
 
-import odemis
 from odemis import model
+from odemis.model import HwError
+
+TARGET_POSITION_RANGE = (0, 100e-6)
 
 
 class FocusTrackerCO(model.HwComponent):
@@ -39,72 +44,86 @@ class FocusTrackerCO(model.HwComponent):
         model.HwComponent.__init__(self, name, role, **kwargs)
         # Connect to the CANbus and the CANopen network.
         self.network = canopen.Network()
-        self.network.connect(bustype='socketcan', channel=channel)
+        bustype = 'socketcan' if channel != 'fake' else 'virtual'
+        try:
+            self.network.connect(bustype=bustype, channel=channel)
+        except IOError as exp:
+            if exp.errno == 19:
+                raise HwError("Focus Tracker is not connected.")
+            else:
+                raise
         self.network.check()
-        self.node = canopen.RemoteNode(node_idx, os.path.dirname(odemis.__file__) + "/driver/FocusTracker.eds")
+        object_dict = pkg_resources.resource_filename("odemis.driver", "FocusTracker.eds")
+        if channel == 'fake':
+            self.node = FakeRemoteNode(node_idx, object_dict)
+        else:
+            self.node = canopen.RemoteNode(node_idx, object_dict)
         self.network.add_node(self.node)
+        self._swVersion = "python-canopen v%s , python-can v%s" % (canopen.__version__, can.__version__)
+        rev_num = self.node.sdo["Identity Object"]["Revision number"].raw
+        minor = rev_num & 0xffff
+        major = (rev_num >> 16) & 0xffff
+        self._hwVersion = "{}.{}".format(major, minor)
         # Create SDO communication objects to communicate
-        self._current_pos_sdo = self.node.sdo["AI Input PV"][1]
+        self._position_sdo = self.node.sdo["AI Input PV"][1]
         self._target_pos_sdo = self.node.sdo["CO Set Point W"][1]
 
         # Read PID gains from the device (and set the current metadata)
-        self.proportional_gain_sdo = self.node.sdo['CO Proportional Band Xp1'][1]
-        self.integral_gain_sdo = self.node.sdo['CO Integral Action Time Tn1'][1]
-        self.derivative_gain_sdo = self.node.sdo['CO Derivative Action Time Tv1'][1]
+        self._proportional_gain_sdo = self.node.sdo['CO Proportional Band Xp1'][1]
+        self._integral_gain_sdo = self.node.sdo['CO Integral Action Time Tn1'][1]
+        self._derivative_gain_sdo = self.node.sdo['CO Derivative Action Time Tv1'][1]
 
         self._tracking_sdo = self.node.sdo['Controller On/ Off'][1]
         # set VAs only if there is hardware, channel = '' if no hardware
-        if channel != '':
-            self._metadata[model.MD_GAIN_P] = self.proportional_gain_sdo.raw
-            self._metadata[model.MD_GAIN_I] = self.integral_gain_sdo.raw
-            self._metadata[model.MD_GAIN_I] = self.derivative_gain_sdo.raw
-            self.target_pos = model.FloatVA(self._target_pos_sdo.raw, unit="m", readonly=True,
-                                            getter=self.get_target_pos)
-            self.current_pos = model.FloatVA(self._current_pos_sdo.raw, unit="m", readonly=True,
-                                             getter=self.get_current_pos)
-            self.tracking = model.BooleanVA(self._tracking_sdo.raw, getter=self.get_tracking, setter=self.set_tracking)
+        self._metadata[model.MD_GAIN_P] = self._proportional_gain_sdo.raw
+        self._metadata[model.MD_GAIN_I] = self._integral_gain_sdo.raw
+        self._metadata[model.MD_GAIN_I] = self._derivative_gain_sdo.raw
+        self.targetPosition = model.FloatContinuous(self._target_pos_sdo.raw, TARGET_POSITION_RANGE, unit="m",
+                                                    getter=self._get_target_pos, setter=self._set_target_pos)
+        self.position = model.FloatVA(self._position_sdo.raw, unit="m", readonly=True, getter=self._get_position)
+        self.tracking = model.BooleanVA(self._tracking_sdo.raw, getter=self._get_tracking,
+                                        setter=self._set_tracking)
 
     def terminate(self):
         """Disconnect from CAN bus."""
         if self.network:
+            self._set_tracking(False)
             self.network.sync.stop()
             self.network.disconnect()
             self.network = None
 
     def updateMetadata(self, md):
         if model.MD_GAIN_P in md:
-            md[model.MD_GAIN_P] = self.set_proportional(md[model.MD_GAIN_P])
+            md[model.MD_GAIN_P] = self._set_proportional(md[model.MD_GAIN_P])
+            super(FocusTrackerCO, self).updateMetadata({model.MD_GAIN_P: md[model.MD_GAIN_P]})
         if model.MD_GAIN_I in md:
-            md[model.MD_GAIN_I] = self.set_integral(md[model.MD_GAIN_I])
+            md[model.MD_GAIN_I] = self._set_integral(md[model.MD_GAIN_I])
+            super(FocusTrackerCO, self).updateMetadata({model.MD_GAIN_I: md[model.MD_GAIN_I]})
         if model.MD_GAIN_D in md:
-            md[model.MD_GAIN_D] = self.set_derivative(md[model.MD_GAIN_D])
-        super(FocusTrackerCO, self).updateMetadata(md)
+            md[model.MD_GAIN_D] = self._set_derivative(md[model.MD_GAIN_D])
+            super(FocusTrackerCO, self).updateMetadata({model.MD_GAIN_D: md[model.MD_GAIN_D]})
 
-    def get_available_nodes(self):
-        """
-        return (list): a list of id's of nodes in the network
-        """
-        return self.network.scanner.nodes
-
-    def get_object_dictionary(self):
-        """
-        return (dict): the object dictionary of the focus tracker.
-        """
-        return self.node.object_dictionary
-
-    def get_current_pos(self):
+    def _get_position(self):
         """
         return (float): The current position of the laser on the linear ccd.
         """
-        return self._current_pos_sdo.raw * 1e-6
+        return self._position_sdo.raw * 1e-6
 
-    def get_target_pos(self):
+    def _get_target_pos(self):
         """
-        return (float): The target position of the laser on the linear ccd.
+        return (float): The target position in meters of the laser on the linear ccd.
         """
         return self._target_pos_sdo.raw * 1e-6
 
-    def set_tracking(self, value):
+    def _set_target_pos(self, value):
+        """
+        value (float, int): The target position in meters.
+        return (float): The target position of the laser on the linear ccd.
+        """
+        self._target_pos_sdo.raw = value * 1e6
+        return self._target_pos_sdo.raw
+
+    def _set_tracking(self, value):
         """
         value (boolean): True if the focus should be tracked, False if the focus should not be tracked.
         return (boolean): Same as input value.
@@ -112,98 +131,93 @@ class FocusTrackerCO(model.HwComponent):
         self._tracking_sdo.raw = value
         return value
 
-    def get_tracking(self):
+    def _get_tracking(self):
         """
         return (boolean): True if the focus is tracked, False if the focus is not tracked.
         """
         return self._tracking_sdo.raw
 
-    def set_proportional(self, value):
+    def _set_proportional(self, value):
         """
         value (int, float): Proportional gain value, can be any value and is rounded down to the nearest int
                             when a float is given.
         return (int): Same as input value, if input in correct range, else current gain.
         """
-        if isinstance(value, (float, int)):
-            self.proportional_gain_sdo.raw = value
+        if isinstance(value, numbers.Real) and value >= 0:
+            self._proportional_gain_sdo.raw = value
         else:
-            logging.info('value {} not accepted, only integers and floats accepted.'.format(value))
-            value = self.proportional_gain_sdo.raw
+            raise ValueError('value {} not accepted, only positive integers and floats accepted.'.format(value))
         return value
 
-    def get_proportional(self):
+    def _get_proportional(self):
         """
         return (int): current proportional gain.
         """
-        return self.proportional_gain_sdo.raw
+        return self._proportional_gain_sdo.raw
 
-    def set_integral(self, value):
+    def _set_integral(self, value):
         """
         value (int, float): Integral gain value, can be any value and is rounded down to the nearest int
                             when a float is given.
         return (int): Same as input value, if input in correct range, else current gain.
         """
-        if isinstance(value, (float, int)):
-            self.integral_gain_sdo.raw = value
+        if isinstance(value, numbers.Real) and value >= 0:
+            self._integral_gain_sdo.raw = value
         else:
-            logging.info('value {} not accepted, only integers and floats accepted.'.format(value))
-            value = self.proportional_gain_sdo.raw
+            raise ValueError('value {} not accepted, only positive integers and floats accepted.'.format(value))
         return value
 
-    def get_integral(self):
+    def _get_integral(self):
         """
         return (int): current integral gain.
         """
-        return self.integral_gain_sdo.raw
+        return self._integral_gain_sdo.raw
 
-    def set_derivative(self, value):
+    def _set_derivative(self, value):
         """
         value (int, float): Derivative gain value, can be any value and is rounded down to the nearest int
                             when a float is given.
         return (int): Same as input value, if input in correct range, else current gain.
         """
-        if isinstance(value, (float, int)):
-            self.derivative_gain_sdo.raw = value
+        if isinstance(value, numbers.Real) and value >= 0:
+            self._derivative_gain_sdo.raw = value
         else:
-            logging.info('value {} not accepted, only integers and floats accepted.'.format(value))
-            value = self.proportional_gain_sdo.raw
+            raise ValueError('value {} not accepted, only positive integers and floats accepted.'.format(value))
         return value
 
-    def get_derivative(self):
+    def _get_derivative(self):
         """
         return (boolean): current derivative gain.
         """
-        return self.derivative_gain_sdo.raw
+        return self._derivative_gain_sdo.raw
 
 
-class FocusTrackerCOSimulator(FocusTrackerCO):
-    def __init__(self, name, role, channel='', node_idx=0x10, **kwargs):
-        """
-        channel (str): channel name of can bus, '' for fake can bus.
-        node_idx (int): name of node of focus tracker
-        """
-        FocusTrackerCO.__init__(self, name, role, channel, node_idx, **kwargs)
-        object_dict = self.get_object_dictionary()
-        self._current_pos_sdo = FakeSDO(self._current_pos_sdo.sdo_node, object_dict['AI Input PV'], init_value=10)
-        self.current_pos = model.FloatVA(self._current_pos_sdo.raw, unit="m", readonly=True,
-                                         getter=self.get_current_pos)
-        self._target_pos_sdo = FakeSDO(self._target_pos_sdo.sdo_node, object_dict['CO Set Point W'], init_value=20)
-        self.target_pos = model.FloatVA(self._target_pos_sdo.raw, unit="m", readonly=True, getter=self.get_target_pos)
-        self._tracking_sdo = FakeSDO(self._tracking_sdo.sdo_node, object_dict['Controller On/ Off'], init_value=False)
-        self.tracking = model.BooleanVA(self._tracking_sdo.raw, getter=self.get_tracking, setter=self.set_tracking)
-        self.proportional_gain_sdo = FakeSDO(self.proportional_gain_sdo.sdo_node,
-                                             object_dict['CO Proportional Band Xp1'], init_value=10.2)
-        self.integral_gain_sdo = FakeSDO(self.integral_gain_sdo.sdo_node, object_dict['CO Integral Action Time Tn1'],
-                                         init_value=10.2)
-        self.derivative_gain_sdo = FakeSDO(self.derivative_gain_sdo.sdo_node,
-                                           object_dict['CO Derivative Action Time Tv1'], init_value=10.2)
+class FakeRemoteNode(canopen.RemoteNode):
+    def __init__(self, node_idx, object_dict):
+        canopen.RemoteNode.__init__(self, node_idx, object_dict)
+        sdo = SDODict()
+        sdo.update({
+            'AI Input PV': [0, FakeSDO(self.sdo["AI Input PV"][1], object_dict, init_value=10)],
+            'CO Set Point W': [0, FakeSDO(self.sdo['CO Set Point W'][1], object_dict, init_value=20)],
+            'Controller On/ Off': [0, FakeSDO(self.sdo['Controller On/ Off'][1], object_dict, init_value=False)],
+            'CO Proportional Band Xp1': [0, FakeSDO(self.sdo['CO Proportional Band Xp1'][1], object_dict,
+                                                    init_value=10.2)],
+            'CO Integral Action Time Tn1': [0, FakeSDO(self.sdo['CO Integral Action Time Tn1'][1], object_dict,
+                                                       init_value=10.2)],
+            'CO Derivative Action Time Tv1': [0, FakeSDO(self.sdo['CO Derivative Action Time Tv1'][1], object_dict,
+                                                         init_value=10.2)],
+            "Identity Object": {"Revision number": FakeSDO(self.sdo["Identity Object"]["Revision number"], object_dict,
+                                                           init_value=0x00010001)},
+            'network': None  # needed to add the node to the network
+        })
+        self.sdo = sdo
 
 
 class FakeSDO(canopen.sdo.base.Array):
     """Simulates an SDO object where the raw data can set and read from the canopen library."""
 
-    def __init__(self, object_sdo, object_dictionary, init_value):
-        canopen.sdo.base.Array.__init__(self, object_sdo, object_dictionary)
+    def __init__(self, object_sdo, object_dict, init_value):
+        canopen.sdo.base.Array.__init__(self, object_sdo, object_dict)
         self._raw = init_value
 
     @property
@@ -213,3 +227,10 @@ class FakeSDO(canopen.sdo.base.Array):
     @raw.setter
     def raw(self, value):
         self._raw = value
+
+
+class SDODict(dict):
+    """Creates a dictionary that can be accessed with dots."""
+
+    def __getattr__(self, name):
+        return self[name]
--- src/odemis/driver/test/focustracker_test.py
+++ src/odemis/driver/test/focustracker_test.py
@@ -26,162 +26,79 @@ import unittest
 
 from canopen import SdoCommunicationError
 
-import odemis
 from odemis import model
-from odemis.driver.focustracker import FocusTrackerCOSimulator, FocusTrackerCO
+from odemis.driver.focustracker import FocusTrackerCO
 from odemis.model import NotSettableError
-from odemis.util import test
 
 logging.basicConfig(level=logging.DEBUG)
 
-CONFIG_PATH = os.path.dirname(odemis.__file__) + "/../../install/linux/usr/share/odemis/"
-FAKE_FOCUS_TRACKER_CONFIG = CONFIG_PATH + "sim/focustracker-sim.odm.yaml"
-FOCUS_TRACKER_CONFIG = CONFIG_PATH + "hwtest/focustracker.odm.yaml"
+TEST_NOHW = (os.environ.get("TEST_NOHW", 0) != 0)  # Default to Hw testing
 
-
-class TestFocusTrackerCOSim(unittest.TestCase):
-    """Test the focus tracker functionality."""
-    backend_was_running = False
-
-    @classmethod
-    def setUpClass(cls):
-        try:
-            test.start_backend(FAKE_FOCUS_TRACKER_CONFIG)
-        except LookupError:
-            logging.info("A running backend is already found, skipping tests")
-            cls.backend_was_running = True
-            return
-        except IOError as exp:
-            logging.error(str(exp))
-            raise
-
-        # find components by their role
-        cls.fake_focus_tracker = model.getComponent(role="focus-tracker")
-
-    @classmethod
-    def tearDownClass(cls):
-        if cls.backend_was_running:
-            return
-        test.stop_backend()
-
-    def test_connection(self):
-        """Test an error is raised when trying to connect to can0 when the focus tracker is not connected."""
-        with self.assertRaises(IOError):
-            # should raise an error, because the focus tracker is not actually connected.
-            FocusTrackerCOSimulator(name="Focus Tracker", role="focus_tracker", channel='can0')
-
-    def test_object_dictionary(self):
-        """Verify that all relevant objects are in the object dictionary."""
-        object_names = ["AI Input PV", "CO Set Point W", "CO Proportional Band Xp1",
-                        "CO Integral Action Time Tn1", "CO Derivative Action Time Tv1"]
-        object_dictionary = self.fake_focus_tracker.get_object_dictionary()
-        self.assertTrue(all(n in object_dictionary.names for n in object_names))
-
-    def test_current_pos(self):
-        """Verify that the current position can be read and not written."""
-        self.assertAlmostEqual(self.fake_focus_tracker.current_pos.value, 1e-5)
-
-    def test_target_pos(self):
-        """Verify that the target position can be read and not written."""
-        self.assertAlmostEqual(self.fake_focus_tracker.target_pos.value, 2e-5)
-
-    def test_switch_tracking(self):
-        """Verify that the focus tracker can switch between tracking and untracking."""
-        self.fake_focus_tracker.tracking.value = True
-        self.assertTrue(self.fake_focus_tracker.tracking.value)
-        self.assertTrue(self.fake_focus_tracker.get_tracking())
-        self.fake_focus_tracker.tracking.value = False
-        self.assertFalse(self.fake_focus_tracker.tracking.value)
-        self.assertFalse(self.fake_focus_tracker.get_tracking())
-
-    def test_pid_gains(self):
-        """Test that PID gains are set when within range and remain unchanged when new gain is out of range."""
-        new_p = 0.5
-        new_i = 15
-        new_d = 3
-        self.fake_focus_tracker.updateMetadata({model.MD_GAIN_P: new_p, model.MD_GAIN_I: new_i, model.MD_GAIN_D: new_d})
-        set_p = self.fake_focus_tracker.get_proportional()
-        self.assertEqual(new_p, set_p)
-        set_i = self.fake_focus_tracker.get_integral()
-        self.assertEqual(new_i, set_i)
-        set_d = self.fake_focus_tracker.get_derivative()
-        self.assertEqual(new_d, set_d)
+if TEST_NOHW:
+    KWARGS = {"name": "Focus Tracker", "role": "focus-tracker", "node_idx": 0x10, "channel": 'fake'}
+else:
+    KWARGS = {"name": "Focus Tracker", "role": "focus-tracker", "node_idx": 0x10, "channel": 'can0'}
 
 
 class TestFocusTrackerCO(unittest.TestCase):
     """Test the focus tracker functionality with the CANopen network."""
-    backend_was_running = False
 
-    @classmethod
-    def setUpClass(cls):
-        try:
-            test.start_backend(FOCUS_TRACKER_CONFIG)
-        except LookupError:
-            logging.info("A running backend is already found, skipping tests")
-            cls.backend_was_running = True
-            return
-        except IOError as exp:
-            logging.error(str(exp))
-            raise
-
-        # find components by their role
-        cls.focus_tracker = model.getComponent(role="focus-tracker")
-
-    @classmethod
-    def tearDownClass(cls):
-        if cls.backend_was_running:
-            return
-        test.stop_backend()
+    def setUp(self):
+        self.focus_tracker = FocusTrackerCO(**KWARGS)
+        self.kwargs = KWARGS
 
     def test_connection(self):
         """Test that connection to the network is successful when connected, and unsuccessful when disconnected."""
-        # If the network scanner does not see a node at index 0x10, the focus tracker is not connected.
-        self.assertTrue(0x10 in self.focus_tracker.get_available_nodes())
+        if not TEST_NOHW:
+            # If the network scanner sees a node at index 0x10, the focus tracker is connected.
+            self.assertTrue(0x10 in self.focus_tracker.network.scanner.nodes)
+            # When the node_idx is incorrect an empty node is added to the network, this then results in an
+            # SDOCommunicationError when we try to send a message to an object in the object dictionary.
+            with self.assertRaises(SdoCommunicationError):
+                FocusTrackerCO(name="Focus Tracker", role="focus_tracker", channel=self.kwargs['channel'],
+                               node_idx=0x12)
         # If a fake channel name is entered, an IOError should be raised.
         with self.assertRaises(IOError):
-            FocusTrackerCO(name="Focus Tracker", role="focus_tracker", channel='not a channel', node_idx=0x10)
-        # When the node_idx is incorrect an empty node is added to the network, this then results in an
-        # SDOCommunicationError when we try to send a message to an object in the object dictionary.
-        with self.assertRaises(SdoCommunicationError):
-            FocusTrackerCO(name="Focus Tracker", role="focus_tracker", channel='can0', node_idx=0x12)
+            FocusTrackerCO(name="Focus Tracker", role="focus_tracker", channel='not a channel',
+                           node_idx=self.kwargs['node_idx'])
 
-    def test_object_dictionary(self):
-        """Verify that all relevant objects are in the object dictionary."""
-        object_names = ["AI Input PV", "CO Set Point W", "CO Proportional Band Xp1",
-                        "CO Integral Action Time Tn1", "CO Derivative Action Time Tv1"]
-        object_dictionary = self.focus_tracker.get_object_dictionary()
-        self.assertTrue(all(n in object_dictionary.names for n in object_names))
-
-    def test_current_pos(self):
+    def test_position(self):
         """Verify that the current position can be read and not written."""
-        self.assertIsInstance(self.focus_tracker.current_pos.value, float)
+        self.assertIsInstance(self.focus_tracker.position.value, float)
         with self.assertRaises(NotSettableError):
-            self.focus_tracker.current_pos.value = 10
+            self.focus_tracker.position.value = 10
 
     def test_target_pos(self):
         """Verify that the target position can be read and not written."""
-        self.assertIsInstance(self.focus_tracker.target_pos.value, float)
-        with self.assertRaises(NotSettableError):
-            self.focus_tracker.target_pos.value = 10
+        self.assertIsInstance(self.focus_tracker.targetPosition.value, float)
+        self.focus_tracker.targetPosition.value = 10e-6
+        self.assertAlmostEqual(self.focus_tracker.targetPosition.value, 10e-6)
+        with self.assertRaises(IndexError):
+            self.focus_tracker.targetPosition.value = -10e-6
 
     def test_switch_tracking(self):
         """Verify that the focus tracker can switch between tracking and untracking."""
         self.focus_tracker.tracking.value = True
         self.assertTrue(self.focus_tracker.tracking.value)
-        self.assertTrue(self.focus_tracker.get_tracking())
         self.focus_tracker.tracking.value = False
         self.assertFalse(self.focus_tracker.tracking.value)
-        self.assertFalse(self.focus_tracker.get_tracking())
 
     def test_pid_gains(self):
         """Test that PID gains are set when within range and remain unchanged when new gain is out of range."""
+        # Test setting all 3 values at the same time.
         new_p = 0
         new_i = 15.8
         new_d = 3
         self.focus_tracker.updateMetadata({model.MD_GAIN_P: new_p, model.MD_GAIN_I: new_i, model.MD_GAIN_D: new_d})
-        set_p = self.focus_tracker.get_proportional()
-        self.assertEqual(numpy.floor(new_p), set_p)
-        set_i = self.focus_tracker.get_integral()
-        self.assertEqual(numpy.floor(new_i), set_i)
-        set_d = self.focus_tracker.get_derivative()
-        self.assertEqual(numpy.floor(new_d), set_d)
+        set_p = self.focus_tracker._get_proportional()
+        self.assertEqual(numpy.floor(new_p), numpy.floor(set_p))
+        set_i = self.focus_tracker._get_integral()
+        self.assertEqual(numpy.floor(new_i), numpy.floor(set_i))
+        set_d = self.focus_tracker._get_derivative()
+        self.assertEqual(numpy.floor(new_d), numpy.floor(set_d))
+        # Test setting a single value.
+        self.focus_tracker.updateMetadata({model.MD_GAIN_I: 10})
+        self.assertEqual(10, self.focus_tracker._get_integral())
+        # check that a ValueError is raised when trying to set a negative value.
+        with self.assertRaises(ValueError):
+            self.focus_tracker.updateMetadata({model.MD_GAIN_P: -10})
