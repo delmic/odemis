delmic/odemis

commit 35d7752f88eac4bf94cfa385e7af453b0d36b449

Bug fix: ar bg was not applied to image when loading a new file and reading the previous bg
While running the updateImage method for AR it calls AngleResolved2Polar, which needs some time.
However, while still in this code, the background VA is updated and resets the polar_dict.
After AngleResolved2Polar, the updateImage writes again in this dict, but with the previous not
bg corrected image. Then updateImage is called again for the changed background VA. However,
the code will now find the previous not bg corrected image in the dict and does not update
the image for the bg as requested.
Fix: Make a copy of the link. When the polar_dict is overwritten/cleared, only the copy is
modified in _project2polar with the new image. Thus when the updateImage is called and thus
_project2Polar again, the polar_dict is still empty. If polar_dict is not modified in between the
link is still the same and polar_dict has the same image as the previous copied link is pointing to.

src/odemis/acq/stream/_static.py
@@ -447,8 +447,11 @@ class StaticARStream(StaticStream):
         pos (float, float, string or None): position (must be part of the ._pos)
         returns DataArray: the polar projection
         """
-        if pos in self._polar:
-            polard = self._polar[pos]
+        # Note: Need a copy of the link to the dict. If self._polar is reset while
+        # still running this method, the dict might get new entries again, though it should be empty.
+        polar = self._polar
+        if pos in polar:
+            polard = polar[pos]
         else:
             # Compute the polar representation
             data = self._pos[pos]
@@ -496,7 +499,7 @@ class StaticARStream(StaticStream):
                 polard = polar.AngleResolved2Polar(data0, size, hole=False, dtype=dtype)
 
                 # TODO: don't hold too many of them in cache (eg, max 3 * 1134**2)
-                self._polar[pos] = polard
+                polar[pos] = polard
             except Exception:
                 logging.exception("Failed to convert to azimuthal projection")
                 return data  # display it raw as fallback

commit 1bda0726766b3ee46cff09d88f26d888f7e09622

acq stream: Fix AR data projection
Commit 35d7752f88e (Bug fix: ar bg was not applied to image when loading) introduced a much bigger bug
which prevented the AR projection from ever happening.

src/odemis/acq/stream/_static.py
@@ -449,9 +449,9 @@ class StaticARStream(StaticStream):
         """
         # Note: Need a copy of the link to the dict. If self._polar is reset while
         # still running this method, the dict might get new entries again, though it should be empty.
-        polar = self._polar
-        if pos in polar:
-            polard = polar[pos]
+        polar_cache = self._polar
+        if pos in polar_cache:
+            polard = polar_cache[pos]
         else:
             # Compute the polar representation
             data = self._pos[pos]
@@ -499,7 +499,7 @@ class StaticARStream(StaticStream):
                 polard = polar.AngleResolved2Polar(data0, size, hole=False, dtype=dtype)
 
                 # TODO: don't hold too many of them in cache (eg, max 3 * 1134**2)
-                polar[pos] = polard
+                polar_cache[pos] = polard
             except Exception:
                 logging.exception("Failed to convert to azimuthal projection")
                 return data  # display it raw as fallback