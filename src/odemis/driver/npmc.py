65c96af8f1a92a0b150d9d81cd94057006c4343e
Philip Winkler <winkler@delmic.com>
2019-08-06 10:21:17 +0200

    python3 migration: driver npmc, .swVersion should be unicode

--- src/odemis/driver/npmc.py
+++ src/odemis/driver/npmc.py
@@ -176,7 +176,7 @@ class ESP(model.Actuator):
         self.referenced = model.VigilantAttribute({a: False for a in axes}, readonly=True)
 
         self._hwVersion = str(self._id)
-        self._swVersion = self._version
+        self._swVersion = self._version.decode('latin1')
 
         # Get the position in object coord with the offset applied.
 
