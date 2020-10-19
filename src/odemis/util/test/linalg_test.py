a5f7523b3258de073d38e86a4e04e60c41ca5e08
Éric Piel <piel@delmic.com>
2020-10-19 22:08:30 +0200

    add license headers
    A couple of files had their license headers missing.

--- src/odemis/util/test/linalg_test.py
+++ src/odemis/util/test/linalg_test.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python3
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
+"""
+@author: Andries Effting
+
+Copyright © 2020 Andries Effting, Delmic
+
+This file is part of Odemis.
+
+Odemis is free software: you can redistribute it and/or modify it under the terms
+of the GNU General Public License version 2 as published by the Free Software
+Foundation.
+
+Odemis is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+Odemis. If not, see http://www.gnu.org/licenses/.
+"""
 
 from __future__ import division
 import unittest
