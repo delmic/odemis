252a01719ace31be48dd7e5cbc991c0acfcec071
Éric Piel <piel@delmic.com>
2020-11-09 17:17:43 +0100

    prepare for v3.0.7

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ import sys
 import numpy
 
 # To be updated to the current version
-VERSION = "3.0.6"
+VERSION = "3.0.7"
 # We cannot use the git version because it's not (always) available when building
 # the debian package
 
