1acd67c177c4d25cdbd7d2b46ac30b99f2afce5b
Arthur Helsloot <helsloot@delmic.com>
2021-03-11 14:51:25 +0100

    Read and check version of 5DOF Smaract driver

--- src/odemis/driver/test/smaract_test.py
+++ src/odemis/driver/test/smaract_test.py
@@ -231,6 +231,12 @@ class Test5DOF(unittest.TestCase):
     def tearDownClass(cls):
         cls.dev.terminate()
 
+    def test_driver_software_version(self):
+        """
+        Checks whether the driver software version is valid
+        """
+        self.assertFalse("Unknown (Odemis" in self.dev.swVersion)
+
     def test_reference_cancel(self):
 
         # TODO: Still fails
