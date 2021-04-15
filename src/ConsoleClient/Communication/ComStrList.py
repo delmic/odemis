4f4bb4a2c65ee08ec962972a3c2fddb43adf0bf4
Arthur Helsloot <helsloot@delmic.com>
2021-04-15 09:05:43 +0200

    Initial orsay microscope file

---
+++ src/ConsoleClient/Communication/ComStrList.py
@@ -0,0 +1,23 @@
+"""Various predefined communciation syntax strings"""
+
+GetUpdatedStr="GetUpdated"
+GetFullDeviceStr="GetFullDevice"
+SetParametersStr="SetParameters"
+
+DoActionStr="DoAction" #return type should be void i think othwerise trouble in paradise
+GetData="GetData"
+SetData="SetData"
+SavePresetStr="SavePreset"
+LoadPresetStr="LoadPreset"
+RequestStr="Request"
+RequestTypeStr="RequestType"
+NameStr="name"
+
+Search="SEARCH"
+PostStr="POST"
+GetStr="GET"
+
+
+ConnectionString="Connection"
+ConfigurationString="Configuration"
+DeviceStr="Device"
