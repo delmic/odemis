73d399173e609abdeb661cd2d3f36453aa23eb01
Éric Piel <piel@delmic.com>
2021-05-21 18:22:44 +0200

    remove excption handling which doesn't do anything

--- src/odemis/driver/smaract.py
+++ src/odemis/driver/smaract.py
@@ -2549,15 +2549,12 @@ class MCS2(model.Actuator):
             self._set_accel(channel, accel)
             self._set_hold_time(channel, hold_time)
 
-            try:
-                # Log referencing mode, and warn if it's not normal (autozero)
-                ref_mode = self.GetProperty_i32(SA_CTLDLL.SA_CTL_PKEY_POSITIONER_TYPE, channel)
-                log_lvl = logging.INFO
-                if ref_mode & SA_CTLDLL.SA_CTL_REF_OPT_BIT_AUTO_ZERO:
-                    log_lvl = logging.WARNING
-                logging.log(log_lvl, "Current referencing mode = {}.".format(ref_mode))
-            except SA_CTLError:
-                    raise
+            # Log referencing mode, and warn if it's not normal (autozero)
+            ref_mode = self.GetProperty_i32(SA_CTLDLL.SA_CTL_PKEY_POSITIONER_TYPE, channel)
+            log_lvl = logging.INFO
+            if ref_mode & SA_CTLDLL.SA_CTL_REF_OPT_BIT_AUTO_ZERO:
+                log_lvl = logging.WARNING
+            logging.log(log_lvl, "Current referencing mode = {}.".format(ref_mode))
 
         self.position = model.VigilantAttribute({}, readonly=True)
 
