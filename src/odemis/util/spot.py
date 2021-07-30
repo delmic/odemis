adaaa851ffd9795f23801d476a5ec340f848fcb6
Thera Pals <46314774+tepals@users.noreply.github.com>
2021-07-30 12:30:44 +0200

    acq align FindGridSpots: added transformation method selection
    - Sometimes it is necessary that the returned grid is a perfect grid, with equal scaling in x and y and no shear. With the possibility to select the similarity method this is now possible.
    - Refactored the transformation calls to not use the deprecated call method, but use the apply method.
    - check added for the minimal number of spots in EstimateLatticeConstants and FindGridSpots.

--- src/odemis/util/spot.py
+++ src/odemis/util/spot.py
@@ -322,6 +322,7 @@ def EstimateLatticeConstant(pos):
     ----------
     pos : array like
         A 2D array of shape (N, 2) containing the (x, y) coordinates of the points.
+        Where N must be at least 5.
 
     Returns
     -------
@@ -330,6 +331,10 @@ def EstimateLatticeConstant(pos):
         the second row corresponds to the (x, y) direction of the second lattice constant.
 
     """
+    if len(pos) < 5:
+        # Because the lattice constants are based on the 4 nearest neighbours of a point,
+        # a total of 5 points is needed to estimate the lattice constants.
+        raise ValueError("Need at least 5 positions to estimate the lattice constants")
     # Find the closest 4 neighbours (excluding itself) for each point.
     tree = KDTree(pos)
     dd, ii = tree.query(pos, k=5)
