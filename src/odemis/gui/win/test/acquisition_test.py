18eb8b3b93cf6621b4a5b11aa82c864db58648a7
Mahmood <barazi@delmic.com>
2021-10-11 12:49:27 +0200

    refine test cases, and add some comments

--- src/odemis/gui/win/test/acquisition_test.py
+++ src/odemis/gui/win/test/acquisition_test.py
@@ -43,14 +43,23 @@ class TestgetROARectMethod(unittest.TestCase):
     """
     @classmethod
     def setUpClass(cls):
-        try:
-            test.start_backend(METEOR_CONFIG)
-        except LookupError:
-            logging.info("There is already running backend. It will be turned off, and the backend of METEOR will be turned on.")
-            test.stop_backend()
-            test.start_backend(METEOR_CONFIG)
-        except Exception:
-            raise
+        if driver.get_backend_status() in driver.BACKEND_RUNNING:
+            microscope = model.getMicroscope()
+            if microscope.role == "meteor":
+                logging.info("There is METEOR backend already running. It will be used.")
+            else:
+                logging.info("A running backend was found. It will be turned off, and the backend of METEOR will be turned on.")
+                try:
+                    test.stop_backend()
+                    test.start_backend(METEOR_CONFIG)
+                except Exception:
+                    raise
+        else:
+            logging.info("METEOR backend will be turned on.")
+            try:
+                test.start_backend(METEOR_CONFIG)
+            except Exception:
+                raise
 
         # get the stage components
         cls.stage = model.getComponent(role="stage")
@@ -64,10 +73,10 @@ class TestgetROARectMethod(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        if driver.get_backend_status in [BACKEND_STARTING, BACKEND_RUNNING]:
+        if driver.get_backend_status() == BACKEND_RUNNING:
             test.stop_backend()
 
-    def test_clipping_is_performed(self):
+    def test_clipping_is_performed_same_width_and_height(self):
         # move the stage close to the bottom left corner of the active range (200 micrometers 
         # # away from the corner in x and y directions).
         self.stage.moveAbs({"x": self.tiling_rng["x"][0]+200e-6,
@@ -75,13 +84,29 @@ class TestgetROARectMethod(unittest.TestCase):
         w = 600e-6
         h = 600e-6
         rect_pts = OverviewAcquisitionDialog.get_ROA_rect(w, h, self.stage.position.value, self.tiling_rng)
-        # check if the intersection is
+        # Note: the return rect_pts is LBRT assuming y axis upwards, or LTRB assuming y axis downwards.
+        # check if the intersection happened
         self.assertAlmostEqual(rect_pts[0], self.tiling_rng["x"][0])
         self.assertAlmostEqual(rect_pts[1], self.tiling_rng["y"][0])
         self.assertAlmostEqual(rect_pts[2], self.tiling_rng["x"][0] + 200e-6 + w/2)
         self.assertAlmostEqual(rect_pts[3], self.tiling_rng["y"][0] + 200e-6 + h/2)
 
-    def test_clipping_is_not_performed(self):
+    def test_clipping_is_performed_different_width_and_height(self):
+        # move the stage close to the bottom left corner of the active range (200 micrometers 
+        # # away from the corner in x and y directions).
+        self.stage.moveAbs({"x": self.tiling_rng["x"][0]+200e-6,
+                            "y": self.tiling_rng["y"][0]+200e-6}).result()
+        w = 700e-6
+        h = 600e-6
+        rect_pts = OverviewAcquisitionDialog.get_ROA_rect(w, h, self.stage.position.value, self.tiling_rng)
+        # Note: the return rect_pts is LBRT assuming y axis upwards, or LTRB assuming y axis downwards.
+        # check if the intersection happened
+        self.assertAlmostEqual(rect_pts[0], self.tiling_rng["x"][0])
+        self.assertAlmostEqual(rect_pts[1], self.tiling_rng["y"][0])
+        self.assertAlmostEqual(rect_pts[2], self.tiling_rng["x"][0] + 200e-6 + w/2)
+        self.assertAlmostEqual(rect_pts[3], self.tiling_rng["y"][0] + 200e-6 + h/2)
+
+    def test_clipping_is_not_performed_same_width_and_height(self):
         # move the stage to some position away from the edges of the active
         # range so that clipping is not needed. 
         stage_grid1_fm = transformFromSEMToMeteor(self.stage_grid1_sem, self.stage_bare)
@@ -90,7 +115,25 @@ class TestgetROARectMethod(unittest.TestCase):
         w = 600e-6
         h = 600e-6
         rect_pts = OverviewAcquisitionDialog.get_ROA_rect(w, h, pos, self.tiling_rng)
-        # check if the intersection area is the same as the tl and br
+        # Note: the return rect_pts is LBRT assuming y axis upwards, or LTRB assuming y axis downwards.
+        # check if the intersection area is the same as the tl and br of the requested area.
+        # tl and br are found by the current position +/- half of the width and height
+        self.assertAlmostEqual(rect_pts[0], pos["x"] - w/2)
+        self.assertAlmostEqual(rect_pts[1], pos["y"] - h/2)
+        self.assertAlmostEqual(rect_pts[2], pos["x"] + w/2)
+        self.assertAlmostEqual(rect_pts[3], pos["y"] + h/2)
+
+    def test_clipping_is_not_performed_different_width_and_height(self):
+        # move the stage to some position away from the edges of the active
+        # range so that clipping is not needed. 
+        stage_grid1_fm = transformFromSEMToMeteor(self.stage_grid1_sem, self.stage_bare)
+        self.stage_bare.moveAbs(stage_grid1_fm).result()
+        pos = self.stage.position.value
+        w = 500e-6
+        h = 600e-6
+        rect_pts = OverviewAcquisitionDialog.get_ROA_rect(w, h, pos, self.tiling_rng)
+        # Note: the return rect_pts is LBRT assuming y axis upwards, or LTRB assuming y axis downwards.
+        # check if the intersection area is the same as the tl and br of the requested area.
         # tl and br are found by the current position +/- half of the width and height
         self.assertAlmostEqual(rect_pts[0], pos["x"] - w/2)
         self.assertAlmostEqual(rect_pts[1], pos["y"] - h/2)
