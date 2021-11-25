delmic/odemis

commit 01bdf242d952700f638979902ade728c41dd6f01

gui: add support for meteor in chamber tab
Adjust the ENZEL chamber tab to also work on the METEOR.
As it's substantially different, we change the generic "cryo-secom" role to "enzel" and "meteor".

The chamber tab now allows to switch between SEM and FM. Also between two grids position.

Co-authored-by: Bassim Lazem <lazem@live.com>
Co-authored-by: Éric Piel <piel@delmic.com>

src/odemis/gui/model/__init__.py
@@ -153,6 +153,7 @@ class MainGUIData(object):
         "overview-ccd": "overview_ccd",
         "stage": "stage",
         "scan-stage": "scan_stage",
+        "stage-bare": "stage_bare",
         "focus": "focus",
         "spec-ded-focus": "spec_ded_focus",
         "pinhole": "pinhole",
@@ -206,6 +207,7 @@ class MainGUIData(object):
         self.ccd = None
         self.stage = None
         self.scan_stage = None  # fast stage to scan, instead of the ebeam (SPARC)
+        self.stage_bare = None # stage in the chamber referential 
         self.focus = None  # actuator to change the camera focus
         self.pinhole = None  # actuator to change the pinhole (confocal SECOM)
         self.aligner = None  # actuator to align ebeam/ccd (SECOM)
@@ -319,7 +321,7 @@ class MainGUIData(object):
 
             # Check for the most known microscope types that the basics are there
             required_roles = []
-            if self.role in ("secom", "delphi", "cryo-secom"):
+            if self.role in ("secom", "delphi", "enzel", "meteor"):
                 required_roles += ["e-beam", "light", "stage", "focus"]
                 if self.role == "secom":
                     required_roles += ["align", "se-detector"]
@@ -362,7 +364,7 @@ class MainGUIData(object):
                 # So the fine alignment dwell time should be at least 0.2 s.
                 self.fineAlignDwellTime.value = 0.5
 
-            if "cryo" in microscope.role:
+            if microscope.role in ["meteor", "enzel"]:
                 # List VA contains all the CryoFeatures
                 self.features = model.ListVA()
                 # VA for the currently selected feature
@@ -631,7 +633,7 @@ class CryoGUIData(MicroscopyGUIData):
     Represents an interface for handling cryo microscopes.
     """
     def __init__(self, main):
-        if "cryo" not in main.role:
+        if main.role not in ("enzel", "meteor"):
             raise ValueError(
                 "Expected a cryo microscope role but found it to be %s." % main.role)
         MicroscopyGUIData.__init__(self, main)
@@ -678,9 +680,9 @@ class CryoLocalizationGUIData(CryoGUIData):
     """
 
     def __init__(self, main):
-        if main.role != "cryo-secom":
+        if main.role not in ("enzel", "meteor"):
             raise ValueError(
-                "Microscope role was found to be %s, while expected 'cryo-secom'" % main.role)
+                "Microscope role was found to be %s, while expected 'enzel' or 'meteor'" % main.role)
         CryoGUIData.__init__(self, main)
 
         # Current tool selected (from the toolbar)
@@ -1901,6 +1903,8 @@ class MicroscopeView(StreamView):
     """
     def __init__(self, name, stage=None, **kwargs):
         StreamView.__init__(self, name, stage=stage, **kwargs)
+        # booleanVA to toggle showing/hiding the features
+        self.showFeatures = model.BooleanVA(True)
         if stage:
             self.stage_pos.subscribe(self._on_stage_pos)
 
@@ -1976,7 +1980,7 @@ class FeatureView(StreamView):
         # booleanVA to toggle showing/hiding the features
         self.showFeatures = model.BooleanVA(True)
 
-class FeatureOverviewView(StreamView):
+class FeatureOverviewView(FeatureView):
     """
     A large FoV view which is used to display an overview map with optional bookmarked features
     """

src/odemis/gui/util/__init__.py
@@ -289,8 +289,7 @@ def enable_tab_on_stage_position(button, stage, pos, target):
     """
     Enable the given tab button if the stage is in target position, disable it otherwise
     :param button: (Button) the Tab button to enable/disable
-    :param stage: (Actuator) the stage driver
     :param pos: (dict str->float) current position to check its label
-    :param target: (int) target position label (IMAGING, LOADING..etc)
+    :param target: (list) target position labels for which the tab button is enabled [IMAGING, FM_IMAGING]
     """
-    button.Enable(getCurrentPositionLabel(pos, stage) == target)
+    button.Enable(getCurrentPositionLabel(pos, stage) in target)

install/linux/usr/share/odemis/sim/enzel-sim.odm.yaml
@@ -1,6 +1,6 @@
 ENZEL-Sim: {
     class: Microscope,
-    role: cryo-secom,
+    role: enzel,
     children: [
         "Sample Thermostat", "Cooler Controller",
         "SEM E-beam Full", "SEM Detector",

install/linux/usr/share/odemis/sim/meteor-sim.odm.yaml
@@ -1,0 +1,202 @@
+# For now, this is esssentially just a FM microscope, as the FIB part is handled
+# separately by the SEM
+
+METEOR-Sim: {
+    class: Microscope,
+    role: meteor,
+    children: ["Optical Objective",
+               "Optical Focus",
+               "Light Source", "Filter Wheel", "Camera",
+               "Linked Stage", "EBeam",
+    ],
+}
+
+"SEM": {
+    class: simsem.SimSEM,
+    role: null,
+    init: {
+           image: "simsem-fake-output.h5", # any large 16 bit image is fine
+    },
+    children: {scanner: "EBeam",
+               detector0: "SE Detector",
+               focus: "EBeam Focus",
+    }
+}
+
+"EBeam": {
+    # Internal child of SimSEM, so no class
+    role: e-beam,
+    init: {},
+    affects: ["BSED Phenom", "Camera"], # affects the CCD in case of cathodoluminescence
+}
+
+"EBeam Focus": {
+    role: ebeam-focus,
+    init: {},
+    affects: ["EBeam"]
+}
+
+"SE Detector": {
+    # Internal child of SimSEM, so no class
+    role: se-detector,
+    init: {},
+}
+
+# Normally provided by the SEM
+"Stage": {
+    class: tmcm.TMCLController,
+    role: stage-bare,
+    init: {
+        port: "/dev/fake6",
+        address: null,
+        axes: ["x", "y", "z", "rx", "rz"],
+        ustepsize: [1.e-7, 1.e-7, 1.e-7, 1.2e-5, 1.2e-5], # unit/µstep
+        rng: [[-0.1, 0.1], [-0.05, 0.05], [0, 0.1], [-2, 2], [0, 6.28]],
+        unit: ["m", "m", "m", "rad", "rad"],
+        refproc: "Standard",
+    },
+    metadata: {
+        FAV_POS_DEACTIVE: { 'rx': 0, 'rz': 1.9076449, 'x': -0.01529, 'y': 0.0506, 'z': 0.01975 },
+        SEM_IMAGING_RANGE: {"x": [-0.0049, 0.0048], "y": [0.0004, 0.0044], "z": [0.0055, 0.0075]},
+        FM_IMAGING_RANGE: {"x": [0.044, 0.054], "y": [-0.0044, 0.0045], "z": [0.0046, 0.0087]},
+        SAMPLE_CENTERS: {"GRID 1": {'x': -0.003, 'y':  0.0025, 'z': 0.0066}, "GRID 2": {'x': 0.003, 'y': 0.0023, 'z': 0.0066}},
+        # Note that these values change every session 
+        POS_COR: { "x": 0.02447, "y": -0.000017 },
+        # Active tilting & rotation angles positions when switching between SEM & FM
+        # Note: these values are calibrated at installation time 
+        FAV_FM_POS_ACTIVE: {"rx": 0.122173048  , "rz":  5.06145483},
+        FAV_SEM_POS_ACTIVE: {"rx": 0.34906585, "rz": 1.91986218}    # Note that milling angle (rx) can be changed per session 
+    },
+}
+
+"Linked Stage": {
+    class: actuator.LinkedAxesActuator,
+    role: stage,
+    dependencies: {
+        "stage": "Stage",
+    },
+    affects: ["Camera", "EBeam"],
+    metadata: {
+        # Matrix to convert from X/Y to X/Y/Z (on the underlying stage)
+        # xd = POS_COR[0] + CALIB[0,0] * x + CALIB[0,1] * y
+        # yd = POS_COR[1] + CALIB[1,0] * x + CALIB[1,1] * y
+        # zd = POS_COR[2] + CALIB[2,0] * x + CALIB[2,1] * y
+        CALIB: [[1, 0],  [0, 0.6941955197522921], [0, -0.7197864824764667]],
+        POS_COR: [0, -0.0007363771884364807, 0.004945901888925916],
+        # Allowed range when in FM imaging mode. The X/Y ranges are also used
+        # as default area for the overview acquisition. The values must be in the
+        # stage coordinates, so converted from the SEM stage (in Y).
+        POS_ACTIVE_RANGE: {"x": [0.044, 0.054], "y": [-0.0044, 0.0045],},
+    },
+}
+
+"Light Source": {
+    class: lle.FakeLLE,
+    role: light,
+    init: {
+        port: "/dev/ttyUSB*",
+        # source name -> 99% low, 25% low, centre, 25% high, 99% high wavelength in m
+        # Values are from vendor: http://lumencor.com/products/filters-for-spectra-x-light-engines/
+        sources: {"UV": [379.e-9, 384.e-9, 390.e-9, 396.e-9, 401.e-9], # 390/22
+                  "cyan": [472.e-9, 479.e-9, 485.e-9, 491.e-9, 497.e-9], # 485/25
+                  "green": [544.e-9, 552.e-9, 560.e-9, 568.e-9, 576.e-9], # 560/32
+                  "red": [638.e-9, 643.e-9, 648.e-9, 653.e-9, 658.e-9], # 648/20
+                 }
+        },
+        # The light is reflected via a Semrock FF410/504/582/669-DI01-25X36
+    affects: ["Camera"],
+}
+
+"Optical Objective": {
+    class: static.OpticalLens,
+    role: lens,
+    init: {
+        mag: 84.0, # ratio, (actually of the complete light path)
+        na: 0.85, # ratio, numerical aperture
+        ri: 1.0, # ratio, refractive index
+    },
+    affects: ["Camera"]
+}
+
+# Normally a IDS uEye
+# Axes: X is horizontal on screen (going left->right), physical: far->close when looking at the door
+#       Y is vertical on screen (going bottom->top), physical: left->right when looking at the door
+"Camera": {
+    class: andorcam2.AndorCam2,
+    role: ccd,
+    init: {
+        device: "fake",
+        image: "andorcam2-fake-clara.tiff",
+        transp: [-1, -2], # To swap/invert axes
+    },
+}
+
+# Controller for the filter-wheel
+# DIP must be configured with address 7 (= 1110000)
+"Optical Actuators": {
+    class: tmcm.TMCLController,
+    role: null,
+    init: {
+        port: "/dev/fake3",
+        address: null,
+        axes: ["fw"],
+        # TODO: check step size, so that 6.28 rad == one full rotation
+        ustepsize: [1.227184e-6], # [rad/µstep]
+        rng: [[-14, 7]], # rad, more than 0->2 Pi, in order to allow one extra rotation in both direction, when quickly switching
+        unit: ["rad"],
+        refproc: "Standard",
+        refswitch: {"fw": 0}, #digital output used to switch on/off sensor
+        inverted: ["fw"], # for the filter wheel, the direction doesn't matter, as long as the positions are correct
+    },
+}
+
+"Filter Wheel": {
+    class: actuator.FixedPositionsActuator,
+    role: filter,
+    dependencies: {"band": "Optical Actuators"},
+    init: {
+        axis_name: "fw",
+        # This filter-wheel is made so that the light goes through two "holes":
+        # the filter, and the opposite hole (left empty). So although it has 8
+        # holes, it only supports 4 filters (from 0° to 135°), and there is no
+        # "fast-path" between the last filter and the first one.
+        positions: {
+             # pos (rad) -> m,m
+             0.08: [510.e-9, 540.e-9], # FF01-440/40-25
+             0.865398: [532.e-9, 552.e-9], # FF01-525/30-25
+             1.650796: [589.e-9, 625.e-9], # FF01-607/36-25
+             2.4361944: [672.e-9, 696.e-9], # FF02-684/24-25
+        },
+        cycle: 6.283185, # position of ref switch (0) after a full turn
+    },
+    # TODO: a way to indicate the best filter to use during alignement and brightfield? via some metadata?
+    affects: ["Camera"],
+}
+
+
+# CLS3252dsc-1
+"Optical Focus": {
+    class: smaract.MCS2,
+    role: focus,
+    affects: ["Camera"],
+    init: {
+        locator: "fake",
+        ref_on_init: True,
+        # TODO: check speed/accel
+        speed: 0.003,  # m/s
+        accel: 0.003,  # m/s²
+        #hold_time: 5 # s, default = infinite
+        # TODO: check the ranges, and the channel
+        axes: {
+            'z': {
+                range: [-10.e-3, 8.e-3],
+                unit: 'm',
+                channel: 0,
+            },
+        },
+    },
+    metadata: {
+        FAV_POS_DEACTIVE: {'z': 0.002},
+        FAV_POS_ACTIVE: {'z': 0.00}
+    },
+}

src/odemis/acq/test/move_test.py
@@ -21,19 +21,24 @@ import os
 import time
 import unittest
 
+import scipy, numpy
+
 import odemis
 from odemis import model
 from odemis import util
-from odemis.acq.move import ATOL_LINEAR_POS, ATOL_ROTATION_POS, RTOL_PROGRESS, cryoSwitchAlignPosition, \
-    getCurrentAlignerPositionLabel, SEM_IMAGING, UNKNOWN
+from odemis.acq.move import cryoSwitchAlignPosition, getCurrentAlignerPositionLabel, _getDistance, SCALING_FACTOR, getRotationMatrix
 from odemis.acq.move import LOADING, IMAGING, ALIGNMENT, COATING, LOADING_PATH
+from odemis.acq.move import ATOL_LINEAR_POS, ATOL_ROTATION_POS, FM_IMAGING, GRID_1, GRID_2, RTOL_PROGRESS, SEM_IMAGING, UNKNOWN, getCurrentGridLabel
+from odemis.util.driver import BACKEND_RUNNING
 from odemis.acq.move import cryoTiltSample, cryoSwitchSamplePosition, getMovementProgress, getCurrentPositionLabel
-from odemis.util import test
+from odemis.util import driver, test
+import threading
 
 logging.getLogger().setLevel(logging.DEBUG)
 
 CONFIG_PATH = os.path.dirname(odemis.__file__) + "/../../install/linux/usr/share/odemis/"
 ENZEL_CONFIG = CONFIG_PATH + "sim/enzel-sim.odm.yaml"
+METEOR_CONFIG = CONFIG_PATH + "sim/meteor-sim.odm.yaml"
 
 
 class TestCryoMove(unittest.TestCase):
@@ -366,6 +371,288 @@ class TestCryoMove(unittest.TestCase):
         cryoSwitchSamplePosition(LOADING).result()
         test.assert_pos_almost_equal(self.stage.position.value, self.stage_deactive,
                                      atol=ATOL_LINEAR_POS)
+
+
+class TestMeteorMove(unittest.TestCase):
+    """
+    Test the function cryoSwitchSamplePosition stage movements of meteor 
+    """
+    @classmethod
+    def setUpClass(cls):
+        if driver.get_backend_status() in driver.BACKEND_RUNNING:
+            microscope = model.getMicroscope()
+            if microscope.role != "meteor":
+                logging.info("There is already running backend. It will be turned off, and the backend of METEOR will be turned on.")
+                test.stop_backend()
+                test.start_backend(METEOR_CONFIG)
+            else:
+                logging.info("There is METEOR backend already running. It will be used.")
+        else:
+            try:
+                logging.info("METEOR backend will be turned on.")
+                test.start_backend(METEOR_CONFIG)
+            except Exception:
+                raise
+
+        # get the stage components
+        cls.stage = model.getComponent(role="stage-bare")
+
+        # get the metadata
+        stage_md = cls.stage.getMetadata()
+        cls.stage_grid_centers = stage_md[model.MD_SAMPLE_CENTERS]
+        cls.stage_loading = stage_md[model.MD_FAV_POS_DEACTIVE]
+
+    @classmethod
+    def tearDownClass(cls):
+        if driver.get_backend_status == BACKEND_RUNNING:
+            test.stop_backend()
+
+    def test_moving_to_grid1_in_sem_imaging_area_after_loading_1st_method(self):
+        # move the stage to the loading position  
+        f = cryoSwitchSamplePosition(LOADING)
+        f.result()
+        # move the stage to the sem imaging area, and grid1 will be chosen by default.
+        f = cryoSwitchSamplePosition(SEM_IMAGING)
+        f.result()
+        position_label = getCurrentPositionLabel(self.stage.position.value, self.stage)
+        grid_label = getCurrentGridLabel(self.stage.position.value, self.stage)
+        self.assertEqual(position_label, SEM_IMAGING)
+        self.assertEqual(grid_label, GRID_1)
+
+    def test_moving_to_grid1_in_sem_imaging_area_after_loading_2nd_method(self):
+        # move the stage to the loading position  
+        f = cryoSwitchSamplePosition(LOADING)
+        f.result()
+        # move the stage to grid1, and sem imaging area will be chosen by default. 
+        f = cryoSwitchSamplePosition(GRID_1)
+        f.result()
+        position_label = getCurrentPositionLabel(self.stage.position.value, self.stage)
+        grid_label = getCurrentGridLabel(self.stage.position.value, self.stage)
+        self.assertEqual(position_label, SEM_IMAGING)
+        self.assertEqual(grid_label, GRID_1)
+
+    def test_moving_to_grid1_in_fm_imaging_area_after_loading(self):
+        # move the stage to the loading position  
+        f = cryoSwitchSamplePosition(LOADING)
+        f.result()
+        # move the stage to the fm imaging area, and grid1 will be chosen by default
+        f = cryoSwitchSamplePosition(FM_IMAGING)
+        f.result()
+        position_label = getCurrentPositionLabel(self.stage.position.value, self.stage)
+        grid_label = getCurrentGridLabel(self.stage.position.value, self.stage)
+        self.assertEqual(position_label, FM_IMAGING)
+        self.assertEqual(grid_label, GRID_1)
+
+    def test_moving_to_grid2_in_sem_imaging_area_after_loading(self):
+        # move the stage to the loading position  
+        f = cryoSwitchSamplePosition(LOADING)
+        f.result()
+        # move the stage to grid2
+        f = cryoSwitchSamplePosition(GRID_2)
+        f.result()
+        position_label = getCurrentPositionLabel(self.stage.position.value, self.stage)
+        grid_label = getCurrentGridLabel(self.stage.position.value, self.stage)
+        self.assertEqual(position_label, SEM_IMAGING)
+        self.assertEqual(grid_label, GRID_2)
+
+    def test_moving_from_grid1_to_grid2_in_sem_imaging_area(self):
+        # move to loading position
+        f = cryoSwitchSamplePosition(LOADING)
+        f.result()
+        # move the stage to the sem imaging area
+        f = cryoSwitchSamplePosition(SEM_IMAGING)
+        f.result()
+        current_imaging_mode = getCurrentPositionLabel(self.stage.position.value, self.stage)
+        self.assertEqual(SEM_IMAGING, current_imaging_mode)
+        # now the selected grid is already the grid1
+        current_grid = getCurrentGridLabel(self.stage.position.value, self.stage)
+        self.assertEqual(GRID_1, current_grid)
+        # move the stage to grid2 
+        f = cryoSwitchSamplePosition(GRID_2)
+        f.result()
+        current_grid = getCurrentGridLabel(self.stage.position.value, self.stage)
+        self.assertEqual(GRID_2, current_grid)
+        # make sure we are still in sem  imaging area 
+        current_imaging_mode = getCurrentPositionLabel(self.stage.position.value, self.stage)
+        self.assertEqual(SEM_IMAGING, current_imaging_mode)
+
+    def test_moving_from_grid2_to_grid1_in_sem_imaging_area(self):
+        # move to loading position
+        f = cryoSwitchSamplePosition(LOADING)
+        f.result()
+        # move the stage to the sem imaging area
+        f = cryoSwitchSamplePosition(SEM_IMAGING)
+        f.result()
+        current_imaging_mode = getCurrentPositionLabel(self.stage.position.value, self.stage)
+        self.assertEqual(SEM_IMAGING, current_imaging_mode)
+        # move the stage to grid2 
+        f = cryoSwitchSamplePosition(GRID_2)
+        f.result()
+        current_grid = getCurrentGridLabel(self.stage.position.value, self.stage)
+        self.assertEqual(GRID_2, current_grid)
+        # move the stage back to grid1
+        f = cryoSwitchSamplePosition(GRID_1)
+        f.result()
+        current_grid = getCurrentGridLabel(self.stage.position.value, self.stage)
+        self.assertEqual(GRID_1, current_grid)
+        # make sure we are still in the sem imaging area
+        current_imaging_mode = getCurrentPositionLabel(self.stage.position.value, self.stage)
+        self.assertEqual(SEM_IMAGING, current_imaging_mode)
+    
+    def test_moving_from_sem_to_fm(self):
+        # move to loading position
+        f = cryoSwitchSamplePosition(LOADING)
+        f.result()
+        # move the stage to the sem imaging area
+        f = cryoSwitchSamplePosition(SEM_IMAGING)
+        f.result()
+        current_imaging_mode = getCurrentPositionLabel(self.stage.position.value, self.stage)
+        self.assertEqual(SEM_IMAGING, current_imaging_mode)
+        # move to the fm imaging area
+        f = cryoSwitchSamplePosition(FM_IMAGING)
+        f.result()
+        current_imaging_mode = getCurrentPositionLabel(self.stage.position.value, self.stage)
+        self.assertEqual(FM_IMAGING, current_imaging_mode)
+
+    def test_moving_from_grid1_to_grid2_in_fm_imaging_Area(self):
+        f = cryoSwitchSamplePosition(LOADING)
+        f.result()
+        # move to the fm imaging area
+        f = cryoSwitchSamplePosition(FM_IMAGING)
+        f.result()
+        current_imaging_mode = getCurrentPositionLabel(self.stage.position.value, self.stage)
+        self.assertEqual(FM_IMAGING, current_imaging_mode)
+        # now the grid is grid1 by default
+        current_grid = getCurrentGridLabel(self.stage.position.value, self.stage)
+        self.assertEqual(GRID_1, current_grid)
+        # move to the grid2
+        f = cryoSwitchSamplePosition(GRID_2)
+        f.result()
+        current_grid = getCurrentGridLabel(self.stage.position.value, self.stage)
+        self.assertEqual(GRID_2, current_grid)
+        # make sure we are still in fm imaging area
+        current_imaging_mode = getCurrentPositionLabel(self.stage.position.value, self.stage)
+        self.assertEqual(FM_IMAGING, current_imaging_mode)
+
+    def test_moving_from_grid2_to_grid1_in_fm_imaging_Area(self):
+        f = cryoSwitchSamplePosition(LOADING)
+        f.result()
+        # move to the fm imaging area
+        f = cryoSwitchSamplePosition(FM_IMAGING)
+        f.result()
+        current_imaging_mode = getCurrentPositionLabel(self.stage.position.value, self.stage)
+        self.assertEqual(FM_IMAGING, current_imaging_mode)
+        # move to the grid2
+        f = cryoSwitchSamplePosition(GRID_2)
+        f.result()
+        current_grid = getCurrentGridLabel(self.stage.position.value, self.stage)
+        self.assertEqual(GRID_2, current_grid)
+        # move back to the grid1 
+        f = cryoSwitchSamplePosition(GRID_1)
+        f.result()
+        current_grid = getCurrentGridLabel(self.stage.position.value, self.stage)
+        self.assertEqual(GRID_1, current_grid)
+        # make sure we are still in fm imaging area
+        current_imaging_mode = getCurrentPositionLabel(self.stage.position.value, self.stage)
+        self.assertEqual(FM_IMAGING, current_imaging_mode)
+
+    def test_moving_to_sem_from_fm(self):
+        f = cryoSwitchSamplePosition(LOADING)
+        f.result()
+        # move to the fm imaging area
+        f = cryoSwitchSamplePosition(FM_IMAGING)
+        f.result()
+        current_imaging_mode = getCurrentPositionLabel(self.stage.position.value, self.stage)
+        self.assertEqual(FM_IMAGING, current_imaging_mode)
+        # move to sem
+        f = cryoSwitchSamplePosition(SEM_IMAGING)
+        f.result()
+        current_imaging_mode = getCurrentPositionLabel(self.stage.position.value, self.stage)
+        self.assertEqual(SEM_IMAGING, current_imaging_mode)
+
+    def test_unknown_label_at_initialization(self):
+        arbitrary_position = {"x": 0.0, "y": 0.0, "z": 0.0}
+        self.stage.moveAbs(arbitrary_position).result()
+        current_imaging_mode = getCurrentPositionLabel(self.stage.position.value, self.stage)
+        self.assertEqual(UNKNOWN, current_imaging_mode)
+        current_grid = getCurrentGridLabel(self.stage.position.value, self.stage)
+        self.assertEqual(current_grid, None)
+
+
+class TestGetDifferenceFunction(unittest.TestCase):
+    """
+    This class is to test _getDistance() function in the move module
+    """
+    def test_only_linear_axes(self):
+        point1 = {'x': 0.023, 'y': 0.032, 'z': 0.01}
+        point2 = {'x': 0.082, 'y': 0.01, 'z': 0.028}
+        pos1 = numpy.array([point1[a] for a in list(point1.keys())])
+        pos2 = numpy.array([point2[a] for a in list(point2.keys())])
+        expected_distance = scipy.spatial.distance.euclidean(pos1, pos2)
+        actual_distance = _getDistance(point1, point2)
+        self.assertAlmostEqual(expected_distance, actual_distance)
+
+    def test_only_linear_axes_but_without_difference(self):
+        point1 = {'x': 0.082, 'y': 0.01, 'z': 0.028}
+        point2 = {'x': 0.082, 'y': 0.01, 'z': 0.028}
+        expected_distance = 0
+        actual_distance = _getDistance(point1, point2)
+        self.assertAlmostEqual(expected_distance, actual_distance)
+
+    def test_only_linear_axes_but_without_common_axes(self):
+        point1 = {'x': 0.023, 'y': 0.032}
+        point2 = {'x': 0.023, 'y': 0.032, 'z': 1}
+        common_axes = point1.keys() & point2.keys()
+        pos1 = numpy.array([point1[a] for a in common_axes])
+        pos2 = numpy.array([point2[a] for a in common_axes])
+        expected_distance = scipy.spatial.distance.euclidean(pos1, pos2)
+        actual_distance = _getDistance(point1, point2)
+        self.assertAlmostEqual(expected_distance, actual_distance)
+
+    def test_only_rotation_axes(self):
+        point1 = {'rx': 0.523599, 'rz': 0} # 30 degree
+        point2 = {'rx': 1.0472, 'rz': 0}    # 60 degree
+        # the rotation difference is 30 degree
+        expected_rotation = getRotationMatrix("rx", numpy.radians(30))
+        exp_rot_error = SCALING_FACTOR*numpy.trace(numpy.eye(3)-expected_rotation)
+        act_rot_error = _getDistance(point2, point1)
+        self.assertAlmostEqual(exp_rot_error, act_rot_error, places=5)
+
+    def test_only_rotation_axes_but_whtout_difference(self):
+        point1 = {'rx': 0, 'rz': 0.523599} # 30 degree
+        point2 = {'rx': 0, 'rz': 0.523599}  # 30 degree
+        # the rotation difference is 0 degree
+        exp_rot_error = 0
+        act_rot_error = _getDistance(point2, point1)
+        self.assertAlmostEqual(exp_rot_error, act_rot_error)
+
+    def test_only_rotation_axes_but_whtout_common_axes(self):
+        point1 = {'rx': 0, 'rz': 0.523599} # 30 degree
+        point2 = {'rz': 1.0472}  # 60 degree
+        # the rotation difference is 30 degree
+        expected_rotation = getRotationMatrix("rz", numpy.radians(30))
+        exp_rot_error = SCALING_FACTOR*numpy.trace(numpy.eye(3)-expected_rotation)
+        act_rot_error = _getDistance(point2, point1)
+        self.assertAlmostEqual(exp_rot_error, act_rot_error, places=5)
+
+    def test_no_common_axes(self):
+        point1 = {'rx': 0.523599, 'rz': 0.523599}
+        point2 = {'x': 0.082, 'y': 0.01}
+        self.assertRaises(ValueError, _getDistance, point1, point2)
+
+    def test_both_axes(self):
+        point1 = {'rx': 0, 'rz': 0.523599, 'x': -0.01529, 'y': 0.0506, 'z': 0.01975}
+        point2 = {'rx': 0, 'rz': 1.0472, 'x': -0.01529, 'y': 0.0506, 'z': 0.01975}
+        lin_axes = {'x', 'y', 'z'}  
+        rot_axes = {'rx', 'rz'}
+        pos1 = numpy.array([point1[a] for a in lin_axes])
+        pos2 = numpy.array([point2[a] for a in lin_axes])
+        exp_lin_error = scipy.spatial.distance.euclidean(pos1, pos2)
+        # the rotation difference is 30 degree
+        expected_rotation = getRotationMatrix("rz", numpy.radians(30))
+        exp_rot_error = SCALING_FACTOR*numpy.trace(numpy.eye(3)-expected_rotation)
+        act_error = _getDistance(point1, point2)
+        self.assertAlmostEqual(act_error, exp_rot_error+exp_lin_error, places=6)
 
 
 if __name__ == "__main__":

src/odemis/gui/comp/overlay/world.py
@@ -181,7 +181,12 @@ class CryoFeatureOverlay(StagePointSelectOverlay, DragMixin):
                 self._mode = MODE_SHOW_FEATURES
 
     def _on_current_feature_va(self, _):
-        # refresh the canvas on _on_current_feature_va change
+        # refresh the canvas on _on_current_feature_va change & when status is changed
+        self.cnvs.update_drawing()
+        if self.tab_data.main.currentFeature.value:
+            self.tab_data.main.currentFeature.value.status.subscribe(self._on_status_change, init=True)
+
+    def _on_status_change(self, _):
         self.cnvs.update_drawing()
 
     def _on_features_changes(self, _):

src/odemis/gui/cont/features.py
@@ -80,6 +80,8 @@ class CryoFeatureController(object):
         :param features: list(CryoFeature) list of modified features
         """
         if not features:
+            self._panel.cmb_features.Clear()
+            self._on_current_feature_changes(None)
             return
         save_features(self._tab.conf.pj_last_path, self._tab_data_model.main.features)
         self._panel.cmb_features.Clear()
@@ -110,6 +112,7 @@ class CryoFeatureController(object):
         if not feature:
             enable_feature_ctrls(False)
             self._panel.cmb_features.SetValue("No Feature Selected")
+            self._panel.cmb_feature_status.SetValue("Not Selected")
             return
         save_features(self._tab.conf.pj_last_path, self._tab_data_model.main.features)
 

commit 7c25e6a18c192112e1af9052dd3e55441943b134

gui model: don't show feature toggle button on every view
Not every viewport supports it. So it should only be on the FeatureView
or FeatureOverviewView.

Mistake introduced in commit 01bdf242d9 (gui: add support for meteor in
chamber tab)

src/odemis/gui/model/__init__.py
@@ -1941,8 +1941,6 @@ class MicroscopeView(StreamView):
     """
     def __init__(self, name, stage=None, **kwargs):
         StreamView.__init__(self, name, stage=stage, **kwargs)
-        # booleanVA to toggle showing/hiding the features
-        self.showFeatures = model.BooleanVA(True)
         if stage:
             self.stage_pos.subscribe(self._on_stage_pos)
 