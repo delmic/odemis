94ac162e725f37c8a636e2fd79c5488b668226c3
Thera Pals <pals@delmic.com>
2022-02-08 11:43:29 +0100

    fastem acquisition manager raised a timeout error for dwell times larger than 10us
    The error was: odemis.util.TimeoutError: Timeout while waiting for field image.
    After investigation I found out this only happened for the first image. Because the first image is actually acquired twice, the timeout for the first image needs to be increased.

--- src/odemis/acq/fastem.py
+++ src/odemis/acq/fastem.py
@@ -358,7 +358,11 @@ class AcquisitionTask(object):
         """
 
         total_field_time = self._detector.frameDuration.value
-        timeout = total_field_time + 5  # TODO what margin should be used?
+        # For a dwell time of 10us the total field time is approximately 7 seconds,
+        # therefore 7 seconds is a save margin for the timeout.
+        timeout = total_field_time + 7
+        # The first field gets acquired twice, therefore the field time is multiplied by 2.
+        timeout_first_field = 2 * total_field_time + 7
 
         # Acquire all single field images, which are automatically offloaded to the external storage.
         for field_idx in self._roa.field_indices:
@@ -376,8 +380,9 @@ class AcquisitionTask(object):
 
             dataflow.next(field_idx)  # acquire the next field image.
 
+            field_timeout = timeout_first_field if field_idx == (0, 0) else timeout
             # Wait until single field image data has been received (image_received sets flag to True).
-            if not self._data_received.wait(timeout):
+            if not self._data_received.wait(field_timeout):
                 # TODO here we often timeout when actually just the offload queue is full
                 #  need to handle offload queue error differently to just wait a bit instead of timing out
                 #   -> check if finish megafield is called in finally when hitting here
