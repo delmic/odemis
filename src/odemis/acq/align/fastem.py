ec26cca8547353b25fc21f9c072c595da3f39caa
Rossberger <rossberger@delmic.com>
2022-02-09 21:53:46 +0100

    fastem calibrations: implement calibration manager

--- src/odemis/acq/align/fastem.py
+++ src/odemis/acq/align/fastem.py
@@ -3,7 +3,9 @@
 """
 Created on 20 Jul 2021
 
-Copyright © 2021 Philip Winkler, Delmic
+@author: Philip Winkler, Sabrina Rossberger
+
+Copyright © 2021 - 2022 Philip Winkler, Delmic
 
 This file is part of Odemis.
 
@@ -18,22 +20,282 @@ PURPOSE. See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along with
 Odemis. If not, see http://www.gnu.org/licenses/.
 """
+import logging
+import time
+from concurrent.futures import CancelledError
 
 from odemis import model
-import time
 
+try:
+    import fastem_calibrations
+    from fastem_calibrations import (
+        autofocus_multiprobe,
+        scan_rotation_pre_align,
+        scan_amplitude_pre_align,
+        image_translation_pre_align,
+        descan_gain,
+        image_rotation_pre_align,
+        image_rotation,
+        image_translation
+    )
+except ImportError:
+    fastem_calibrations = None
+
+# TODO does it make sense to make this a list?
+OPTICAL_AUTOFOCUS = "autofocus_multiprobe"
+SCAN_ROTATION_PREALIGN = "scan_rotation_pre_align"
+SCAN_AMPLITUDE_PREALIGN = "scan_amplitude_pre_align"
+DESCAN_GAIN_STATIC = "descan_gain_static"
+IMAGE_ROTATION_PREALIGN = "image_rotation_pre_align"
+IMAGE_TRANSLATION_PREALIGN = "image_translation_pre_align"
+IMAGE_ROTATION_FINAL = "image_rotation"
+IMAGE_TRANSLATION_FINAL = "image_translation"
+
+# The executor is a single object, independent of how many times the module (fastem.py) is loaded.
 _executor = model.CancellableThreadPoolExecutor(max_workers=1)
 
 
-def align(main_data):
+def align(scanner, multibeam, descanner, detector, stage, ccd, beamshift, det_rotator, calibrations):
     """
-    :param main_data (odemis.gui.model.FastEMMainGUIData):
-    :returns: (ProgressiveFuture): acquisition future
+    Start a calibration task for a given list of calibrations.
+
+    :param scanner: (xt_client.Scanner) Scanner component connecting to the XT adapter.
+    :param multibeam: (technolution.EBeamScanner) The multibeam scanner component of the acquisition server module.
+    :param descanner: (technolution.MirrorDescanner) The mirror descanner component of the acquisition server module.
+    :param detector: (technolution.MPPC) The detector object to be used for collecting the image data.
+    :param stage: (actuator.ConvertStage) The stage in the corrected scan coordinate system, the x and y axes are
+        aligned with the x and y axes of the multiprobe and the multibeam scanner.
+    :param ccd: (model.DigitalCamera) A camera object of the diagnostic camera.
+    :param beamshift: (tfsbc.BeamShiftController) Component that controls the beamshift deflection.
+    :param det_rotator: (tmcm.CANController) K-mirror controller.
+    :param calibrations: (list of str) List of calibrations that should be run.
+
+    :returns: (ProgressiveFuture) Alignment future object, which can be cancelled.
+            The result of the future is:
+                (Exception or None): Exception raised during the acquisition or None.
     """
+
+    if fastem_calibrations is None:
+        raise ModuleNotFoundError("fastem_calibration module missing. Cannot run calibrations.")
+
     f = model.ProgressiveFuture()
-    _executor.submitf(f, _run_fake_alignment)
+
+    # Create a task that runs the calibration and alignments.
+    task = CalibrationTask(scanner, multibeam, descanner, detector, stage, ccd, beamshift, det_rotator, f, calibrations)
+
+    f.task_canceller = task.cancel  # lets the future know how to cancel the task.
+
+    # Connect the future to the task and run it in a thread.
+    # task.run is executed by the executor and runs as soon as no other task is executed
+    _executor.submitf(f, task.run)
+
     return f
 
 
-def _run_fake_alignment():
-    time.sleep(2)
+def estimate_calibration_time(calibrations):
+    """
+    Computes the approximate time it will take to run all calibrations.
+    :param calibrations: (list of str) List of calibrations that should be run.
+    :return (0 <= float): The estimated time for the requested calibrations in s.
+    """
+    tot_time = 0
+    for calib in calibrations:
+        # TODO how to use a list of function calls in this loop to avoid writing the same line
+        #  for each calibration?
+        if calib == OPTICAL_AUTOFOCUS:
+            tot_time += autofocus_multiprobe.estimate_calibration_time()
+        if calib == IMAGE_TRANSLATION_PREALIGN:
+            tot_time += image_translation_pre_align.estimate_calibration_time()
+        if calib == SCAN_AMPLITUDE_PREALIGN:
+            tot_time += scan_amplitude_pre_align.estimate_calibration_time()
+        if calib == DESCAN_GAIN_STATIC:
+            tot_time += descan_gain.estimate_calibration_time()
+        if calib == IMAGE_ROTATION_PREALIGN:
+            tot_time += image_rotation_pre_align.estimate_calibration_time()
+        if calib == IMAGE_TRANSLATION_PREALIGN:
+            tot_time += image_translation_pre_align.estimate_calibration_time()
+        if calib == IMAGE_ROTATION_FINAL:
+            tot_time += image_rotation.estimate_calibration_time()
+        if calib == IMAGE_TRANSLATION_FINAL:
+            tot_time += image_translation.estimate_calibration_time()
+
+    return tot_time
+
+
+class CalibrationTask(object):
+    """
+    The calibration task.
+    """
+
+    def __init__(self, scanner, multibeam, descanner, detector, stage, ccd, beamshift, det_rotator, future, calibrations):
+        """
+        :param scanner: (xt_client.Scanner) Scanner component connecting to the XT adapter.
+        :param multibeam: (technolution.EBeamScanner) The multibeam scanner component of the acquisition server module.
+        :param descanner: (technolution.MirrorDescanner) The mirror descanner component of the acquisition server module.
+        :param detector: (technolution.MPPC) The detector object to be used for collecting the image data.
+        :param stage: (actuator.ConvertStage) The stage in the corrected scan coordinate system, the x and y axes are
+            aligned with the x and y axes of the multiprobe and the multibeam scanner.
+        :param ccd: (model.DigitalCamera) A camera object of the diagnostic camera.
+        :param beamshift: (tfsbc.BeamShiftController) Component that controls the beamshift deflection.
+        :param det_rotator: (tmcm.CANController) K-mirror controller.
+        :param calibrations: (list of str) List of calibrations that should be run.
+
+        :param future: (ProgressiveFuture) Acquisition future object, which can be cancelled.
+                        (Exception or None): Exception raised during the calibration or None.
+        """
+        self._scanner = scanner
+        self._multibeam = multibeam
+        self._descanner = descanner
+        self._detector = detector
+        self._dataflow = detector.data
+        self._stage = stage
+        self._ccd = ccd
+        self._beamshift = beamshift
+        self._det_rotator = det_rotator
+        self._future = future
+
+        self.calibrations = calibrations
+
+        # List of calibrations to be executed. Used for progress update.
+        self._calibrations_remaining = set(calibrations)
+
+        # keep track if future was cancelled or not
+        self._cancelled = False
+
+        if model.MD_CALIB not in self._beamshift.getMetadata().keys():
+            self._beamshift.updateMetadata({model.MD_CALIB: self._scanner.beamShiftTransformationMatrix.value})
+
+    def run(self):
+        """
+        Runs a set of calibration procedures.
+        :returns:
+            (None) If calibrations successful return None.
+        :raise:
+            Exception: If a calibration failed.
+            CancelledError: If the calibration was cancelled.
+        """
+
+        # Get the estimated time for all requested calibrations.
+        total_calibration_time = self.estimate_calibration_time()
+
+        # No need to set the start time of the future: it's automatically done when setting its state to running.
+        self._future.set_progress(end=time.time() + total_calibration_time)  # provide end time to future
+        logging.info("Starting calibrations, with expected duration of %f s", total_calibration_time)
+
+        try:
+            logging.debug("Starting calibration.")
+
+            # loop over calibrations in list (order in list is important!)
+            for calib in self.calibrations:
+                # TODO return a sub-future when implemented for calibrations
+                self.run_calibrations(calib)
+
+                # def _pass_future_progress(sub_f, start, end):
+                #     f.set_progress(start, end)
+
+                # TODO Connect the progress of the sub-future to the main future
+                # sf.add_update_callback(_pass_future_progress)
+                # sf.result()
+
+                # remove from list of calibrations when finished
+                self._calibrations_remaining.discard(calib)
+
+                # In case the calibrations was cancelled by a client, before the future returned,
+                # raise cancellation error.
+                if self._cancelled:
+                    raise CancelledError()
+
+                # Update the time left for the calibrations remaining
+                self._future.set_progress(end=time.time() + self.estimate_calibration_time())
+
+        except CancelledError:  # raised in try statement
+            logging.debug("Calibration was cancelled.")
+            raise
+        except Exception as ex:
+            raise ex
+        finally:
+            # Remove references to the calibrations once all calibrations are finished/cancelled.
+            self._calibrations_remaining.clear()
+            self._scanner.blanker.value = True  # always blank the beam to reduce beam damage on sample
+            logging.debug("Finish calibrations.")
+
+    def run_calibrations(self, calibration):
+        """
+        Run a calibration.
+        # Note: All calibrations can be run on bare scintillator.
+        """
+        if calibration == OPTICAL_AUTOFOCUS:
+            autofocus_multiprobe.run_autofocus(self._scanner, self._multibeam, self._descanner, self._detector,
+                                               self._dataflow, self._ccd, self._stage)
+
+        if calibration == SCAN_ROTATION_PREALIGN:
+            scan_rotation_pre_align.run_scan_rotation_pre_align(self._scanner, self._multibeam, self._descanner,
+                                                                self._detector, self._dataflow, self._ccd)
+
+        if calibration == SCAN_AMPLITUDE_PREALIGN:
+            scan_amplitude_pre_align.run_scan_amplitude_pre_align(self._scanner, self._multibeam, self._descanner,
+                                                                  self._detector, self._dataflow, self._ccd)
+
+        if calibration == DESCAN_GAIN_STATIC:
+            descan_gain.run_descan_gain_static(self._scanner, self._multibeam, self._descanner,
+                                               self._detector, self._dataflow, self._ccd)
+
+        if calibration == IMAGE_ROTATION_PREALIGN:
+            image_rotation_pre_align.run_image_rotation_pre_align(self._scanner, self._multibeam, self._descanner,
+                                                                  self._detector, self._dataflow, self._ccd,
+                                                                  self._det_rotator)
+
+        if calibration == IMAGE_TRANSLATION_PREALIGN:
+            image_translation_pre_align.run_image_translation_pre_align(self._scanner, self._multibeam,
+                                                                        self._descanner, self._detector,
+                                                                        self._dataflow, self._ccd)
+
+        if calibration == IMAGE_ROTATION_FINAL:
+            image_rotation.run_image_rotation(self._scanner, self._multibeam, self._descanner,
+                                              self._detector, self._dataflow, self._det_rotator)
+
+        if calibration == IMAGE_TRANSLATION_FINAL:
+            image_translation.run_image_translation(self._scanner, self._multibeam, self._descanner,
+                                                    self._detector, self._dataflow, self._ccd)
+
+    def cancel(self, future):
+        """
+        Cancels the calibrations.
+        :param future: (future) The calibration future.
+        :return: (bool) True if cancelled, TODO False?
+        """
+        self._cancelled = True
+
+        # FIXME Currently there is no subfuture implemented for each calibration. So currently,
+        #  when cancelling the calibration is still finished.
+        # TODO When to set this to False in which event?
+
+        return True
+
+    def estimate_calibration_time(self):
+        """
+        Computes the approximate time it will take to run the remaining calibrations.
+        :return (0 <= float): The estimated time for the requested calibrations in s.
+        """
+        tot_time = 0
+        for calib in self._calibrations_remaining:
+            # TODO how to use a list of function calls in this loop to avoid writing the same line
+            #  for each calibration?
+            if calib == OPTICAL_AUTOFOCUS:
+                tot_time += autofocus_multiprobe.estimate_calibration_time()
+            if calib == IMAGE_TRANSLATION_PREALIGN:
+                tot_time += image_translation_pre_align.estimate_calibration_time()
+            if calib == SCAN_AMPLITUDE_PREALIGN:
+                tot_time += scan_amplitude_pre_align.estimate_calibration_time()
+            if calib == DESCAN_GAIN_STATIC:
+                tot_time += descan_gain.estimate_calibration_time()
+            if calib == IMAGE_ROTATION_PREALIGN:
+                tot_time += image_rotation_pre_align.estimate_calibration_time()
+            if calib == IMAGE_TRANSLATION_PREALIGN:
+                tot_time += image_translation_pre_align.estimate_calibration_time()
+            if calib == IMAGE_ROTATION_FINAL:
+                tot_time += image_rotation.estimate_calibration_time()
+            if calib == IMAGE_TRANSLATION_FINAL:
+                tot_time += image_translation.estimate_calibration_time()
+
+        return tot_time
---
+++ src/odemis/acq/align/test/fastem_test.py
@@ -0,0 +1,185 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 24th January 2022
+
+@author: Sabrina Rossberger
+
+Copyright © 2022 Sabrina Rossberger, Delmic
+
+This file is part of Odemis.
+
+Odemis is free software: you can redistribute it and/or modify it under the terms
+of the GNU General Public License version 2 as published by the Free Software
+Foundation.
+
+Odemis is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+Odemis. If not, see http://www.gnu.org/licenses/.
+
+"""
+from __future__ import division
+
+import logging
+import os
+import time
+import unittest
+from concurrent.futures._base import CancelledError
+
+import numpy
+
+import odemis
+from odemis import model
+from odemis.acq.align import fastem
+from odemis.acq.align.fastem import OPTICAL_AUTOFOCUS, IMAGE_TRANSLATION_PREALIGN
+from odemis.util import test
+
+# * TEST_NOHW = 1: use simulator (asm/sam and xt adapter simulators need to be running)
+# * TEST_NOHW = 0: connected to the real hardware (backend needs to be running)
+# technolution_asm_simulator/simulator2/run_the_simulator.py
+TEST_NOHW = (os.environ.get("TEST_NOHW", "0") != "0")  # Default is HW testing
+
+logging.getLogger().setLevel(logging.DEBUG)
+logging.basicConfig(format="%(asctime)s  %(levelname)-7s %(module)s:%(lineno)d %(message)s")
+
+CONFIG_PATH = os.path.dirname(odemis.__file__) + "/../../install/linux/usr/share/odemis/"
+FASTEM_CONFIG = CONFIG_PATH + "sim/fastem-sim-asm.odm.yaml"
+
+
+class TestFastEMCalibration(unittest.TestCase):
+    """Test the calibration manager."""
+
+    @classmethod
+    def setUpClass(cls):
+        if TEST_NOHW:
+            test.start_backend(FASTEM_CONFIG)
+
+        # get the hardware components
+        cls.scanner = model.getComponent(role='e-beam')
+        cls.asm = model.getComponent(role="asm")
+        cls.mppc = model.getComponent(role="mppc")
+        cls.multibeam = model.getComponent(role="multibeam")
+        cls.descanner = model.getComponent(role="descanner")
+        cls.stage = model.getComponent(
+            role="stage")  # TODO replace with stage-scan when ROA conversion method available
+        cls.ccd = model.getComponent(role="diagnostic-ccd")
+        cls.focuser = model.getComponent(role="diagnostic-cam-focus")
+        cls.beamshift = model.getComponent(role="ebeam-shift")
+        cls.det_rotator = model.getComponent(role="det-rotator")
+
+    def setUp(self):
+
+        self.good_focus = -70e-6  # position where the image of the multiprobe is displayed in focus [m]
+        self.good_mp_position = (650, 450)  # position for multiprobe where mp is aligned with mppc detector [px_dc]
+        self.ccd.updateMetadata(
+            {model.MD_FAV_POS_ACTIVE:
+                {"x": self.good_mp_position[0],
+                 "y": self.good_mp_position[1],
+                 "z": self.good_focus}}
+                                )
+
+        # move the stage so that the image is in focus
+        self.focuser.moveAbs({"z": self.good_focus}).result()
+
+    def test_optical_autofocus(self):
+        """Run the optical autofocus calibration. Can also be tested with simulator."""
+
+        calibrations = [OPTICAL_AUTOFOCUS]
+
+        # move the stage so that the image is out of focus
+        self.center_position = -30e-6
+        self.focuser.moveAbs({"z": self.center_position}).result()
+
+        # Run auto focus
+        f = fastem.align(self.scanner, self.multibeam, self.descanner, self.mppc, self.stage, self.ccd,
+                         self.beamshift, self.det_rotator, calibrations)
+
+        # foc_pos, foc_lev = f.result(timeout=900)  # TODO make use of the focus level?
+        e = f.result(timeout=900)
+
+        self.assertIsNone(e)  # check no exceptions were returned
+        # check that z stage position is close to good position
+        # Note: This accuracy is dependent on the value chosen for the magnification on the lens.
+        numpy.testing.assert_allclose(self.focuser.position.value["z"], self.good_focus, atol=2e-6)
+
+    def test_image_translation_prealign(self):
+        """Run the image translation prealing calibration. Can also be tested with simulator.
+        It calibrates the descanner offset."""
+
+        calibrations = [IMAGE_TRANSLATION_PREALIGN]
+
+        # get current descanner offset
+        descan_offset_cur = self.descanner.scanOffset.value
+
+        # Run image translation pre-align
+        f = fastem.align(self.scanner, self.multibeam, self.descanner, self.mppc, self.stage, self.ccd,
+                         self.beamshift, self.det_rotator, calibrations)
+
+        e = f.result(timeout=900)
+
+        self.assertIsNone(e)  # check no exceptions were returned
+
+        # get the calibrated descanner offset
+        descan_offset_calib = self.descanner.scanOffset.value
+
+        # check the calibrated descan offset is different from the previous offset
+        self.assertNotEqual(descan_offset_cur, descan_offset_calib)
+
+    def test_progress(self):
+        """Check if some progress is reported during the optical autofocus calibration."""
+
+        self.updates = 0  # updated in callback on_progress_update
+
+        calibrations = [OPTICAL_AUTOFOCUS, IMAGE_TRANSLATION_PREALIGN]
+        f = fastem.align(self.scanner, self.multibeam, self.descanner, self.mppc, self.stage, self.ccd,
+                         self.beamshift, self.det_rotator, calibrations)
+
+        f.add_update_callback(self.on_progress_update)  # callback executed every time f.set_progress is called
+        f.add_done_callback(self.on_done)  # callback executed when f.set_result is called (via bindFuture)
+
+        e = f.result()
+
+        self.assertIsNone(e)  # check no exceptions were returned
+        self.assertTrue(self.done)
+        # at least one update per calibration plus once at start of calibration, plus once at end of calibration
+        self.assertGreaterEqual(self.updates, 4)  # at least one update per
+
+    def test_cancel(self):
+        """Test if it is possible to cancel the optical autofocus calibration."""
+
+        # FIXME no subfuture available yet, which are cancelable
+
+        self.end = None  # updated in callback on_progress_update
+        self.updates = 0  # updated in callback on_progress_update
+        self.done = False  # updated in callback on_done
+
+        calibrations = [OPTICAL_AUTOFOCUS]
+        f = fastem.align(self.scanner, self.multibeam, self.descanner, self.mppc, self.stage, self.ccd,
+                         self.beamshift, self.det_rotator, calibrations)
+
+        f.add_update_callback(self.on_progress_update)  # callback executed every time f.set_progress is called
+        f.add_done_callback(self.on_done)  # callback executed when f.set_result is called (via bindFuture)
+
+        time.sleep(1)  # make sure it's started
+        self.assertTrue(f.running())
+        f.cancel()
+
+        self.assertRaises(CancelledError, f.result, 1)  # add timeout = 1s in case cancellation error was not raised
+        self.assertGreaterEqual(self.updates, 2)  # at least one update at cancellation
+        self.assertLessEqual(self.end, time.time())
+        self.assertTrue(self.done)
+        self.assertTrue(f.cancelled())
+
+    def on_done(self, future):
+        self.done = True
+
+    def on_progress_update(self, future, start, end):
+        self.start = start
+        self.end = end
+        self.updates += 1
+
+
+if __name__ == "__main__":
+    unittest.main()
