delmic/odemis

commit a8fec30dee4b9e0202ee6e9760814388262049a1

gui cont: move cryo acquired streams control to the streambar controller
Simplifies the tab controller which was doing a lot of different things.

Now there is a dedicated streambar controller for Cryo localization.
In addition to hiding/showing the streams when the view is selected,
it does the same when a feature is selected.

Among other things, it fixes a bug where *every* stream was hidden when changing feature.

src/odemis/gui/comp/stream.py
@@ -1547,6 +1547,7 @@ class StreamBar(wx.Panel):
         self._sz.Detach(spanel)
         self.stream_panels.remove(spanel)
         spanel.Destroy()
+        spanel.Unbind(wx.EVT_WINDOW_DESTROY, source=spanel, handler=self.on_streamp_destroy)
 
     def clear(self):
         """
@@ -1554,7 +1555,8 @@ class StreamBar(wx.Panel):
         Must be called in the main GUI thread
         """
         for p in list(self.stream_panels):
-            # Only refit the (empty) bar after all streams are gone
+            # Prematurely stop listening to the Destroy event, to only refit the
+            # (empty) bar once, after all streams are gone.
             p.Unbind(wx.EVT_WINDOW_DESTROY, source=p, handler=self.on_streamp_destroy)
             self.remove_stream_panel(p)
 

src/odemis/acq/feature.py
@@ -80,11 +80,11 @@ def save_features(project_dir, features):
     """
     Save the whole features list directly to the file
     :param project_dir: (string) directory to save the file to (typically project directory)
-    :param features: (ListVA) ListVA with features to serialize
+    :param features: (list of Features) all the features to serialize
     """
     filename = os.path.join(project_dir, "features.json")
     with open(filename, 'w') as jsonfile:
-        json.dump(get_features_dict(features.value), jsonfile)
+        json.dump(get_features_dict(features), jsonfile)
 
 
 def read_features(project_dir):

src/odemis/acq/test/feature_test.py
@@ -39,11 +39,11 @@ class TestFeatureEncoderDecoder(unittest.TestCase):
         feature1 = CryoFeature("Feature-1", 0, 0, 0, 10)
         feature2 = CryoFeature("Feature-2", 1e-3, 1e-3, 2e-3, 20)
 
-        features = ListVA(value=[feature1, feature2])
+        features = [feature1, feature2]
         save_features("", features)
         r_features = read_features("")
-        self.assertEqual(len(features.value), len(r_features))
-        self.assertEqual(features.value[0].name.value, r_features[0].name.value)
+        self.assertEqual(len(features), len(r_features))
+        self.assertEqual(features[0].name.value, r_features[0].name.value)
 
 
 if __name__ == "__main__":

src/odemis/gui/cont/features.py
@@ -5,6 +5,7 @@ import wx
 from odemis.acq.feature import FEATURE_ACTIVE, FEATURE_ROUGH_MILLED, FEATURE_DEACTIVE, save_features, FEATURE_POLISHED
 from odemis.gui.model import TOOL_FEATURE
 from odemis.gui.util.widgets import VigilantAttributeConnector
+from odemis.gui.util import call_in_wx_main
 
 
 class CryoFeatureController(object):
@@ -74,22 +75,64 @@ class CryoFeatureController(object):
         self._main_data_model.stage.moveAbs({'x': pos[0], 'y': pos[1]})
         self._main_data_model.focus.moveAbs({'z': pos[2]})
 
+    def _enable_feature_ctrls(self, enable: bool):
+        """
+        Enables/disables the feature controls
+
+        enable: If True, allow all the feature controls to be used.
+        """
+        self._panel.cmb_feature_status.Enable(enable)
+        self._panel.ctrl_feature_z.Enable(enable)
+        self._panel.btn_use_current_z.Enable(enable)
+        self._panel.btn_go_to_feature.Enable(enable)
+
+    def _update_feature_cmb_list(self):
+        """
+        Fill up the combobox with the list of features, and select the current feature
+        To be called in the main GUI thread
+        """
+        current_feature = self._tab_data_model.main.currentFeature.value
+        # Special case: there is no selected feature
+        if current_feature is None:
+            self._enable_feature_ctrls(False)
+            self._panel.cmb_features.SetValue("No Feature Selected")
+            self._panel.cmb_feature_status.SetValue(FEATURE_ACTIVE)  # Default
+        else:
+            self._enable_feature_ctrls(True)
+
+            features = self._tab_data_model.main.features.value
+            self._panel.cmb_features.Clear()
+            for i, feature in enumerate(features):
+                self._panel.cmb_features.Insert(feature.name.value, i, feature)
+
+            # Select the current feature
+            index = features.index(current_feature)
+            if index == -1:
+                logging.debug("Current selected feature '%s' is not part of the list of features",
+                              current_feature.name.value)
+                return
+
+            self._panel.cmb_features.SetSelection(index)
+
+    @call_in_wx_main
     def _on_features_changes(self, features):
         """
-        repopulate the feature list dropdown with the modified features
-        :param features: list(CryoFeature) list of modified features
+        repopulate the feature list dropdown with the modified .features
+        Note that .currentFeature is supposed to be updated too, so that it points
+        to one of the features, or None.
+        :param features: list[CryoFeature] new list of available features
         """
         if not features:
             # Clear current selections
             self._panel.cmb_features.Clear()
-            self._on_current_feature_changes(None)
+            # currentFeature should also have been set to None, which will disable the other widgets
             return
-        save_features(self._tab.conf.pj_last_path, self._tab_data_model.main.features)
-        self._panel.cmb_features.Clear()
-        for i, feature in enumerate(features):
-            self._panel.cmb_features.Insert(feature.name.value, i, feature)
-        self._on_current_feature_changes(None)
+        save_features(self._tab.conf.pj_last_path, self._tab_data_model.main.features.value)
 
+        # Make sure the current feature is selected
+        self._update_feature_cmb_list()
+
+    @call_in_wx_main
     def _on_current_feature_changes(self, feature):
         """
         Update the feature panel controls when the current feature VA is modified
@@ -104,23 +147,13 @@ class CryoFeatureController(object):
         if self._feature_z_va_connector:
             self._feature_z_va_connector.disconnect()
 
-        def enable_feature_ctrls(enable):
-            self._panel.cmb_feature_status.Enable(enable)
-            self._panel.ctrl_feature_z.Enable(enable)
-            self._panel.btn_use_current_z.Enable(enable)
-            self._panel.btn_go_to_feature.Enable(enable)
+        self._update_feature_cmb_list()
 
-        if not feature:
-            enable_feature_ctrls(False)
-            self._panel.cmb_features.SetValue("No Feature Selected")
-            self._panel.cmb_feature_status.SetValue("Not Selected")
+        if feature is None:
+            self._enable_feature_ctrls(False)
             return
-        save_features(self._tab.conf.pj_last_path, self._tab_data_model.main.features)
 
-        enable_feature_ctrls(True)
-        # Set feature list with the current feature
-        index = self._tab_data_model.main.features.value.index(feature)
-        self._panel.cmb_features.SetSelection(index)
+        self._enable_feature_ctrls(True)
 
         # Disconnect and reconnect the VA connectors to the newly selected feature
         self._feature_name_va_connector = VigilantAttributeConnector(feature.name,
@@ -146,14 +179,18 @@ class CryoFeatureController(object):
     def _on_feature_pos(self, feature_pos):
         # Set the feature Z ctrl with the 3rd (focus) element of the feature position
         self._panel.ctrl_feature_z.SetValue(feature_pos[2])
-        save_features(self._tab.conf.pj_last_path, self._tab_data_model.main.features)
+        save_features(self._tab.conf.pj_last_path, self._tab_data_model.main.features.value)
 
     def _on_feature_name(self, _):
-        save_features(self._tab.conf.pj_last_path, self._tab_data_model.main.features)
+        # Force an update of the list of features
+        self._on_features_changes(self._tab_data_model.main.features.value)
 
-    def _on_cmb_feature_name_change(self, ):
+        save_features(self._tab.conf.pj_last_path, self._tab_data_model.main.features.value)
+
+    def _on_cmb_feature_name_change(self):
         feature = self._tab_data_model.main.currentFeature.value
-        value= self._panel.cmb_features.GetValue()
+        value = self._panel.cmb_features.GetValue()  # Old name
+        # Update the name of the streams with the new name
         for stream in feature.streams.value:
             stream.name.value = stream.name.value.replace(feature.name.value, value)
         return value
@@ -164,7 +201,7 @@ class CryoFeatureController(object):
         :param feature_status: (string) the updated feature status
         """
         self._panel.cmb_feature_status.SetValue(feature_status)
-        save_features(self._tab.conf.pj_last_path, self._tab_data_model.main.features)
+        save_features(self._tab.conf.pj_last_path, self._tab_data_model.main.features.value)
 
     def _on_cmb_features_change(self, evt):
         """

src/odemis/gui/cont/streams.py
@@ -40,7 +40,8 @@ from wx.lib.pubsub import pub
 import odemis.acq.stream as acqstream
 import odemis.gui.model as guimodel
 from odemis import model, util
-from odemis.acq.stream import MeanSpectrumProjection, FastEMOverviewStream
+from odemis.acq.stream import MeanSpectrumProjection, FastEMOverviewStream, \
+    StaticStream
 from odemis.gui import FG_COLOUR_DIS, FG_COLOUR_WARNING, FG_COLOUR_ERROR, \
     CONTROL_COMBO, CONTROL_FLT
 from odemis.gui.comp.overlay.world import RepetitionSelectOverlay
@@ -3083,3 +3084,130 @@ class FastEMStreamsController(StreamBarController):
             if isinstance(s, FastEMOverviewStream) and s not in tab_streams:
                 tab_streams.append(s)
                 canvas.view.addStream(s)
+
+
+class CryoAcquiredStreamsController(StreamBarController):
+    """
+    StreamBarController to control the display of the Static streams in the Cryo
+    Localization tab. It deals with two types of streams:
+     * Overview streams: shown only in the overview view. Independent of the
+       current feature selected.
+     * Feature streams: the streams related to a specific CryoFeature. They are
+       shown only in the "acquired" view, and only when the related feature is
+       selected. Note that *all* acquired streams are linked to one (and only one)
+       CryoFeature.
+    """
+
+    def __init__(self, tab_data, feature_view, ov_view, *args, **kwargs):
+        """
+        feature_view (StreamView): the view to show the feature streams
+        ov_view (StreamView): the view to show the overview streams
+        """
+        super().__init__(tab_data, *args, **kwargs)
+        self._feature_view = feature_view
+        self._ov_view = ov_view
+
+        # tab_data has:
+        # * .streams, which contains *all* the streams. This controller takes
+        #   care only of the StaticStreams there.
+        # * .overviewStreams, which contains the list of overview streams
+        # The main has:
+        # * .features: which contain every CryoFeature, which all have a .streams
+        # * .currentFeature: the current feature (can be None)
+
+        # TODO: eventually, also unload/reload data when the feature is not shown? (to save memory)
+
+        tab_data.main.currentFeature.subscribe(self._on_current_feature_changes)
+
+    def showOverviewStream(self, stream) -> StreamController:
+        """
+        Shows an Overview stream (in the Overview view)
+        Must be run in the main GUI thread.
+        """
+        self._ov_view.addStream(stream)
+        sc = self._add_stream_cont(stream, show_panel=True, static=self.static_mode,
+                                   view=self._ov_view)
+
+        return sc
+
+    def showFeatureStream(self, stream) -> StreamController:
+        """
+        Shows an Feature stream (in the Acquired view)
+        Must be run in the main GUI thread.
+        """
+        # TODO: don't delete/create stream controller every time? Instead, we
+        # could just hide/show them the same way it's done when switching view.
+        self._feature_view.addStream(stream)
+        sc = self._add_stream_cont(stream, show_panel=True, static=self.static_mode,
+                                   view=self._feature_view)
+        return sc
+
+    def _on_current_feature_changes(self, feature):
+        """
+        Handle switching the acquired streams appropriate to the current feature
+        :param feature: (CryoFeature or None) the newly selected current feature
+        """
+        self.clear_feature_streams()
+        # show the feature streams on the acquired view
+        acquired_streams = feature.streams.value if feature else []
+        for stream in acquired_streams:
+            self.showFeatureStream(stream)
+
+    def clear_feature_streams(self):
+        """
+        Remove from display all feature streams (but leave the overview and live streams)
+        But DO NOT REMOVE the streams from the model
+        """
+        # Remove the panels, and indirectly it will clear the view
+        v = self._feature_view
+        for sc in self.stream_controllers.copy():
+            if not isinstance(sc.stream, StaticStream):
+                logging.warning("Unexpected non static stream: %s", sc.stream)
+                continue
+            # Leave the overview streams
+            if sc.stream in self._tab_data_model.overviewStreams.value:
+                continue
+
+            self._stream_bar.remove_stream_panel(sc.stream_panel)
+            if hasattr(v, "removeStream"):
+                v.removeStream(sc.stream)
+            self.stream_controllers.remove(sc)
+
+        self._stream_bar.fit_streams()
+
+        # Force a check of what can be garbage collected, as some of the streams
+        # could be quite big, that will help to reduce memory pressure.
+        gc.collect()
+
+    def clear(self, clear_model=True):
+        """
+        Remove all the streams, from the GUI (view, stream panels) and possibly
+        from the model too (in .streams and features.streams)
+        Must be called in the main GUI thread.
+        :param clear_model: (bool) if True, streams will be removed from model
+        """
+        # clear the graphical part
+        self._stream_bar.clear()
+
+        # Clean up the views
+        for stream in self._tab_data_model.streams.value:
+            if isinstance(stream, StaticStream):
+                for v in (self._feature_view, self._ov_view):
+                    if hasattr(v, "removeStream"):
+                        v.removeStream(stream)
+
+        if clear_model:
+            while self._tab_data_model.overviewStreams.value:
+                stream = self._tab_data_model.overviewStreams.value.pop()
+
+            # Typically, all the features will also be deleted, but that's not our job
+            # (see CryoChamberTab._reset_project_data())
+            for f in self._tab_data_model.main.features.value:
+                f.streams.value.clear()
+
+        # Clear the stream controller
+        self.stream_controllers = []
+
+        # Force a check of what can be garbage collected, as some of the streams
+        # could be quite big, that will help to reduce memory pressure.
+        gc.collect()

src/odemis/gui/cont/tabs.py
@@ -404,15 +404,16 @@ class LocalizationTab(Tab):
         self._acquisition_controller = acqcont.CryoAcquiController(
             tab_data, panel, self)
 
-        self._acquired_stream_controller = streamcont.StreamBarController(
+        self._acquired_stream_controller = streamcont.CryoAcquiredStreamsController(
             tab_data,
-            panel.pnl_cryosecom_acquired,
+            feature_view=tab_data.views.value[1],
+            ov_view=tab_data.views.value[0],
+            stream_bar=panel.pnl_cryosecom_acquired,
             view_ctrl=self.view_controller,
             static=True,
         )
 
         self._feature_panel_controller = CryoFeatureController(tab_data, panel, self)
-        self.tab_data_model.main.currentFeature.subscribe(self._on_current_feature_changes)
         self.tab_data_model.streams.subscribe(self._on_acquired_streams)
         self.conf = conf.get_acqui_conf()
 
@@ -523,16 +524,14 @@ class LocalizationTab(Tab):
     def load_overview_data(self, data):
         # Create streams from data
         streams = data_to_static_streams(data)
-
-        view = self.tab_data_model.views.value[0]  # overview map view
         bbox = (None, None, None, None)  # ltrb in m
         for s in streams:
             s.name.value = "Overview " + s.name.value
-            self._show_acquired_stream(s, view)
             # Add the static stream to the streams list of the model and also to the overviewStreams to easily
             # distinguish between it and other acquired streams
             self.tab_data_model.overviewStreams.value.append(s)
             self.tab_data_model.streams.value.insert(0, s)
+            self._acquired_stream_controller.showOverviewStream(s)
 
             # Compute the total bounding box
             try:
@@ -553,33 +552,27 @@ class LocalizationTab(Tab):
         chamber_tab = self.main_data.getTabByName("cryosecom_chamber")
         chamber_tab.load_overview_streams(streams)
 
-    def clear_live_streams(self):
+    def reset_live_streams(self):
         """
-        Clear the content of the live streams
-
-
+        Clear the content of the live streams. So the streams and their settings
+        are still available, but without any image.
         """
         live_streams = [stream for stream in self.tab_data_model.streams.value if isinstance(stream, LiveStream)]
         for stream in live_streams:
             if stream.raw:
                 stream.raw = []
                 stream.image.value = None
-                stream.histogram._value = numpy.empty(0)
-                stream.histogram.notify(stream.histogram._value)
+                stream.histogram._set_value(numpy.empty(0), force_write=True)
 
-    def clear_overview_streams(self, filter_stream_name=None):
+    def clear_acquired_streams(self):
         """
-        Remove overview map (Static) streams and clear them from view and panel.
-        @:param filter_stream_name: (StaticStream or None) the newly acquired stream name to filter on, None will remove all static streams
+        Remove overview map streams and feature streams, both from view and panel.
         """
-        for stream in self.tab_data_model.overviewStreams.value:
-            if filter_stream_name and stream.name.value != filter_stream_name:
-                continue
-            self._acquired_stream_controller.removeStreamPanel(stream)
+        self._acquired_stream_controller.clear()
 
     def _onAutofocus(self, active):
-        # Determine which stream is active
         if active:
+            # Determine which stream is active
             try:
                 self.curr_s = self.tab_data_model.streams.value[0]
             except IndexError:
@@ -590,37 +583,9 @@ class LocalizationTab(Tab):
 
             # run only if focuser is available
             if self.curr_s.focuser:
-                # TODO: maybe this can be done in a less hard-coded way
-                self.orig_hfw = None
-                self.orig_binning = None
-                self.orig_exposureTime = None
-                init_focus = None
                 emt = self.curr_s.emitter
                 det = self.curr_s.detector
-                # Set binning to 8 in case of optical focus to avoid high SNR
-                # and limit HFW in case of ebeam focus to avoid extreme brightness.
-                # Also apply ACB before focusing in case of ebeam focus.
-                if self.curr_s.focuser.role == "ebeam-focus" and self.main_data.role == "delphi":
-                    self.orig_hfw = emt.horizontalFoV.value
-                    emt.horizontalFoV.value = min(self.orig_hfw, AUTOFOCUS_HFW)
-                    f = det.applyAutoContrast()
-                    f.result()
-                    # Use the good initial focus value if known
-                    init_focus = self._state_controller.good_focus
-
-                    # TODO: for the DELPHI, it should also be possible to use the
-                    # opt_focus value from calibration as a "good value"
-                else:
-                    if model.hasVA(det, "binning"):
-                        self.orig_binning = det.binning.value
-                        det.binning.value = max(self.orig_binning, AUTOFOCUS_BINNING)
-                        if model.hasVA(det, "exposureTime"):
-                            self.orig_exposureTime = det.exposureTime.value
-                            bin_ratio = numpy.prod(self.orig_binning) / numpy.prod(det.binning.value)
-                            expt = self.orig_exposureTime * bin_ratio
-                            det.exposureTime.value = det.exposureTime.clip(expt)
-
-                self._autofocus_f = AutoFocus(det, emt, self.curr_s.focuser, good_focus=init_focus)
+                self._autofocus_f = AutoFocus(det, emt, self.curr_s.focuser)
                 self._autofocus_f.add_done_callback(self._on_autofocus_done)
             else:
                 # Should never happen as normally the menu/icon are disabled
@@ -631,16 +596,10 @@ class LocalizationTab(Tab):
 
     def _on_autofocus_done(self, future):
         self.tab_data_model.autofocus_active.value = False
-        if self.orig_hfw is not None:
-            self.curr_s.emitter.horizontalFoV.value = self.orig_hfw
-        if self.orig_binning is not None:
-            self.curr_s.detector.binning.value = self.orig_binning
-        if self.orig_exposureTime is not None:
-            self.curr_s.detector.exposureTime.value = self.orig_exposureTime
 
     def _on_current_stream(self, streams):
         """
-        Called when some VAs affecting the current stream change
+        Called when the current stream changes
         """
         # Try to get the current stream
         try:
@@ -652,17 +611,6 @@ class LocalizationTab(Tab):
             self.curr_s.should_update.subscribe(self._on_stream_update, init=True)
         else:
             wx.CallAfter(self.tb.enable_button, TOOL_AUTO_FOCUS, False)
-
-    def _on_stage_pos(self, pos):
-        """
-        Called when the stage is moved, enable the tab if position is imaging mode, disable otherwise
-        :param pos: (dict str->float or None) updated position of the stage
-        """
-        if self.main_data.role == "enzel":
-            # TODO: IMAGING position should not be used on ENZEL anymore
-            guiutil.enable_tab_on_stage_position(self.button, self.stage, pos, target=[THREE_BEAMS, IMAGING])
-        elif self.main_data.role == "meteor":
-            guiutil.enable_tab_on_stage_position(self.button, self.stage, pos, target=[FM_IMAGING, SEM_IMAGING])
 
     def _on_stream_update(self, updated):
         """
@@ -682,53 +630,6 @@ class LocalizationTab(Tab):
         if not f_enable:
             self.tab_data_model.autofocus_active.value = False
         wx.CallAfter(self.tb.enable_button, TOOL_AUTO_FOCUS, f_enable)
-
-    def terminate(self):
-        super(LocalizationTab, self).terminate()
-        self.stage.position.unsubscribe(self._on_stage_pos)
-        # make sure the streams are stopped
-        for s in self.tab_data_model.streams.value:
-            s.is_active.value = False
-
-    def Show(self, show=True):
-        assert (show != self.IsShown())  # we assume it's only called when changed
-        super(LocalizationTab, self).Show(show)
-
-        if not show: # if localization tab is not chosen
-            # pause streams when not displayed
-            self._streambar_controller.pauseStreams()
-
-    def _show_acquired_stream(self, acquired_stream, selected_view):
-        """
-        Create a stream controller for the given stream and view and add it to the stream bar controller list of stream controllers
-        :param acquired_stream: the static stream to show
-        :param selected_view: the view to show it on
-        """
-        stream_cont = StreamController(self.panel.pnl_cryosecom_acquired, acquired_stream, self.tab_data_model,
-                                       show_panel=True, view=selected_view, sb_ctrl=self._acquired_stream_controller)
-        stream_cont.to_static_mode()
-        selected_view.addStream(acquired_stream)
-
-        stream_cont.stream_panel.set_visible(True)
-        stream_cont.stream_panel.show_remove_btn(True)
-        self._acquired_stream_controller.stream_controllers.append(stream_cont)
-
-    def _on_current_feature_changes(self, feature):
-        """
-        Handle switching the acquired streams appropriate to the current feature
-        :param feature: (CryoFeature or None) the newly selected current feature
-        """
-        self._acquired_stream_controller.clear(clear_model=False)
-        # show the feature streams on the acquired view
-        view = self.tab_data_model.views.value[1]
-        acquired_streams = feature.streams.value if feature else []
-        for stream in acquired_streams:
-            self._show_acquired_stream(stream, view)
-
-        # show the overview maps (if any) on the overview view
-        view = self.tab_data_model.views.value[0]
-        for stream in self.tab_data_model.overviewStreams.value:
-            self._show_acquired_stream(stream, view)
 
     def _on_acquired_streams(self, streams):
         """
@@ -755,13 +656,6 @@ class LocalizationTab(Tab):
                     if st in feature.streams.value:
                         feature.streams.value.remove(st)
 
-    @classmethod
-    def get_display_priority(cls, main_data):
-        if main_data.role in ("enzel", "meteor"):
-            return 2
-        else:
-            return None
-
     @call_in_wx_main
     def display_acquired_data(self, data):
         """
@@ -770,13 +664,33 @@ class LocalizationTab(Tab):
         """
         # get the top right view port
         view = self.tab_data_model.views.value[1]
+        self.tab_data_model.focussedView.value = view
         self.tab_data_model.select_current_position_feature()
         for s in data_to_static_streams(data):
             if self.tab_data_model.main.currentFeature.value:
                 s.name.value = self.tab_data_model.main.currentFeature.value.name.value + " - " + s.name.value
                 self.tab_data_model.main.currentFeature.value.streams.value.append(s)
-            self.tab_data_model.streams.value.insert(0, s)
-            self._show_acquired_stream(s, view)
+            self.tab_data_model.streams.value.insert(0, s)  # TODO: let addFeatureStream do that
+            self._acquired_stream_controller.showFeatureStream(s)
+
+    def _on_stage_pos(self, pos):
+        """
+        Called when the stage is moved, enable the tab if position is imaging mode, disable otherwise
+        :param pos: (dict str->float or None) updated position of the stage
+        """
+        if self.main_data.role == "enzel":
+            # TODO: IMAGING position should not be used on ENZEL anymore
+            target = [THREE_BEAMS, IMAGING]
+        elif self.main_data.role == "meteor":
+            target = [FM_IMAGING, SEM_IMAGING]
+        guiutil.enable_tab_on_stage_position(self.button, self.stage, pos, target=target)
+
+    def terminate(self):
+        super(LocalizationTab, self).terminate()
+        self.stage.position.unsubscribe(self._on_stage_pos)
+        # make sure the streams are stopped
+        for s in self.tab_data_model.streams.value:
+            s.is_active.value = False
 
     @classmethod
     def get_display_priority(cls, main_data):
@@ -784,6 +698,15 @@ class LocalizationTab(Tab):
             return 2
         else:
             return None
+
+    def Show(self, show=True):
+        assert (show != self.IsShown())  # we assume it's only called when changed
+        super(LocalizationTab, self).Show(show)
+
+        if not show:  # if localization tab is not chosen
+            # pause streams when not displayed
+            self._streambar_controller.pauseStreams()
+
 
 class SecomStreamsTab(Tab):
     def __init__(self, name, button, panel, main_frame, main_data):
@@ -2755,9 +2678,10 @@ class CryoChamberTab(Tab):
     def _reset_project_data(self):
         try:
             localization_tab = self.tab_data_model.main.getTabByName("cryosecom-localization")
-            localization_tab.clear_overview_streams()
-            localization_tab.clear_live_streams()
+            localization_tab.clear_acquired_streams()
+            localization_tab.reset_live_streams()
             self.tab_data_model.main.features.value = []
+            self.tab_data_model.main.currentFeature.value = None
         except LookupError:
             logging.warning("Unable to find localization tab.")
 

commit 4900ba72745d2baa52f46a577757fcbd22505186

gui comp stream: remove "improvement" to event handler which broke closing streams
In commit a8fec30dee4 (move cryo acquired streams control to the streambar
controller), I tried to "clean" the event removal. However, as the
control is destroyed, the event is automatically removed. In some cases,
this would even cause an error as we were accessing dead objects.

src/odemis/gui/comp/stream.py
@@ -1547,7 +1547,6 @@ class StreamBar(wx.Panel):
         self._sz.Detach(spanel)
         self.stream_panels.remove(spanel)
         spanel.Destroy()
-        spanel.Unbind(wx.EVT_WINDOW_DESTROY, source=spanel, handler=self.on_streamp_destroy)
 
     def clear(self):
         """