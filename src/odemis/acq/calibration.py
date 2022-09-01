068ae1167a2c48bf5b3ab0f76c9877dd5084b7b6
Éric Piel <piel@delmic.com>
2022-09-02 00:47:17 +0200

    acq calibration: bring back failure when trying to set incorrect calibration
    We changed the failure to a logging message. That meant that code
    would not detect that something went wrong.

--- src/odemis/acq/calibration.py
+++ src/odemis/acq/calibration.py
@@ -280,8 +280,9 @@ def apply_spectrum_corrections(data, bckg=None, coef=None):
     # handle time correlator data (chronograph) data
     # -> no spectrum efficiency compensation and bg correction supported
     if data.shape[-5] <= 1 and data.shape[-4] > 1:
-        raise ValueError("Do not support any background correction or spectrum efficiency "
-                         "compensation for time correlator (chronograph) data")
+        if bckg is not None or coef is not None:
+            raise ValueError("Background correction and spectrum efficiency compensation "
+                             "not supported on time correlator (chronograph) data")
 
     # TODO: use MD_BASELINE as a fallback?
     if bckg is not None:
