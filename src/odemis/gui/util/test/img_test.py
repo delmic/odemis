delmic/odemis

commit 0df545dcbf679f7d4b83a96e12f464b33a301a1e

gui util img: make calculate_ticks() symetrical
Whether the range is from 100 -> 1000 or 1000 -> 100, or -1000 -> -100,
the number of ticks and values should be the same (modulo the sign).

Also allow to have ticks near the border, as it's actually quite useful
when reading the graph, and now the code displaying the values do handle
properly the positioning of text next to the border.

src/odemis/gui/util/img.py
@@ -1012,12 +1012,8 @@ def calculate_ticks(value_range, client_size, orientation, tick_spacing):
     # Get the horizontal/vertical space in pixels
     if orientation == wx.HORIZONTAL:
         pixel_space = client_size[0]
-        # Don't display ticks too close from the left border
-        min_pixel = 10
     else:
-        # Don't display ticks too close from the border
         pixel_space = client_size[1]
-        min_pixel = 10
 
     # Skip the NaNs. We don't check that the NaNs are just on the border. We
     # just assume it's so, and if not, the output will be incorrect.
@@ -1056,12 +1052,13 @@ def calculate_ticks(value_range, client_size, orientation, tick_spacing):
     logging.debug("Value step is %s with range %s and requested spacing %s px",
                   value_step, value_space, tick_spacing)
 
-    first_val = (int(min_val / value_step) + 1) * value_step
+    # Find the smallest value multiple of value_step, which is within the range
+    first_val = math.ceil(min_val / value_step) * value_step
     # logging.debug("Setting first tick at value %s", first_val)
 
     tick_values = [min_val] if min_val == 0 else []
     cur_val = first_val
-    while cur_val < max_val:
+    while cur_val <= max_val:
         tick_values.append(cur_val)
         cur_val += value_step
     if len(tick_values) < 2:
@@ -1085,9 +1082,13 @@ def calculate_ticks(value_range, client_size, orientation, tick_spacing):
                     abs(pixel - prev_pixel) < min_margin):
                 # keep a min distance between ticks
                 continue
-            if min_pixel <= pixel <= pixel_space:
-                ticks.append(pix_val)
-                prev_pixel = pixel
+            if not 0 <= pixel <= pixel_space:
+                logging.warning("Computed a tick %s outside of pixel space (%s px)",
+                                tick_value, pixel)
+                continue
+
+            ticks.append(pix_val)
+            prev_pixel = pixel
 
     tick_list = ticks
 

src/odemis/gui/util/test/img_test.py
@@ -137,6 +137,24 @@ class TestCalculateTicks(unittest.TestCase):
             # ticks are made of 2 values
             self.assertEqual(len(ticks[0]), 2)
 
+    def test_neg_value(self):
+        """
+        Check that a range with negative values behaves the same as a range with positive values
+        """
+        ranges = [(100, 1000), (0, 9500), (1e-3, 8e-3), (-5, 6), (-2, -1000)]
+
+        csize = (1000, 737)
+        for rng in ranges:
+            print(f"Trying range {rng}")
+            for orient in (wx.HORIZONTAL, wx.VERTICAL):
+                ticks_pos = calculate_ticks(rng, csize, orient, csize[0] / 10)
+                ticks_pos_inv = calculate_ticks((rng[1], rng[0]), csize, orient, csize[0] / 10)
+                self.assertEqual(len(ticks_pos), len(ticks_pos_inv))
+                ticks_neg = calculate_ticks((-rng[1], -rng[0]), csize, orient, csize[0] / 10)
+                self.assertEqual(len(ticks_pos), len(ticks_neg))
+                ticks_neg_inv = calculate_ticks((-rng[0], -rng[1]), csize, orient, csize[0] / 10)
+                self.assertEqual(len(ticks_pos), len(ticks_neg_inv))
+
     def test_empty_range(self):
         # Should display just one tick: the value of the range
         ticks = calculate_ticks((900, 900), (50, 600), wx.HORIZONTAL, 20)

commit fc501c4bf9df52591faf88598378043f1e6c3980

gui util img: fix calculate_ticks when passing empty client size
fix bug introduced in 0df545dc (gui util img: make calculate_ticks()
symetrical).

Also add a test case to explicitly check for this corner case.

src/odemis/gui/util/img.py
@@ -1025,7 +1025,7 @@ def calculate_ticks(value_range, client_size, orientation, tick_spacing):
     if value_space == 0:
         logging.info("Trying to compute legend tick with empty range %s", value_range)
         # Just one tick, at the origin
-        pixel = max(min_pixel, value_to_pixel(min_val, pixel_space, value_range, orientation))
+        pixel = value_to_pixel(min_val, pixel_space, value_range, orientation)
         tick_list = [(pixel, min_val)]
         return tick_list
 

src/odemis/gui/util/test/img_test.py
@@ -203,7 +203,7 @@ class TestCalculateTicks(unittest.TestCase):
         ticks = calculate_ticks(value_range, csize, wx.VERTICAL, csize[1] / 10)
         self.assertGreaterEqual(len(ticks), 2)  # There should be at least 2 ticks
         for pos_px, val in ticks:
-            self.assertTrue(0 < pos_px < csize[1] / 2, f"pos_px = {pos_px}")  # Every tick should be within the first half
+            self.assertTrue(0 <= pos_px < csize[1] / 2, f"pos_px = {pos_px}")  # Every tick should be within the first half
             self.assertTrue(0 <= val <= 3)  # Every tick value should be within the value_range
 
         # Reversed vertical
@@ -213,6 +213,27 @@ class TestCalculateTicks(unittest.TestCase):
         for pos_px, val in ticks:
             self.assertTrue(csize[1] / 2 < pos_px < csize[1], f"pos_px = {pos_px}")  # Every tick should be within the first half
             self.assertTrue(0 <= val <= 3)  # Every tick value should be within the value_range
+
+    def test_empty(self):
+        """
+        Check it handles corner case where the client size is 0 (eg, a window has been
+        resized to a very small area)
+        """
+        # No space => should return either 0 or 1 tick
+        value_range = [numpy.nan] * 5 + [0, 1, 2, 3]
+        csize = (0, 0)
+        ticks = calculate_ticks(value_range, csize, wx.HORIZONTAL, 10)
+        self.assertIn(len(ticks), (0, 1))
+        if ticks:
+            # If there is a tick, it should be at pixel 0
+            self.assertEqual(ticks[0][0], 0)
+
+        # Same thing vertically
+        ticks = calculate_ticks(value_range, csize, wx.VERTICAL, 10)
+        self.assertIn(len(ticks), (0, 1))
+        if ticks:
+            # If there is a tick, it should be at pixel 0
+            self.assertEqual(ticks[0][0], 0)
 
 
 class TestARExport(unittest.TestCase):