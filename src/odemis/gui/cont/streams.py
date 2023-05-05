c6cfac589e087fb18559d20151903a3e78897658
Nandish Patel <46875149+nandishjpatel@users.noreply.github.com>
2023-05-05 12:51:10 +0200

    Update comment
    Co-authored-by: Thera Pals <46314774+tepals@users.noreply.github.com>

--- src/odemis/gui/cont/streams.py
+++ src/odemis/gui/cont/streams.py
@@ -346,7 +346,7 @@ class StreamController(object):
 
         # Process the hardware VAs first (emitter and detector hardware VAs are combined into one
         # attribute called 'hw_vas'
-        # sort the la first in case lb does not contain an item from la, it will then be appended in the end
+        # sort the list of hardware VAs first in case it contains items that are not in the emitter and detector conf, they will be appended at the end
         vas_names = util.sorted_according_to(sorted(list(self.stream.hw_vas.keys())),
                                              list(emitter_conf.keys()) + list(detector_conf.keys()))
 
