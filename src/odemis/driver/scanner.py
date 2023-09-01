2cf5b4d5076a634d47045731f39dd1fc5c84cd74
Éric Piel <piel@delmic.com>
2023-09-01 16:52:12 +0800

    [feature] driver scanner: support multiple detectors
    The detector children allow to have automatically external and blanker
    signals. However, for the SPARC, they are several detectors connected to
    the e-beam. So we need to support multiple detectors at the same time.
    
    Also fix some docstrings

--- src/odemis/driver/scanner.py
+++ src/odemis/driver/scanner.py
@@ -20,6 +20,7 @@ You should have received a copy of the GNU General Public License along with
 Odemis. If not, see http://www.gnu.org/licenses/.
 '''
 import logging
+from typing import Hashable
 
 from odemis import model
 from odemis.model import ComponentBase
@@ -37,6 +38,8 @@ class CompositedScanner(model.Emitter):
         '''
         dependencies (dict string->model.HwComponent): the dependencies
             There must be exactly two dependencies "external" and "internal".
+        children (dict str->dict): An optional "detector" which will create a
+            CompositedDetector
         Raise:
           ValueError: if the dependencies are not compatible
         '''
@@ -45,8 +48,6 @@ class CompositedScanner(model.Emitter):
 
         if children is None:
             children = {}
-        elif set(children.keys()) - {"detector"}:
-            raise ValueError("Only a detector child is supported")
 
         # we will fill the set of dependencies with Components later in ._dependencies
         model.Emitter.__init__(self, name, role, dependencies=dependencies, daemon=daemon, **kwargs)
@@ -101,18 +102,18 @@ class CompositedScanner(model.Emitter):
         self._va_external_ctrl = None
         self._va_blanker_ctrl = None
 
-        if "detector" in children:
-            ckwargs = children["detector"]
-            self._detector = CompositedDetector(parent=self, daemon=daemon, **ckwargs)
-            self.children.value.add(self._detector)
+        for cname, ckwargs in children.items():
+            if not cname.startswith("detector"):
+                raise ValueError(f"Only supports children with role as 'detector...', but got '{cname}'")
+
+            detector = CompositedDetector(parent=self, daemon=daemon, **ckwargs)
+            self.children.value.add(detector)
 
             # Setting up the automatic blanker on the composited scanner
             if model.hasVA(self._external_scanner, "blanker") and None in self._external_scanner.blanker.choices:
                 self.blanker = self._external_scanner.blanker
-
             elif model.hasVA(self._external_scanner, "blanker") and None not in self._external_scanner.blanker.choices:
                 self._createAutoBlanker(self._external_scanner)
-
             elif model.hasVA(self._internal_scanner, "blanker"):  # Internal blankers are never automatic, so we create one
                 self._createAutoBlanker(self._internal_scanner)
             else:
@@ -121,16 +122,15 @@ class CompositedScanner(model.Emitter):
             # Setting up the automatic external VA on the composited scanner (For switching between acquisition mode and external mode on the SEM)
             if model.hasVA(self._external_scanner, "external") and None in self._external_scanner.external.choices:
                 self.external = self._external_scanner.external
-
             elif model.hasVA(self._external_scanner, "external") and None not in self._external_scanner.external.choices:
                 self._createAutoExternal(self._external_scanner)
-
             elif model.hasVA(self._internal_scanner, "external"):
                 self._createAutoExternal(self._internal_scanner)
             else:
                 logging.debug("No external VA supported for the Composited Scanner.")
 
-        else:
+        # If no automatic blanker/external, just duplicate the original ones
+        if not self.children.value:
             va_names += ["blanker", "external"]
 
         for vaname in va_names:
@@ -141,7 +141,7 @@ class CompositedScanner(model.Emitter):
                 va = getattr(self._external_scanner, vaname)
                 setattr(self, vaname, va)
 
-        self._beamIsUsed = False
+        self._beamUsers = set()  # contains any object
 
     def _createAutoBlanker(self, blanker_ctrl):
         """
@@ -150,6 +150,9 @@ class CompositedScanner(model.Emitter):
         blanker_ctrl (Scanner): component with a blanker VA to set the actual
           blanker state on/off
         """
+        if self._va_blanker_ctrl is not None:
+            return
+
         self._va_blanker_ctrl = blanker_ctrl
         self.blanker = model.VAEnumerated(
             None,
@@ -163,6 +166,9 @@ class CompositedScanner(model.Emitter):
         external_ctrl (Scanner): component with a external VA to set the actual
           blanker state on/off
         """
+        if self._va_external_ctrl is not None:
+            return
+
         self._va_external_ctrl = external_ctrl
         self.external = model.VAEnumerated(
             None,
@@ -173,7 +179,7 @@ class CompositedScanner(model.Emitter):
         if blank is not None:
             self._va_blanker_ctrl.blanker.value = blank
         else:
-            self._va_blanker_ctrl.blanker.value = not self._beamIsUsed
+            self._va_blanker_ctrl.blanker.value = not bool(self._beamUsers)
 
         return blank
 
@@ -181,7 +187,7 @@ class CompositedScanner(model.Emitter):
         if mode is not None:
             self._va_external_ctrl.external.value = mode
         else:
-            self._va_external_ctrl.external.value = self._beamIsUsed
+            self._va_external_ctrl.external.value = bool(self._beamUsers)
 
         return mode
 
@@ -201,30 +207,36 @@ class CompositedScanner(model.Emitter):
     def getMetadata(self):
         return self._external_scanner.getMetadata()
 
-    def claimBeam(self, claim):
+    def claimBeam(self, claim: bool, user: Hashable):
         """
         Used to indicate the start and end of beam usage. It takes care of
           updating the blanker and external control, if present and set to None
           (ie, automatic control).
         :param claim (boolean): True for when starting to use the beam. False when
           not using the beam any more.
+        :param user: an object used to identify who is claiming (or not) the beam
         """
-        self._beamIsUsed = claim
+        if claim:
+            self._beamUsers.add(user)
+        else:
+            self._beamUsers.discard(user)
+
+        in_use = bool(self._beamUsers)
 
         if self._va_blanker_ctrl and self.blanker.value is None:
             # Disable the blanker when using the beam
-            self._va_blanker_ctrl.blanker.value = not claim
+            self._va_blanker_ctrl.blanker.value = not in_use
 
         if self._va_external_ctrl and self.external.value is None:
             # Activate the external mode when using the beam
-            self._va_external_ctrl.external.value = claim
+            self._va_external_ctrl.external.value = in_use
 
 
 class CompositedDetector(model.Detector):
     '''
-    A generic Detector which takes 2 dependencies to create a one detector. It's
-    essentially a wrapper to an Detector to generate data using the "external"
-    detector while manipulating the "internal" detector.
+    A wrapper Detector which can be used in addition to the CompositedScanner.
+    It's used to automatically control the external and/or blanker when an
+    acquisition starts and stops.
     '''
     def __init__(self, name, role, parent, dependencies, **kwargs):
         '''
@@ -276,7 +288,7 @@ class CompositedDataflow(model.DataFlow):
         Sets the scan mode to "external" and subscribes self.notify to the external dataflow
         :return:
         """
-        self._composited_scanner.claimBeam(True)
+        self._composited_scanner.claimBeam(True, self)
         self._external_dataflow.subscribe(self._on_data)
 
     def stop_generate(self):
@@ -284,7 +296,7 @@ class CompositedDataflow(model.DataFlow):
         Unsubscribes self.notify of the external dataflow and updates the external and blanker VA's
         """
         self._external_dataflow.unsubscribe(self._on_data)
-        self._composited_scanner.claimBeam(False)
+        self._composited_scanner.claimBeam(False, self)
 
     def _on_data(self, dataflow, data):
         """
