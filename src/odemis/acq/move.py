60e2b3b743029081aab4f1ef5cd1fa1613584a23
Karishma Kumar <kumar@delmic.com>
2023-09-11 17:03:15 +0200

    add a TODO

--- src/odemis/acq/move.py
+++ src/odemis/acq/move.py
@@ -502,6 +502,7 @@ class MeteorTFS1PostureManager(MeteorPostureManager):
             if target in (GRID_1, GRID_2):
                 # The current mode doesn't change. Only X/Y/Z should move (typically
                 # only X/Y).
+                # TODO: probably a better way would be to forbid grid switch if not in SEM/FM imaging posture
                 sub_moves.append((self.stage, filter_dict({'x', 'y', 'z'}, target_pos)))
                 sub_moves.append((self.stage, filter_dict({'rx', 'rz'}, target_pos)))
             elif target in (LOADING, SEM_IMAGING, FM_IMAGING):
