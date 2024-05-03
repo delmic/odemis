7a823cd0385d5eeafcaefee16327bd5656421713
Karishma Kumar <kumar@delmic.com>
2024-05-03 15:11:35 +0200

    include offset correction in x and y when raw coordinates are read - untested

--- src/odemis/driver/xt_client.py
+++ src/odemis/driver/xt_client.py
@@ -2158,7 +2158,14 @@ class Stage(model.Actuator):
 
     def __init__(self, name: str, role: str, parent: model.HwComponent, rng: dict = None, raw: bool = False,
                  **kwargs) -> None:
-        parent.set_raw_coordinate_system(raw)
+        self.raw = raw
+        self.parent = parent
+        # define axes which needs offset correction such that TFS stage coordinates
+        # are equal to stage coordinates displayed through Odemis
+        self.offset = {"x": 0, "y": 0}
+        # update the offset if raw stage coordinates are read
+        self._get_offset_raw_coordinate_system()
+        parent.set_raw_coordinate_system(self.raw)
         if rng is None:
             rng = {}
         stage_info = parent.stage_info()
@@ -2194,12 +2201,35 @@ class Stage(model.Actuator):
         self._pos_poll = util.RepeatingTimer(5, self._refreshPosition, "Stage position polling")
         self._pos_poll.start()
 
+    def _get_offset_raw_coordinate_system(self) -> None:
+        """Calculate the offset in linear stage axes x and y when the stage coordinates are read in raw coordinates such
+         that it is equal to x and y when stage coordinates are read in linked coordinates."""
+        # TODO needs to be check on hardware
+        # 1. by checking if raw coordinates displayed by TFS system is the same as Odemis
+        # 2. by checking if same feature is visible in FM after switching from SEM
+        if self.raw:
+            pos = self._getPosition()
+            self.parent.set_raw_coordinate_system(False)
+            pos_after = self._getPosition()
+            for axis in self.offset.keys():
+                self.offset[axis] = pos_after[axis] - pos[axis]
+            # the offset should only be in linear axes, it is not expected in rotational axes
+            assert all(pos[axis] == pos_after[axis] for axis in ["rx", "rz"])
+            logging.debug(f"The offset values in x and y are {self.offset} when stage is in the raw coordinate system")
+            # Set back the original coordinate system
+            self.parent.set_raw_coordinate_system(self.raw)
+
     def _updatePosition(self) -> None:
         """
         update the position VA
         """
         old_pos = self.position.value
         pos = self._getPosition()
+        if self.raw:
+            # correct for the offset such that the stage coordinates displayed in
+            # TFS software is the same as Odemis
+            pos["x"] = pos["x"] + self.offset["x"]
+            pos["y"] = pos["y"] + self.offset["y"]
         self.position._set_value(self._applyInversion(pos), force_write=True)
         if old_pos != self.position.value:
             logging.debug("Updated position to %s", self.position.value)
