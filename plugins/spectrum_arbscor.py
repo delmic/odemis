ed1a8eeb0501dc781842cd24905c7a2ab4a9d5d4
Éric Piel <piel@delmic.com>
2024-05-24 10:30:46 +0200

    [refactor] update MDStream API on plugins
    MDStream._assembleLiveData() has one new argument => need to update
    all the overridding methods. Also, make use of it when it can be useful
    (ie, instead of relying on the SEM metadata)

--- plugins/spectrum_arbscor.py
+++ plugins/spectrum_arbscor.py
@@ -141,23 +141,25 @@ class SEMSpectrumArbitraryOrderMDStream(SEMSpectrumMDStream):
         return pos
 
     def _assembleLiveData(self, n: int, raw_data: "DataArray",
-                          px_idx: Tuple[int, int], rep: Tuple[int, int], pol_idx: int):
+                          px_idx: Tuple[int, int], px_pos: Tuple[float, float],
+                          rep: Tuple[int, int], pol_idx: int = 0):
         """
         Wrapper for _assembleLiveData() to convert back the standard px_idx (eg, (0,0), (0,1), (0,2)...)
         into the index that was actually scanned at that moment.
         :param px_idx: y, x position
+        :param pxs_pos: position of center of data in m: x, y
         :param rep: x, y number of points in the scan
         For other parameters, see MultipleDetectorStream._assembleLiveData()
         """
         px_idx_flat = px_idx[0] * rep[0] + px_idx[1]
         act_px_idx = self._px_order[px_idx_flat][::-1]
         logging.debug("Converted back idx %s to %s", px_idx, act_px_idx)
-        return super()._assembleLiveData(n, raw_data, act_px_idx, rep, pol_idx)
+        return super()._assembleLiveData(n, raw_data, act_px_idx, px_pos, rep, pol_idx)
 
 
 class SpectrumArbitraryScanOrderPlugin(Plugin):
     name = "Spectrum acquisition in arbitrary scan order"
-    __version__ = "1.0"
+    __version__ = "1.1"
     __author__ = u"Éric Piel"
     __license__ = "GPLv2"
 
