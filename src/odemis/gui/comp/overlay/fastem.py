a334ce0ad3d4fa246b4071648f4e49b899025046
Nandish Patel <nandish221993@yahoo.in>
2024-08-10 11:16:04 +0200

    [feat] FastEMScintillatorOverlay class creates a background overlay for a Scintillator as per its shape
    The currently supported scintillator shapes are RectangleScintillator and CircleScintillator

--- src/odemis/gui/comp/overlay/fastem.py
+++ src/odemis/gui/comp/overlay/fastem.py
@@ -21,20 +21,36 @@ This file is part of Odemis.
     Odemis. If not, see http://www.gnu.org/licenses/.
 
 """
+import math
+
 import wx
 
 import odemis.gui as gui
 from odemis import model, util
 from odemis.acq.stream import UNDEFINED_ROI
-from odemis.gui.comp.overlay.base import (SEL_MODE_NONE, SEL_MODE_DRAG,
-                                          DragMixin, Vec, WorldOverlay)
+from odemis.gui.comp.overlay.base import (
+    SEL_MODE_DRAG,
+    SEL_MODE_NONE,
+    DragMixin,
+    Vec,
+    WorldOverlay,
+)
 from odemis.gui.comp.overlay.world_select import WorldSelectOverlay
+from odemis.gui.model.main_gui_data import (
+    CircleScintillator,
+    RectangleScintillator,
+    Scintillator,
+)
+
+NUM_RINGS = 11
 
 
 class FastEMROCOverlay(WorldSelectOverlay):
-    """ Overlay representing one region of calibration (ROC) on the FastEM. """
+    """Overlay representing one region of calibration (ROC) on the FastEM."""
 
-    def __init__(self, cnvs, coordinates, label, sample_bbox, colour=gui.SELECTION_COLOUR):
+    def __init__(
+        self, cnvs, coordinates, label, sample_bbox, colour=gui.SELECTION_COLOUR
+    ):
         """
         cnvs (FastEMAcquisitionCanvas): canvas for the overlay
         coordinates (TupleContinuousVA): VA of 4 floats representing region of calibration coordinates
@@ -57,6 +73,7 @@ class FastEMROCOverlay(WorldSelectOverlay):
         """
         if coordinates != UNDEFINED_ROI:
             self.set_physical_sel(coordinates)
+            self._phys_to_view()
             wx.CallAfter(self.cnvs.request_drawing_update)
 
     def on_left_down(self, evt):
@@ -85,12 +102,16 @@ class FastEMROCOverlay(WorldSelectOverlay):
             self._view_to_phys()
             rect = self.get_physical_sel()
             if rect:
-                pos = self.cnvs.view_to_phys(evt.Position, self.cnvs.get_half_buffer_size())
+                pos = self.cnvs.view_to_phys(
+                    evt.Position, self.cnvs.get_half_buffer_size()
+                )
                 # The calibration region often needs to be selected from a distant zoom level, so it is difficult
                 # to select a point inside the rectangle with the mouse. Instead, we consider a selection "inside"
                 # the rectangle if the selection is near (based on mpp value, so independent of scale).
                 margin = self.cnvs.view.mpp.value * 20
-                self.selected.value = util.is_point_in_rect(pos, util.expand_rect(rect, margin)) or (self.selection_mode == SEL_MODE_DRAG)
+                self.selected.value = util.is_point_in_rect(
+                    pos, util.expand_rect(rect, margin)
+                ) or (self.selection_mode == SEL_MODE_DRAG)
                 if self.selected.value:
                     self._coordinates.value = rect
 
@@ -162,27 +183,98 @@ class FastEMROCOverlay(WorldSelectOverlay):
             self._write_labels(ctx)
 
 
-class FastEMBackgroundOverlay(WorldOverlay):
-    """ Background overlay. Displays a list of rectangles in grey to simulate a background, e.g. a sample carrier. """
-
-    def __init__(self, cnvs, rectangles):
+class FastEMScintillatorOverlay(WorldOverlay):
+    def __init__(self, cnvs, scintillator: Scintillator):
         """
         cnvs (FastEMAcquisitionCanvas): canvas for the overlay
-        rectangles (list of tuples of 4 floats): l, t, r, b positions of rectangle in m
         """
-        super(FastEMBackgroundOverlay, self).__init__(cnvs)
-        self.rectangles = rectangles
+        super(FastEMScintillatorOverlay, self).__init__(cnvs)
+        self.shape = scintillator.shape
+        self._colour = (1.0, 1.0, 1.0, 1.0)  # White
+        self._label = self.add_label(
+            str(scintillator.number),
+            align=wx.ALIGN_CENTRE_HORIZONTAL | wx.ALIGN_CENTER_VERTICAL,
+            colour=self._colour,
+            background=(0.0, 0.0, 0.0, 1.0),  # Black
+        )
+        if isinstance(self.shape, RectangleScintillator):
+            self._surrounding_rectangles = self._calculate_surrounding_rectangles()
+
+    def get_scintillator_bbox(self):
+        return self.shape.get_bbox()
+
+    def _calculate_surrounding_rectangles(self, scale=1.0):
+        rects = []
+        minx, miny, maxx, maxy = self.get_scintillator_bbox()
+
+        width, height = self.shape.get_size()
+        scaled_width = scale * width
+        scaled_height = scale * height
+
+        # Surrounding rectangles
+        # left
+        rects.append(
+            [minx - scaled_width, miny - scaled_height, minx, maxy + scaled_height]
+        )
+
+        # right
+        rects.append(
+            [maxx, miny - scaled_height, maxx + scaled_width, maxy + scaled_height]
+        )
+
+        # top
+        rects.append(
+            [minx - scaled_width, maxy, maxx + scaled_width, maxy + scaled_height]
+        )
+
+        # bottom
+        rects.append(
+            [minx - scaled_width, miny - scaled_height, maxx + scaled_width, miny]
+        )
+        return rects
 
     def draw(self, ctx, shift=(0, 0), scale=1.0):
-        """ Draw the background image by displaying all rectangles in grey. """
-        for r in self.rectangles:
-            offset = self.cnvs.get_half_buffer_size()
-            b_start_pos = self.cnvs.phys_to_buffer((r[0], r[1]), offset)
-            b_end_pos = self.cnvs.phys_to_buffer((r[2], r[3]), offset)
-            rect = (b_start_pos[0],
+        """Draw the background image by displaying all shapes in grey."""
+        offset = self.cnvs.get_half_buffer_size()
+
+        if isinstance(self.shape, RectangleScintillator):
+            for r in self._surrounding_rectangles:
+                b_start_pos = self.cnvs.phys_to_buffer((r[0], r[1]), offset)
+                b_end_pos = self.cnvs.phys_to_buffer((r[2], r[3]), offset)
+                rect = (
+                    b_start_pos[0],
                     b_start_pos[1],
                     b_end_pos[0] - b_start_pos[0],
-                    b_end_pos[1] - b_start_pos[1])
-            ctx.set_source_rgba(0.5, 0.5, 0.5, 1)  # grey
-            ctx.rectangle(*rect)
-            ctx.fill()
+                    b_end_pos[1] - b_start_pos[1],
+                )
+                ctx.set_source_rgba(0.5, 0.5, 0.5, 1)  # grey
+                ctx.rectangle(*rect)
+                ctx.fill()
+        elif isinstance(self.shape, CircleScintillator):
+            x, y = self.shape.position
+            r = self.shape.radius
+            b_center_pos = self.cnvs.phys_to_buffer((x, y), offset)
+            b_radius = self.cnvs.phys_to_buffer((x + r, y), offset)[0] - b_center_pos[0]
+
+            # Parameters for the ring effect
+            ring_thickness = b_radius * 0.05  # Thickness of each ring
+
+            # Draw multiple concentric rings
+            for i in range(1, NUM_RINGS):
+                outer_radius = b_radius + i * ring_thickness
+                ctx.set_source_rgba(0.5, 0.5, 0.5, 1)  # grey
+                if i > 1:
+                    ctx.set_line_width(1.1 * ring_thickness)  # Line width for the ring
+                else:
+                    ctx.set_line_width(ring_thickness)  # Line width for the ring
+                ctx.new_sub_path()  # Start a new path to avoid connecting to previous drawings
+                ctx.arc(b_center_pos[0], b_center_pos[1], outer_radius, 0, 2 * math.pi)
+                ctx.stroke()
+
+        # Draw the scintillator number label at the bottom of the shape
+        position = self.shape.position
+        size = self.shape.get_size()
+        p_label_pos = (position[0], position[1] - 0.6 * size[1])
+        b_label_pos = self.cnvs.phys_to_buffer(p_label_pos, offset)
+        self._label.pos = b_label_pos
+        self._write_labels(ctx)
