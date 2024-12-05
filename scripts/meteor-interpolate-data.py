371af0eb35faa855793cccb5784dc649ceffe749
Patrick Cleeve <cleeve@delmic.com>
2024-12-06 09:33:46 +1100

    [feat] add interpolation utility for zstacks

---
+++ scripts/meteor-interpolate-data.py
@@ -0,0 +1,104 @@
+import argparse
+import glob
+import logging
+import os
+
+from odemis.dataio import tiff
+from odemis.util.dataio import open_acquisition
+from odemis.util.interpolation import multi_channel_interpolation
+
+logging.getLogger().setLevel(level=logging.INFO)
+
+# REF: from https://github.com/patrickcleeve2/3DCT/blob/refactor/tdct/util.py
+
+def interpolate_meteor_zstack(
+    filename: str,
+    pixelsize_in: float = None,
+    pixelsize_out: float = None,
+    method: str = "linear",
+) -> None:
+    """Open a metoer Interpolate METEOR z-stack data
+    :param filename: str: input filename
+    :param pixelsize_in: float: input z-step size
+    :param pixelsize_out: float: output z-step size
+    :param method: str: interpolation method
+    :return: None"""
+    # assumes all channels have the same pixelsize, zstep, shape
+
+    # open data
+    dat = open_acquisition(filename)
+
+    # interpolate z-stack
+    ndat = multi_channel_interpolation(
+        dat=dat,
+        pixelsize_in=pixelsize_in,
+        pixelsize_out=pixelsize_out,
+        method=method
+    )
+
+    # save data
+    new_filename = filename.replace(".ome.tiff", "-interpolated.ome.tiff")
+    tiff.export(new_filename, ndat)
+    logging.info(f"Interpolated data saved to: {new_filename}")
+
+
+def main():
+    argparser = argparse.ArgumentParser(description="Interpolate METEOR z-level data")
+    argparser.add_argument("filename", type=str, help="Input filename or directory")
+    argparser.add_argument(
+        "--zstep", type=float, help="Output Z-step size", required=False
+    )
+    argparser.add_argument(
+        "--input", type=float, help="Input Z-step size", required=False
+    )
+    argparser.add_argument(
+        "--method",
+        type=str,
+        help="Interpolation method",
+        default="linear",
+        choices=["linear", "cubic"],
+    )
+    args = argparser.parse_args()
+
+    filename = args.filename
+    zstep = args.zstep
+    input_zstep = args.input
+    method = args.method
+
+    filenames = []
+    if os.path.isdir(filename):
+        logging.info(f"Interpolating all files in directory: {filename}")
+        filenames = glob.glob(os.path.join(filename, "*.ome.tiff"))
+        # exclude filenames with "overview" or "interpolated"
+        filenames = [
+            f for f in filenames if "overview" not in f and "interpolated" not in f
+        ]
+
+    if os.path.isfile(filename):
+        logging.info(f"Interpolating single file: {filename}")
+        filenames = [filename]
+
+    logging.info(f"Number of files to interpolate: {len(filenames)}")
+
+    if input_zstep:
+        logging.info(f"Input Z-step size: {input_zstep}")
+    if zstep:
+        logging.info(f"Output Z-step size: {zstep}")
+
+    # interpolate all files
+    for filename in filenames:
+        logging.info("-" * 50)
+        try:
+            logging.info(f"Interpolating file: {filename}")
+            interpolate_meteor_zstack(
+                filename=filename,
+                pixelsize_in=input_zstep,
+                pixelsize_out=zstep,
+                method=method,
+            )
+        except Exception as e:
+            logging.error(f"Error interpolating file: {filename}. error: {e}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/odemis/util/interpolation.py
@@ -0,0 +1,123 @@
+import copy
+import logging
+from typing import List
+
+import numpy as np
+from scipy import ndimage
+
+from odemis import model
+
+# REF: from https://github.com/patrickcleeve2/3DCT/blob/refactor/tdct/util.py
+
+def interpolate_z_stack(da: model.DataArray,
+                        pixelsize_in: float = None,
+                        pixelsize_out: float = None,
+                        method: str = "linear") -> model.DataArray:
+    # check for shadow
+    if isinstance(da, model.DataArrayShadow):
+        da = da.getData()
+
+    # read pixelsize
+    pixelsize = da.metadata[model.MD_PIXEL_SIZE]
+
+    # default to isotropic pixels
+    if pixelsize_in is None:
+        pixelsize_in = pixelsize[2] # default to z-pixelsize
+    if pixelsize_out is None:
+        pixelsize_out = pixelsize[0] # default to x-pixelsize
+
+    # CTZYX data, squeeze down to ZYX
+    # NOTE: Attempted to use 5D data, but it was 10x slower than 3D data
+    if da.ndim == 5:
+        # initially only support CTZYX data with C=1, T=1 (meteor data)
+        if da.metadata[model.MD_DIMS] != "CTZYX":
+            raise ValueError(f"Got dims {da.metadata[model.MD_DIMS]}, expected CTZYX")
+        if da.shape[0] != 1 or da.shape[1] != 1:
+            raise ValueError(
+                f"Got shape {da.shape}, expected (1, 1, Z, Y, X)"
+            )
+        da = np.squeeze(da, axis=(0, 1))
+
+    if da.ndim != 3:
+        raise ValueError(f"data must be a ZYX array, but got {da.ndim}")
+
+    # interpolate along z-axis
+    logging.info(f"Interpolating z-stack from {pixelsize_in:.2e} to {pixelsize_out:.2e} with method {method}")
+    interpolated = z_interpolation(da, pixelsize_in, pixelsize_out, method=method)
+
+    # add back channel dimension
+    interpolated = np.expand_dims(interpolated, axis=(0, 1))
+
+    # update metadata
+    md = copy.deepcopy(da.metadata)
+    md[model.MD_PIXEL_SIZE] = (pixelsize[0], pixelsize[1], pixelsize_out)
+
+    return model.DataArray(interpolated, md)
+
+def z_interpolation(
+    da: np.ndarray,
+    original_z_size: float,
+    target_z_size: float,
+    method: str = "linear",
+) -> np.ndarray:
+    """Interpolate a 3D image array along the z-axis using scipy's zoom function.
+    :param da: 3D numpy array (ZYX)
+    :param original_z_size: original pixel size in z-axis
+    :param target_z_size: desired pixel size in z-axis
+    :param method: interpolation method, one of "nearest-neighbor", "linear", "cubic"
+    :return: interpolated 3D numpy array
+    """
+    # Create zoom factors for each dimension
+    # Only scale the z-axis (first dimension)
+    zoom_factors = (original_z_size / target_z_size, 1, 1)
+
+    if method not in ["nearest-neighbor", "linear", "cubic"]:
+        logging.warning(f"method {method} not supported, using linear instead")
+        method = "linear"
+
+    # Map method to scipy's order parameter
+    method_map = {
+        "nearest-neighbor": 0,
+        "linear": 1,
+        "cubic": 3,
+    }
+
+    interpolated = ndimage.zoom(
+        input=da,
+        zoom=zoom_factors,
+        order=method_map[method],
+        mode="reflect",  # to handle edge cases
+        prefilter=True,  # for better quality
+    )
+
+    return interpolated
+
+
+#### multi-channel interpolation ####
+
+def multi_channel_interpolation(
+    dat: List[model.DataArray],
+    pixelsize_in: float = None,
+    pixelsize_out: float = None,
+    method: str = "linear",
+) -> List[model.DataArray]:
+    """Interpolate a multi-channel z-stack (CZYX) along the z-axis
+    :param dat: list of DataArray (CTZYX)
+    :param pixelsize_in: original pixel size in z-axis (default to z-pixelsize)
+    :param pixelsize_out: desired pixel size in z-axis (default to x-pixelsize)
+    :param method: interpolation method, one of "nearest-neighbor", "linear", "cubic"
+    :return interpolated: list of interpolated DataArray (CTZYX)
+    """
+    # QUERY: how to speed up?
+    ch_interpolated = []
+    for i, da in enumerate(dat):
+        logging.info(f"Interpolating channel {i+1}/{len(dat)}")
+        ch_interpolated.append(
+            interpolate_z_stack(
+                da=da,
+                pixelsize_in=pixelsize_in,
+                pixelsize_out=pixelsize_out,
+                method=method,
+            )
+        )
+    return ch_interpolated
