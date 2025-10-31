229e501d7a774e3c700596f4c8c81f4a39375fe1
Thera Pals <pals@delmic.com>
2025-10-31 12:08:07 +0100

    [test] fix n_jobs usage in spot_test for ubuntu 24
    In newer versions of scipy kdtree the usage of the `n_jobs` argument is replaced by the `workers` argument. This was already fixed in the code, but not yet in the test case.
    
    This fixes the following error:
    ```
    spot_test.py:335: in test_multiple
        distances, indices = tree.query(ji, k=1, n_jobs=-1)
    _ckdtree.pyx:786: in scipy.spatial._ckdtree.cKDTree.query
        ???
    _ckdtree.pyx:388: in scipy.spatial._ckdtree.get_num_workers
        ???
    E   TypeError: Unexpected keyword argument {'n_jobs': -1}
    ```

--- src/odemis/util/test/spot_test.py
+++ src/odemis/util/test/spot_test.py
@@ -27,6 +27,8 @@ import unittest
 
 TEST_IMAGE_PATH = os.path.dirname(__file__)
 
+scipy_version = tuple(map(int, scipy.__version__.split(".")))
+scipy_old_ckdtree = (scipy_version < (1, 6, 0))
 
 class TestMomentOfInertia(unittest.TestCase):
     """
@@ -332,7 +334,13 @@ class TestFindSpotPositions(unittest.TestCase):
         # Map the found spot locations to the known spot locations.
         tree = scipy.spatial.cKDTree(loc)
         # NOTE: Starting SciPy v1.6.0 the `n_jobs` argument will be renamed `workers`
-        distances, indices = tree.query(ji, k=1, n_jobs=-1)
+        # Ubuntu 20.04: v1.3.3 -> n_jobs
+        # Ubuntu 22.04: v1.8.0 -> workers or n_jobs
+        # Ubuntu 24.04: v1.11.4 -> workers
+        if scipy_old_ckdtree:
+            distances, indices = tree.query(ji, k=1, n_jobs=-1)
+        else:
+            distances, indices = tree.query(ji, k=1, workers=-1)
 
         # Check that all spot positions have been found within the required
         # accuracy.
