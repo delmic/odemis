384b6efc7673250b11e26fee7c729244d28dfdc2
Éric Piel <piel@delmic.com>
2026-08-06 21:19:00 +0800

    [feat] plugin SECOM user simulator: new simulation mode for intensive usage
    Allow to test moving the focus and at the same time the filter and
    stigmator move too. This is for testing the "worse case scenario".

--- plugins/secom_user_sim.py
+++ plugins/secom_user_sim.py
@@ -4,7 +4,7 @@
 Created on 17 May 2018
 
 @author: Éric Piel
-Copyright © 2018 Éric Piel, Delmic
+Copyright © 2018-2026 Éric Piel, Delmic
 
 This file is part of Odemis.
 
@@ -18,20 +18,29 @@ Public License for more details.
 You should have received a copy of the GNU General Public License along with Odemis. If not,
 see http://www.gnu.org/licenses/.
 '''
-
-from collections import OrderedDict
 import logging
-from odemis.acq.stream import FluoStream
-from odemis.gui.model import TabName
-from odemis.gui.plugin import Plugin, AcquisitionDialog
+import math
 import random
 import threading
+
 import wx
+
+from odemis.acq.stream import FluoStream
+from odemis.gui.model import MicroscopyGUIData
+from odemis.gui.plugin import Plugin, AcquisitionDialog
+
+try:
+    from odemis.gui.model import TabName
+except ImportError:
+    # For backward compatibility with Odemis < 3.9
+    class TabName:
+        SECOM_LIVE = "secom_live"
+        CRYOSECOM_LOCALIZATION = "cryosecom-localization"
 
 
 class SecomUserPlugin(Plugin):
     name = "SECOM/METEOR user simulator"
-    __version__ = "1.1"
+    __version__ = "1.2"
     __author__ = "Éric Piel"
     __license__ = "GPLv2"
 
@@ -41,18 +50,24 @@ class SecomUserPlugin(Plugin):
         if not microscope:
             return
 
-        self.addMenu("Help/Development/Simulate SECOM user...", self.start)
+        if microscope.role == "meteor":
+            self._mic_name = "METEOR"
+        else:
+            self._mic_name = "SECOM"
+
+        self.addMenu(f"Help/Development/Simulate {self._mic_name} user...", self.start)
 
         # Set when the simulation should be stopped
         self._should_stop = threading.Event()
 
-    def start(self):
+    def start(self) -> None:
         """
         Called when the menu entry is selected
         """
-        dlg = AcquisitionDialog(self, "SECOM user simulator",
-                                "Simulates typical actions of a SECOM user.")
+        dlg = AcquisitionDialog(self, f"{self._mic_name} user simulator",
+                                f"Simulates typical actions of a {self._mic_name} user.")
         dlg.addButton("FM search", self._sim_fluo_search, face_colour='blue')
+        dlg.addButton("Intensive use", self._sim_intensive_use, face_colour='blue')
         dlg.addButton("Stop", self._stop_sim)
         dlg.ShowModal() # Blocks until the user closes the window
 
@@ -62,41 +77,46 @@ class SecomUserPlugin(Plugin):
         if dlg: # If dlg hasn't been destroyed yet
             dlg.Destroy()
 
-    def _sim_fluo_search(self, dlg):
+    def _init_sim(self, dlg: AcquisitionDialog) -> MicroscopyGUIData:
+        self._should_stop.clear()
+
+        # Prevent running another simulation by disabling the buttons
+        wx.CallAfter(dlg.buttons[0].Disable)
+        wx.CallAfter(dlg.buttons[1].Disable)
+
+        main_data = self.main_app.main_data
+        # Switch to STREAMS tab (on SECOM) or to LOCALIZATION (on METEOR)
+        try:
+            acq_tab = main_data.getTabByName(TabName.SECOM_LIVE)
+        except LookupError:
+            acq_tab = main_data.getTabByName(TabName.CRYOSECOM_LOCALIZATION)
+            # fails if none of the tabs are found
+
+        main_data.tab.value = acq_tab
+        return acq_tab.tab_data_model
+
+    def _get_fm_stream(self, tab_data: MicroscopyGUIData) -> FluoStream:
+        # Pick a FM stream to use
+        for s in tab_data.streams.value:
+            if isinstance(s, FluoStream):
+                return s
+        raise ValueError("No FluoStream found")
+
+    def _sim_fluo_search(self, dlg: AcquisitionDialog) -> None:
         """
         Simulates the user going around with the Fluorescence stream.
         In particular, it will play/pause the stream, tweak the exposure time,
         and the binning.
         """
-        self._should_stop.clear()
-
-        # Prevent running another simulation by disabling the button
-        wx.CallAfter(dlg.buttons[0].Disable)
-
-        main_data = self.main_app.main_data
+        tab_data = self._init_sim(dlg)
         try:
-            # Switch to STREAMS tab (on SECOM) or to LOCALIZATION (on METEOR)
-            try:
-                acq_tab = main_data.getTabByName(TabName.SECOM_LIVE)
-            except LookupError:
-                acq_tab = main_data.getTabByName(TabName.CRYOSECOM_LOCALIZATION)
-                # fails if none of the tabs are found
-
-            main_data.tab.value = acq_tab
-            tab_data = acq_tab.tab_data_model
-
-            if hasattr(dlg, "setAcquisitionInfo"):
-                dlg.setAcquisitionInfo("Running FM search...")
+            dlg.setAcquisitionInfo("Running FM search...")
 
             # Pick a FM stream to use
-            for s in tab_data.streams.value:
-                if isinstance(s, FluoStream):
-                    fms = s
-                    break
-            else:
-                raise ValueError("No FluoStream found")
+            fms = self._get_fm_stream(tab_data)
 
             exp_orig = fms.detExposureTime.value
+            focuser = fms.focuser
             while not self._should_stop.is_set():
                 # Play the stream
                 fms.should_update.value = True
@@ -117,16 +137,17 @@ class SecomUserPlugin(Plugin):
                     break
 
                 # Move the focus (back and forth, to make sure we never end too far)
-                focuser = fms.focuser
                 if focuser is not None:
                     refocus_dist = random.uniform(-1e-6, 1e-6)  # m
                     logging.debug("Moving focus back and forth by %g m", refocus_dist)
                     focuser.moveRelSync({"z": refocus_dist})
                     if self._should_stop.wait(1):
                         break
+                    # Move back everything to it original position
                     focuser.moveRelSync({"z": -refocus_dist})
 
-                if self._should_stop.wait(3):
+                # Let the stream play a little longer
+                if self._should_stop.wait(2):
                     break
 
                 # TODO: change binning
@@ -139,11 +160,108 @@ class SecomUserPlugin(Plugin):
 
         finally:
             wx.CallAfter(dlg.buttons[0].Enable)
+            wx.CallAfter(dlg.buttons[1].Enable)
             logging.debug("FM simulation stopped")
-            if hasattr(dlg, "setAcquisitionInfo"): # Odemis v2.7-
+            if dlg:
                 dlg.setAcquisitionInfo() # Hide the message
             self._should_stop.clear()
 
-    def _stop_sim(self, dlg):
+    def _sim_intensive_use(self, dlg: AcquisitionDialog) -> None:
+        """
+        Simulates an intensive use of the microscope.
+        In particular, it will play/pause the stream, tweak the exposure time,
+        and moves the axes (focus, filter wheel, stigmators) back and forth.
+        """
+        tab_data = self._init_sim(dlg)
+        main_data = self.main_app.main_data
+        dlg.setAcquisitionInfo("Running intensive use...")
+
+        # Pick a FM stream to use
+        fms = self._get_fm_stream(tab_data)
+
+        exp_orig = fms.detExposureTime.value
+        focuser = fms.focuser
+        filter = main_data.light_filter
+        if filter:
+            orig_filter = filter.position.value
+            filter_choices = list(filter.axes["band"].choices)
+        else:
+            filter_choices = None
+
+        stigmator = main_data.stigmator
+
+        try:
+            while not self._should_stop.is_set():
+                # Play the stream
+                fms.should_update.value = True
+
+                # Wait a little while
+                if self._should_stop.wait(3):
+                    break
+
+                # Change a little the exposure time
+                exp = fms.detExposureTime.clip(exp_orig * random.uniform(0.1, 3.0))
+                fms.detExposureTime.value = exp
+                logging.debug("Changed exposure time to %g s", exp)
+
+                # Wait a little while
+                if self._should_stop.wait(1):
+                    break
+
+                # Move the focus (back and forth, to make sure we never end too far)
+                # Move the filter wheel and stigmators too at the same time, to simulate the worst case
+                if focuser is not None:
+                    refocus_dist = random.uniform(-1e-6, 1e-6)  # m
+                    logging.debug("Moving focus back and forth by %g m", refocus_dist)
+                    f_foc = focuser.moveRel({"z": refocus_dist})
+                    if filter:
+                        f_filter = filter.moveAbs({"band": random.choice(filter_choices)})
+
+                    if stigmator:
+                        f_stigmator = stigmator.moveAbs({"rz": math.radians(10)})
+
+                    f_foc.result()
+                    if filter:
+                        f_filter.result()
+                    if stigmator:
+                        f_stigmator.result()
+                    if self._should_stop.wait(1):
+                        break
+
+                    # Move back everything to it original position
+                    f_foc = focuser.moveRel({"z": -refocus_dist})
+                    if filter:
+                        f_filter = filter.moveAbs(orig_filter)
+                    if stigmator:
+                        f_stigmator = stigmator.moveAbs({"rz": 0})
+                    f_foc.result()
+                    if filter:
+                        f_filter.result()
+                    if stigmator:
+                        f_stigmator.result()
+
+                # Let the stream play a little longer
+                if self._should_stop.wait(2):
+                    break
+
+                # (short) coffee break
+                fms.should_update.value = False
+                if self._should_stop.wait(1):
+                    break
+                fms.should_update.value = True
+
+        finally:
+            wx.CallAfter(dlg.buttons[0].Enable)
+            wx.CallAfter(dlg.buttons[1].Enable)
+            if filter:  # Move back, without waiting for the result
+                filter.moveAbs(orig_filter)
+            if stigmator:
+                stigmator.moveAbs({"rz": 0})
+            logging.debug("Intensive simulation stopped")
+            if dlg:
+                dlg.setAcquisitionInfo()  # Hide the message
+            self._should_stop.clear()
+
+    def _stop_sim(self, dlg: AcquisitionDialog) -> None:
         logging.debug("Requesting stop of the simulation")
         self._should_stop.set()
