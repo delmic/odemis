b10fdb9fd84dd7cecb17dc989a98e608cb65936b
Tim Moerkerken <tim.moerkerken@gmail.com>
2026-09-17 10:26:37 +0200

    [fix] missing imports

--- src/odemis/gui/layout/components/__init__.py
+++ src/odemis/gui/layout/components/__init__.py
@@ -1,6 +1,9 @@
+from .panel_tab_correlation import PnlTabCorrelation
 from .panel_tab_cryosecom_chamber import PnlTabCryosecomChamber
+from .panel_tab_fastem_acqui import PnlTabFastemAcqui
 from .panel_tab_fastem_main import PnlTabFastemMain
 from .panel_tab_localization import PnlTabLocalization
+from .panel_tab_secom_streams import PnlTabSecomStreams
 
 __all__ = [
     "PnlTabCorrelation",
